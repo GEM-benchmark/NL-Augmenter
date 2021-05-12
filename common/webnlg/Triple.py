af81288f1365b5132a68d8dc072310008245cae4
Kaustubh Dhole <kaustubhdhole@hotmail.com>
2021-05-12 23:16:04 +0530

    Remove data transformations.

--- common/webnlg/Triple.py
+++
@@ -1,9 +0,0 @@
-class Triple:
-    def __init__ (self, line_triple):
-        """breaks down each input triple into subject, property, object"""
-        self.subj = line_triple.split(' | ')[0]
-        self.prop = line_triple.split(' | ')[1]
-        self.obj = line_triple.split(' | ')[2]
-
-    def get_triple(self):
-        return "{} | {} | {}".format(self.subj, self.prop, self.obj)
