10fd493b7a79612586975d10a357881bc95be830
vgtomahawk@gmail.com <vgtomahawk@gmail.com>
2021-05-23 07:19:29 +0000

    Adding tagging transformation interface

---
+++ interfaces/TaggingTransformation.py
@@ -0,0 +1,46 @@
+import abc
+from typing import Tuple, List
+
+"""
+Base Class for implementing the different input transformations a generation should be robust against.
+"""
+
+
+class TaggingTransformation(abc.ABC):
+    """
+     The base class for implementing tagging ({word_i,tag_i}* --> {word_j,tag_j}*) perturbations and transformations.
+
+     "tasks" :: The tasks for which this perturbation is applicable. All the list of tasks are
+     given in tasks.TaskType.
+
+     "locales" :: The locales and/or languages for which this perturbation is applicable. eg. "es", "mr",
+     "en_IN"
+    """
+
+    locales = None
+    tasks = None
+
+    def __init__(self):
+        print(f"Loading Transformation {self.name()}")
+
+    @classmethod
+    def domain(cls):
+        return cls.tasks, cls.locales
+
+    @classmethod
+    def name(cls):
+        return cls.__name__
+
+    @abc.abstractmethod
+    def generate(self, sentence: str) -> str:
+        raise NotImplementedError
+
+    def generateFromParse(self, parse) -> str:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def generate(self, sentence: List[str], tagSeq: List[str]) -> Tuple[List[str], List[str]]:
+        raise NotImplementedError
+
+    def generateFromParse(self, parse, tagSeq: List[str]) -> Tuple[str, List[str]]:
+        raise NotImplementedError
