a824ecd7301e0d5229984e2e4e7558165a87027e
Nickeilf <nickeilf0804@gmail.com>
2021-06-11 11:30:46 +0100

    fix function names and type hintings

--- datasets.py
+++ datasets.py
@@ -1,9 +1,12 @@
+from __future__ import annotations
 from typing import List, Iterable
+from tqdm import tqdm
+
 from interfaces.Operation import Operation
 from interfaces.SentenceOperation import *
 from interfaces.QuestionAnswerOperation import QuestionAnswerOperation
 from tasks.TaskTypes import TaskType
-from tqdm import tqdm
+
 
 class BaseDataset(object):
     
@@ -67,13 +70,13 @@ class TextLineDataset(BaseDataset):
     def __len__(self):
         return len(self.data)
         
-    def __or__(self, other: BaseDataset):
+    def __or__(self, other: TextLineDataset):
         return list(set(self.data).union(set(other.data)))
 
-    def __and__(self, other: BaseDataset):
+    def __and__(self, other: TextLineDataset):
         return list(set(self.data).intersection(set(other.data)))
 
-    def __sub__(self, other: BaseDataset):
+    def __sub__(self, other: TextLineDataset):
         return list(set(self.data).difference(set(other.data)))
 
 
@@ -98,12 +101,12 @@ class KeyValueDataset(BaseDataset):
         if self.task_type == TaskType.TEXT_TO_TEXT_GENERATION:
             if len(self.fields) == 2:
                 # assume two keys for SentenceAndTargetOperation, the two keys should refer to the "sentence" and "target"
-                self.operation_type = "sentenceandtarget"
+                self.operation_type = "sentence_and_target"
             elif len(self.fields) > 2:
                 # assume more than two keys for SentenceAndTargetsOperation, the first key refer to "sentence", others refer to "targets"
-                self.operation_type = "sentenceandtargets"
+                self.operation_type = "sentence_and_targets"
         elif self.task_type in [TaskType.QUESTION_ANSWERING, TaskType.QUESTION_GENERATION]:
-            self.operation_type = "questionanswer"
+            self.operation_type = "question_answer"
             
         self.filter_func = self.__getattribute__(
             "_apply_" + self.operation_type + "_filter")
@@ -120,17 +123,17 @@ class KeyValueDataset(BaseDataset):
         self.data = filtered_data
         return self.data
     
-    def _apply_sentenceandtarget_filter(self, datapoint: dict, filter: SentenceAndTargetOperation):
+    def _apply_sentence_and_target_filter(self, datapoint: dict, filter: SentenceAndTargetOperation):
         sentence = datapoint[self.fields[0]]
         target = datapoint[self.fields[1]]
         return filter.filter(sentence, target)
     
-    def _apply_sentenceandtargets_filter(self, datapoint: dict, filter: SentenceAndTargetsOperation):
+    def _apply_sentence_and_targets_filter(self, datapoint: dict, filter: SentenceAndTargetsOperation):
         sentence = datapoint[self.fields[0]]
         targets = [datapoint[target_key] for target_key in self.fields[1:]]
         return filter.filter(sentence, targets)
     
-    def _apply_questionanswer_filter(self, datapoint: dict, filter: QuestionAnswerOperation):
+    def _apply_question_answer_filter(self, datapoint: dict, filter: QuestionAnswerOperation):
         context = datapoint[self.fields[0]]
         question = datapoint[self.fields[1]]
         answers = [datapoint[answer_key] for answer_key in self.fields[2:]]
@@ -145,7 +148,7 @@ class KeyValueDataset(BaseDataset):
         self.data = transformed_data
         return self.data
     
-    def _apply_sentenceandtarget_transformation(self, datapoint: dict, transformation: SentenceAndTargetOperation):
+    def _apply_sentence_and_target_transformation(self, datapoint: dict, transformation: SentenceAndTargetOperation):
         sentence = datapoint[self.fields[0]]
         target = datapoint[self.fields[1]]
         transformed_sentence, transformed_target = transformation.generate(sentence, target)
@@ -153,7 +156,7 @@ class KeyValueDataset(BaseDataset):
         datapoint[self.fields[1]] = transformed_target
         return datapoint
 
-    def _apply_sentenceandtargets_transformation(self, datapoint: dict, transformation: SentenceAndTargetsOperation):
+    def _apply_sentence_and_targets_transformation(self, datapoint: dict, transformation: SentenceAndTargetsOperation):
         sentence = datapoint[self.fields[0]]
         targets = [datapoint[target_key] for target_key in self.fields[1:]]
         transformed_sentence, transformed_targets = transformation.generate(
@@ -165,7 +168,7 @@ class KeyValueDataset(BaseDataset):
             
         return datapoint
 
-    def _apply_questionanswer_transformation(self, datapoint: dict, transformation: QuestionAnswerOperation):
+    def _apply_question_answer_transformation(self, datapoint: dict, transformation: QuestionAnswerOperation):
         context = datapoint[self.fields[0]]
         question = datapoint[self.fields[1]]
         answers = [datapoint[answer_key] for answer_key in self.fields[2:]]
@@ -184,7 +187,7 @@ class KeyValueDataset(BaseDataset):
     def _sanity_check(self, other: BaseDataset):
         assert self.data[0].keys() == other.data[0].keys(), "You cannot do dataset operation on datasets with different keys"
         assert self.task_type == other.task_type, "You cannot do dataset operation on datasets for different tasks"
-        assert self.fields == other.fields, "You cannot do dataset operation on datasets with different fields"
+        assert len(self.fields) == len(other.fields), "You cannot do dataset operation on datasets with different number fields"
 
     def _data2identifier(self, data: List[str]):
         id2datapoint = {}
@@ -206,12 +209,12 @@ class KeyValueDataset(BaseDataset):
             result_data.append(id2datapoint[identifier2id[identifier]])
         return result_data
 
-    def __or__(self, other: BaseDataset):
+    def __or__(self, other: KeyValueDataset):
         self._sanity_check(other)
         id2datapoint, identifier2id, identifiers = self._data2identifier(self.data+other.data)
         return self._identifier2data(id2datapoint, identifier2id, identifiers)
             
-    def __and__(self, other: BaseDataset):
+    def __and__(self, other: KeyValueDataset):
         self._sanity_check(other)
         id2datapoint, identifier2id, identifiers = self._data2identifier(self.data)
         _, _, identifiers2 = self._data2identifier(other.data)
@@ -219,7 +222,7 @@ class KeyValueDataset(BaseDataset):
         identifiers = identifiers.intersection(identifiers2)
         return self._identifier2data(id2datapoint, identifier2id, identifiers)
 
-    def __sub__(self, other: BaseDataset):
+    def __sub__(self, other: KeyValueDataset):
         self._sanity_check(other)
         id2datapoint, identifier2id, identifiers = self._data2identifier(self.data)
         _, _, identifiers2 = self._data2identifier(other.data)
