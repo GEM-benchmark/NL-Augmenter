df8ea2c1d4a4445253b2867c65c9a6e12773b2df
kaustubhdhole <caxton007.john@gmail.com>
2021-06-14 11:27:55 +0530

    Add filter evaluation for NER tagging

--- evaluation/evaluation_engine.py
+++ evaluation/evaluation_engine.py
@@ -1,6 +1,5 @@
 from datasets import load_dataset
 from transformers import pipeline
-from sacrebleu import corpus_bleu
 from seqeval.metrics import accuracy_score
 import numpy as np
 
@@ -18,12 +17,12 @@ eg. python evaluate.py -t butter_fingers_perturbation
 
 
 def evaluate(
-    implementation,
-    task_type,
-    locale="en",
-    model=None,
-    dataset=None,
-    percent_of_examples=None,
+        implementation,
+        task_type,
+        locale="en",
+        model=None,
+        dataset=None,
+        percent_of_examples=None,
 ):
     # The evaluation engine would effectively do the following
     # (1) Loading a standard model and a test set (the model's original test set would be the best choice)
@@ -48,40 +47,40 @@ def get_task_type(implementation, task_type):
 
 
 def execute_model(
-    implementation,
-    task_type,
-    locale="en",
-    model_name=None,
-    dataset=None,
-    percentage_of_examples=20,
+        implementation,
+        task_type,
+        locale="en",
+        model_name=None,
+        dataset=None,
+        percentage_of_examples=20,
 ):
     interface = implementation.__bases__[0]  # SentenceTransformation
     impl = implementation()
     if locale is "en":
         if (
-            isinstance(impl, SentenceOperation)
-            and TaskType[task_type] == TaskType.TEXT_CLASSIFICATION
+                isinstance(impl, SentenceOperation)
+                and TaskType[task_type] == TaskType.TEXT_CLASSIFICATION
         ):
             return evaluate_text_classifier(
                 impl, model_name, dataset, split=f"test[:{percentage_of_examples}%]"
             )
         elif (
-            isinstance(impl, QuestionAnswerOperation)
-            and TaskType[task_type] == TaskType.QUESTION_ANSWERING
+                isinstance(impl, QuestionAnswerOperation)
+                and TaskType[task_type] == TaskType.QUESTION_ANSWERING
         ):
             return evaluate_question_answering_model(
                 impl, model_name, dataset, split=f"validation[:{percentage_of_examples}%]"
             )
         elif (
-            isinstance(impl, SentenceOperation)
-            and TaskType[task_type] == TaskType.TEXT_TO_TEXT_GENERATION
+                isinstance(impl, SentenceOperation)
+                and TaskType[task_type] == TaskType.TEXT_TO_TEXT_GENERATION
         ):
             return evaluate_text_summarization(
                 impl, model_name, dataset, split=f"test[:{percentage_of_examples}%]"
             )
         elif (
-            isinstance(impl, TaggingOperation)
-            and TaskType[task_type] == TaskType.TEXT_TAGGING
+                isinstance(impl, TaggingOperation)
+                and TaskType[task_type] == TaskType.TEXT_TAGGING
         ):
             return evaluate_ner_tagging(impl, model_name, dataset, split=f'test[:{percentage_of_examples}%]')
         # Other if else cases should be added here.
@@ -103,45 +102,12 @@ def execute_model(
         )
 
 
-def sacrebleu_score(hypotheses, references):
-    return corpus_bleu(hypotheses, [references]).score
-
-
-def convert_ner_ids_to_tags(ner_tags):
-    # convert list of ner ids [0,1,2,0] to list of ner tags ['0', 'B-PER', 'I-PER', '0']
-    ner_tag_sequence = []
-    ner_tag_dict = {1: 'B-PER', 2: 'I-PER', 3: 'B-ORG', 4: 'I-ORG', 5: 'B-LOC', 6: 'I-LOC', 7: 'B-MISC', 8: 'I-MISC'}
-    for tag in ner_tags:
-        ner_tag_sequence.append(ner_tag_dict.get(tag, "0")) # '0', tag for no ner token
-    return ner_tag_sequence
-
-
-def create_prediction_seq(prediction, expected_seq_length):
-    # create model output into ner tag sequence
-    # input : model output in the form [[], [{ner-info}], [{ner-info}], []]
-    # output : ['0', 'B-PER', 'I-PER', '0']
-    if (prediction == []):  # corner case where model prediction is [] and gold label is not []. ex: example["tokens"] = [',']
-        return ['0'] * expected_seq_length
-    seq = []
-    tag = ""
-    for item in prediction:
-        if(len(item)==0):
-            seq.append('0')
-        else:
-            if(isinstance(item, list)):
-                tag = item[0]['entity']
-            elif(isinstance(item, dict)): # to handle a corner case
-                tag = item['entity']
-            seq.append(tag)
-    return seq
-
-
-def evaluate_ner_tagging(transformation, model_name, dataset_name, split='validation[:20%]'):
+def evaluate_ner_tagging(operation, model_name, dataset_name, split='validation[:20%]', evaluate_filter=False):
     # load modal
     if model_name is None:
         model_name = "dslim/bert-base-NER"
     # load test set
-    if(dataset_name is None):
+    if dataset_name is None:
         dataset_name = "conll2003"
 
     print(f"Loading <{dataset_name}> dataset to evaluate <{model_name}> model.")
@@ -151,39 +117,67 @@ def evaluate_ner_tagging(transformation, model_name, dataset_name, split='valida
     average_score = 0.0
     average_pertubed_score = 0.0
     print(f"Length of Evaluation dataset is {len(dataset)}")
+    if evaluate_filter:
+        filter_true_average_score = 0.0
+        filter_false_average_score = 0.0
+        filter_true_count = 0  # This will track the number of examples where the filter is +ve
+        filter_false_count = 0  # This will track the number of examples where the filter is -ve
     for example in dataset:
         # Calculating the performance on the original set
         gold_tag_seq = convert_ner_ids_to_tags(example['ner_tags'])
         prediction = tagging_pipeline(example['tokens'])
         predicted_tag_seq = create_prediction_seq(prediction, len(gold_tag_seq))
         score = accuracy_score([gold_tag_seq], [predicted_tag_seq])
-        average_score +=score
-
-        # Calculating the performance on the perturbed set
-        trans_input, trans_gold_tag_seq = transformation.generate(example['tokens'], gold_tag_seq)
-        trans_gold_tag_seq = convert_ner_ids_to_tags(trans_gold_tag_seq)
-        transformed_input_prediction = tagging_pipeline(trans_input)
-        trans_predicted_tag_seq = create_prediction_seq(transformed_input_prediction, len(trans_gold_tag_seq))
-        pt_score = accuracy_score([trans_gold_tag_seq], [trans_predicted_tag_seq])
-        average_pertubed_score += pt_score
+        average_score += score
+        if evaluate_filter:
+            # The Operation is a "filter"
+            if operation.filter(example['tokens'], gold_tag_seq):
+                filter_true_average_score += score
+                filter_true_count += 1
+            else:
+                filter_false_average_score += score
+                filter_false_count += 1
+        else:
+            # The Operation is a "transformation"
+            # Calculating the performance on the perturbed set
+            trans_input, trans_gold_tag_seq = operation.generate(example['tokens'], gold_tag_seq)
+            trans_gold_tag_seq = convert_ner_ids_to_tags(trans_gold_tag_seq)
+            transformed_input_prediction = tagging_pipeline(trans_input)
+            trans_predicted_tag_seq = create_prediction_seq(transformed_input_prediction, len(trans_gold_tag_seq))
+            pt_score = accuracy_score([trans_gold_tag_seq], [trans_predicted_tag_seq])
+            average_pertubed_score += pt_score
 
     average_score = average_score / len(dataset) * 100
-    average_pertubed_score = average_pertubed_score / len(dataset) * 100
 
     print(f"Here is the performance of the model {model_name} on the {split} split of the {dataset} dataset")
     print(f"The average accuracy on a subset of {dataset_name} = {average_score}")
-    print(f"The average accuracy on its pertubed set = {average_pertubed_score}")
-
-    return {
+    performance = {
         "model_name": model_name,
         "split": split,
         "dataset_name": dataset_name,
         "accuracy": np.round(average_score, 1),
-        "pt_accuracy": np.round(average_pertubed_score, 1)
     }
+    if evaluate_filter:
+        filter_true_average_score = filter_true_average_score / filter_true_count * 100
+        filter_false_average_score = filter_false_average_score / filter_false_count * 100
+        print(
+            f"The average accuracy of {filter_true_count} examples which pass the filter = {filter_true_average_score}")
+        print(
+            f"The average accuracy of {filter_false_count} examples which fail the filter = {filter_false_average_score}")
+        performance["filter_true_count"] = filter_true_count
+        performance["filter_false_count"] = filter_false_count
+        performance["filter_true_average_score"] = filter_true_average_score
+        performance["filter_false_average_score"] = filter_false_average_score
+    else:
+        performance["pt_accuracy"]: np.round(average_pertubed_score, 1)
+        average_pertubed_score = average_pertubed_score / len(dataset) * 100
+        print(f"The average accuracy on its pertubed set = {average_pertubed_score}")
+
+    return performance
+
 
 def evaluate_text_summarization(
-    transformation, model_name, dataset_name, split="test[:20%]"
+        transformation, model_name, dataset_name, split="test[:20%]"
 ):
     # load model
     if model_name is None:
@@ -209,7 +203,7 @@ def evaluate_text_summarization(
     )
     predicted_summary_score = 0.0
     transformed_summary_score = 0.0
-    
+
     references = []
     raw_hypotheses = []
     pt_hypotheses = []
@@ -220,23 +214,22 @@ def evaluate_text_summarization(
         transformed_article, _ = pt_example
 
         max_len = (
-            len(gold_summary.split(" ")) + 10
+                len(gold_summary.split(" ")) + 10
         )  # approximate max length to control summary generation upto length of gold summary
         predicted_summary = summarization_pipeline(
             article, truncation=True, max_length=max_len
         )[0]["summary_text"]
-        
+
         transformed_article_summary = summarization_pipeline(
             transformed_article, truncation=True, max_length=max_len
         )[0]["summary_text"]
-        
+
         references.append(gold_summary)
         raw_hypotheses.append(predicted_summary)
         pt_hypotheses.append(transformed_article_summary)
-        
-    predicted_summary_score = sacrebleu_score(raw_hypotheses, references) # 15.989 BLEU
-    transformed_summary_score = sacrebleu_score(pt_hypotheses, references) # 11.830 BLEU
 
+    predicted_summary_score = sacrebleu_score(raw_hypotheses, references)  # 15.989 BLEU
+    transformed_summary_score = sacrebleu_score(pt_hypotheses, references)  # 11.830 BLEU
 
     print(
         f"Here is the performance of the model {model_name} on the {split} split of the {dataset_name} dataset"
@@ -253,10 +246,12 @@ def evaluate_text_summarization(
         "pt_bleu": np.round(predicted_summary_score, 1)
     }
 
+
 def evaluate_text_classifier(
-    transformation, model_name, dataset_name, split="test[:20%]", input_key=None):
+        transformation, model_name, dataset_name, split="test[:20%]", input_key=None):
     def is_positive(label):
         return label == 1 or (type(label) == str and "pos" in label.lower())
+
     # TODO: extend the task to other classification tasks that's not sentiment analysis.
     # (1) load model
     if model_name is None:
@@ -273,7 +268,6 @@ def evaluate_text_classifier(
         fields = ["sentence", "label"]
     else:
         hf_dataset = load_dataset(dataset_name, split=split)
-        
 
     dataset = TextLineDataset.from_huggingface(hf_dataset, ['text', 'label'])
     pt_dataset = dataset.apply_transformation(transformation)
@@ -288,11 +282,11 @@ def evaluate_text_classifier(
     for raw_example, pt_example in zip(dataset, pt_dataset):
         raw_text, label = raw_example
         pt_text, _ = pt_example
-       
+
         pred = nlp(raw_text, truncation=True)[0]["label"]
         if is_positive(pred) == is_positive(label):
             accuracy += 1
-        
+
         pt_pred = nlp(pt_text, truncation=True)[0]["label"]
         if is_positive(pt_pred) == is_positive(label):
             pt_accuracy += 1
@@ -314,7 +308,7 @@ def evaluate_text_classifier(
 
 
 def evaluate_question_answering_model(
-    transformation, model_name, dataset_name, split="validation[:20%]"
+        transformation, model_name, dataset_name, split="validation[:20%]"
 ):
     # (1) load model
     if model_name is None:
@@ -324,7 +318,7 @@ def evaluate_question_answering_model(
         dataset_name = "squad"
     print(
         f"Loading <{dataset_name}> dataset to evaluate <{model_name}> model.")
-    
+
     hf_dataset = load_dataset(dataset_name, split=split)
     dataset = KeyValueDataset.from_huggingface(
         hf_dataset, TaskType.QUESTION_ANSWERING, ['context', 'question', 'answers'])
@@ -345,7 +339,7 @@ def evaluate_question_answering_model(
         ]
         if pred in answers:
             accuracy += 1
-        
+
         pt_pred = nlp({"context": context_t, "question": question_t}, truncation=True)[
             "answer"
         ]
