2001e1ab18cc0c2a654408afc52d62f1c130edf6
kaustubhdhole <caxton007.john@gmail.com>
2021-06-15 01:05:56 +0530

    Combine test case loading for filters and transformations.

--- transformations/butter_fingers_perturbation/transformation.py
+++ transformations/butter_fingers_perturbation/transformation.py
@@ -88,12 +88,21 @@ class ButterFingersPerturbation(SentenceOperation):
 
 """
 # Sample code to demonstrate usage. Can also assist in adding test cases.
-
 if __name__ == '__main__':
     import json
+
     tf = ButterFingersPerturbation()
     sentence = "Andrew finally returned the French book to Chris that I bought last week"
-    test_case = {"input": sentence, "output": tf.generate(sentence)}
-    print(json.dumps(test_case))
-
+    test_cases = []
+    for sentence in ["Andrew finally returned the French book to Chris that I bought last week",
+                     "Sentences with gapping, such as Paul likes coffee and Mary tea, lack an overt predicate to indicate the relation between two or more arguments.",
+                     "Alice in Wonderland is a 2010 American live-action/animated dark fantasy adventure film",
+                     "Ujjal Dev Dosanjh served as 33rd Premier of British Columbia from 2000 to 2001",
+                     "Neuroplasticity is a continuous processing allowing short-term, medium-term, and long-term remodeling of the neuronosynaptic organization."]:
+        test_cases.append({
+            "class": tf.name(),
+            "inputs": {"sentence": sentence}, "outputs": {"sentence": tf.generate(sentence)}}
+        )
+    json_file = {"type": convert_to_snake_case(tf.name()), "test_cases": test_cases}
+    print(json.dumps(json_file))
 """
