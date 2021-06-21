3407da1a7f38f6ce1d2ba1e829768fba6022acb8
kaustubhdhole <caxton007.john@gmail.com>
2021-06-21 19:52:52 +0530

    Allow recursive dependencies.

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 import os
 from setuptools import setup
 
+from TestRunner import OperationRuns
+
+
+def all_folders():
+    folder_names = ["transformations/" + f for f in list(OperationRuns.get_all_folder_names())]
+    folder_names.extend(["filters/" + f for f in list(OperationRuns.get_all_folder_names("filters"))])
+    return folder_names
+
 
 def read(fname):
     with open(os.path.join(os.path.dirname(__file__), fname)) as f:
@@ -8,12 +16,24 @@ def read(fname):
     return data
 
 
+def recursive_requirements():
+    # (1) read main requirements.txt
+    requirements = read("requirements.txt")
+    # (1) read all requirements.txt in the folder.
+    for folder in all_folders():
+        r_file = os.path.join(os.path.dirname(__file__), folder + "/requirements.txt")
+        if os.path.isfile(r_file):
+            with open(r_file) as f:
+                requirements += f.read() + "\n"
+    return requirements
+
+
 setup(
     name="NL-Augmenter",
     version="0.0.1",
-    description=("The official repository of transformations."),
+    description="The official repository of transformations.",
     long_description=read("README.md"),
-    install_requires=read("requirements.txt"),
+    install_requires=recursive_requirements(),  # read("requirements.txt"),
     package_data={"": ["*.json", "*.txt", "*.tsv", "*.csv", "*.npz", "*.ckpt"]},
     include_package_data=True,
 )
