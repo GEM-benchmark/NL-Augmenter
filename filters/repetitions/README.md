52af4df9f47a92fcd9f2493702cd80ed9d8ddaa2
gentaiscool <gentaindrawinata@gmail.com>
2021-06-25 04:22:19 +0800

    add filters and examples

---
+++ filters/repetitions/README.md
@@ -0,0 +1,9 @@
+## Repetitions filter
+
+## What type of a filter is this?
+
+This module filters examples which contain repeitions.
+Author: Genta Indra Winata (giwinata@connect.ust.hk)
+
+## Use Cases
+This filter can be useful to find natural language with a lot of repetitions that frequently occurs in the spoken language data.
---
+++ filters/repetitions/filter.py
@@ -0,0 +1,43 @@
+from interfaces.SentenceOperation import SentenceOperation
+from tasks.TaskTypes import TaskType
+from typing import List
+
+
+class TextContainsRepetitionsFilter(SentenceOperation):
+    tasks = [TaskType.TEXT_CLASSIFICATION, TaskType.TEXT_TO_TEXT_GENERATION]
+    locales = ["en"]
+
+    def __init__(self, keywords: List[str] = None):
+        super().__init__()
+
+    def filter(self, sentence: str = None) -> bool:
+        words = sentence.split()
+        for i in range(len(words)):
+            if i > 0:
+                if words[i] == words[i-1]:
+                    return True
+        return False
+
+"""
+if __name__ == '__main__':
+    import json
+
+    tf = TextContainsRepetitionsFilter()
+    sentence = "I love cat"
+    test_cases = []
+    for sentence in ["I love cat",
+                     "I love cat cat !!",
+                     "I I want to sleep",
+                     "I want to sleep",
+                     "I really really cannot stay"]:
+        test_cases.append({
+            "class": tf.name(),
+            "args": {},
+            "inputs": {"sentence": sentence}, "outputs": tf.filter(sentence)}
+        )
+    json_file = {"type": "repetitions", "test_cases": test_cases}
+    print(json.dumps(json_file))
+
+    with open("test.json", "w") as out_file:
+        json.dump(json_file, out_file, indent=2, ensure_ascii=True)
+"""
