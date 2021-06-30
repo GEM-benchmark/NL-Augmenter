647a797306b10d34731cccbbc8066cac20c258ad
wwydmanski <wwydmanski@gmail.com>
2021-06-30 12:57:30 +0200

    feat: suspecting paraphraser for questions rephrasing

--- transformations/suspecting_paraphraser/README.md
+++ transformations/suspecting_paraphraser/README.md
@@ -1,5 +1,8 @@
 # Suspecting Paraphraser 🦎  + ⌨️ → 🐍
-This paraphraser transforms a yes/no question into a subjunctive one, which helps to add more question-specific variety to the dataset.
+This paraphraser transforms a yes/no question into a subjunctive one, which helps to add more question specific informality to the dataset. 
+
+Example: "Did the American National Shipment company really break its own fleet?" -> "The American National Shipment company really broke its own fleet, didn't it?"
+
 
 Author name: Witold Wydmański
 Author email: witold.wydmanski@uj.edu.pl
@@ -9,7 +12,7 @@ Author Affiliation: Jagiellonian University
 This transformation acts like a paraphraser and makes lexical substitutions.
 
 ## What tasks does it intend to benefit?
-This transformation would benefit tasks that are focused on question answering
+This transformation would benefit tasks that are focused on conversational question answering.
 
 ## What are the limitations of this transformation?
-The pronouns used in the endings aren't always on point - mostly because of ambiguity. As such, this transformation shouldn't be used for tasks that require their correctness.
+The pronouns used in the endings aren't always on point - mostly because of ambiguity. As such, this transformation shouldn't be used for tasks that require their correctness. 
--- transformations/suspecting_paraphraser/transformation.py
+++ transformations/suspecting_paraphraser/transformation.py
@@ -3,7 +3,7 @@ import nltk
 from pyinflect import getInflection
 import spacy
 import gender_guesser.detector as gender
-from enum import Enum 
+from enum import Enum
 import numpy as np
 
 
@@ -13,6 +13,7 @@ from tasks.TaskTypes import TaskType
 """
 Base Class for implementing the different input transformations a generation should be robust against.
 """
+
 
 class Pronouns:
     HE = 0
@@ -32,7 +33,7 @@ class SuspectingParaphraser(QuestionAnswerOperation):
         self.seed = seed
         self.max_output = max_output
 
-        self.nlp = spacy.load('en_core_web_sm')
+        self.nlp = spacy.load("en_core_web_sm")
         self.gender_detector = gender.Detector()
         self.pronouns = ["he", "she", "it", "they"]
         self.static_pronouns = ["i", "you", *self.pronouns]
@@ -41,48 +42,54 @@ class SuspectingParaphraser(QuestionAnswerOperation):
             "may": "may {} not",
             "might": "might {} not",
             "shall": "shan't {}",
-            "will": "won't {}"
+            "will": "won't {}",
         }
 
         self.pronoun_mod = pronoun_mod
-        self._pronoun_alt = (1-pronoun_mod)/(len(self.pronouns)-1)
+        self._pronoun_alt = (1 - pronoun_mod) / (len(self.pronouns) - 1)
 
     def _transform(self, question):
         text = nltk.word_tokenize(question)
         doc = self.nlp(question)
         modal = str(doc[0]).lower()
-        if doc[0].pos_!="AUX":
-            return question
+        token = doc[0]
 
-        token = doc[0]          
-
-        verb_position = [i for i in range(len(doc)) if str(doc[i])==token.head.text][0]
+        verb_position = [i for i in range(len(doc)) if str(doc[i]) == token.head.text][
+            0
+        ]
 
         rest_of_sentence = [i for i in text[verb_position:]]
+
+        if text[1].lower() == "n't":
+            text = text[1:]
+            doc = doc[1:]
+            verb_position = verb_position - 1
+
         beginning = [text[1].capitalize()]
         beginning.extend(text[2:verb_position])
-        sentence = nltk.tokenize.treebank.TreebankWordDetokenizer().detokenize(beginning + rest_of_sentence)
+        sentence = nltk.tokenize.treebank.TreebankWordDetokenizer().detokenize(
+            beginning + rest_of_sentence
+        )
 
         first_verb = doc[verb_position]
-        if modal=='did':
-            demodded = first_verb._.inflect('VBD')
+        if modal == "did":
+            demodded = first_verb._.inflect("VBD")
         else:
             demodded = modal + " " + str(first_verb)
         sentence = sentence.replace(str(first_verb), str(demodded)).replace("?", "")
 
         ending = self._resolve_ending(doc, modal)
         result = sentence + ending
-        print(result)
         return result
 
     def _resolve_ending(self, doc, modal):
         try:
-            subject = str([tok for tok in doc if (tok.dep_ == "nsubj") ][0])
+            subject = str([tok for tok in doc if (tok.dep_ == "nsubj")][0])
         except IndexError:
             print(doc)
             return ", right?"
 
-        prob = np.ones(len(self.pronouns))/len(self.pronouns)
+        prob = np.ones(len(self.pronouns)) / len(self.pronouns)
 
         tagged = [(X.text, X.label_) for X in doc.ents]
         if subject.lower() in self.static_pronouns:
@@ -90,23 +97,23 @@ class SuspectingParaphraser(QuestionAnswerOperation):
             if pronoun == "i":
                 pronoun = "I"
         else:
-            if len(tagged) > 0 and tagged[0][1]!='PERSON':
-                prob = [0]*len(self.pronouns)
+            if len(tagged) > 0 and tagged[0][1] != "PERSON":
+                prob = [0] * len(self.pronouns)
                 prob[Pronouns.IT] = 1
             else:
                 noun_gender = self.gender_detector.get_gender(subject)
 
-                if noun_gender in ['male', 'mostly_male']:
+                if noun_gender in ["male", "mostly_male"]:
                     prob = [self._pronoun_alt] * len(self.pronouns)
                     prob[Pronouns.HE] = self.pronoun_mod
 
-                elif noun_gender in ['female', 'mostly_female']:
+                elif noun_gender in ["female", "mostly_female"]:
                     prob = [self._pronoun_alt] * len(self.pronouns)
                     prob[Pronouns.SHE] = self.pronoun_mod
 
             pronoun = np.random.choice(self.pronouns, p=prob)
-        if modal=='have' and (pronoun in ['he', 'she', 'it']):
-            modal = 'has'
+        if modal == "have" and (pronoun in ["he", "she", "it"]):
+            modal = "has"
 
         ending = ", "
         if modal in self._special_endings.keys():
@@ -116,35 +123,63 @@ class SuspectingParaphraser(QuestionAnswerOperation):
         ending += "?"
         return ending
 
+    def _filter_phrase(self, question):
+        try:
+            if question.strip()[-1] != "?":
+                return False
+        except IndexError:
+            return False
+
+        if " or " in question:
+            return False
+
+        doc = self.nlp(question)
+        token = doc[0]
+        if token.pos_ != "AUX":
+            return False
+
+        return True
 
     def generate(
         self, context: str, question: str, answers: [str]
     ) -> List[Tuple[str, str, List[str]]]:
-        question = self._transform(question)
-        return [(context, question, answers)]
+        if not self._filter_phrase(question):
+            return [(context, question, answers)]
+
+        paraphrased = self._transform(question)
+        print(paraphrased)
+        return [(context, paraphrased, answers)]
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import json
     from TestRunner import convert_to_snake_case
 
     tf = SuspectingParaphraser()
 
     test_cases = []
-    for i, sentence in enumerate(["Did Sally finally return the french book to Chris?",
-                                "Did the American National Shipment company really break its own fleet?",
-                                "Would you be so kind to pass me the soup?",
-                                "Shall you begone, lad?",
-                                "Has Buzz Aldrin, the first person who walked on the moon, brought back some aliens?"]):
+    for i, sentence in enumerate(
+        [
+            "Did Sally finally return the french book to Chris?",
+            "Did the American National Shipment company really break its own fleet?",
+            "Couldn't she just leave?",
+            "Shall you begone, lad?",
+            "Has Buzz Aldrin, the first person who walked on the moon, brought back some aliens?",
+        ]
+    ):
         res = tf.generate("", sentence, [])
-        test_cases.append({
-            "class": tf.name(),
-            "inputs": {"context": "", "question": sentence, "answers": []}, "outputs": []}
+        test_cases.append(
+            {
+                "class": tf.name(),
+                "inputs": {"context": "", "question": sentence, "answers": []},
+                "outputs": [],
+            }
         )
 
         for p_context, p_question, p_answers in res:
-            test_cases[i]["outputs"].append({"context": p_context, "question": p_question, "answers": p_answers})
+            test_cases[i]["outputs"].append(
+                {"context": p_context, "question": p_question, "answers": p_answers}
+            )
 
     json_file = {"type": convert_to_snake_case(tf.name()), "test_cases": test_cases}
     print(json.dumps(json_file))
