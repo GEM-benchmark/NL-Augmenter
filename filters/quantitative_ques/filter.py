fb13e5b18af26c5eb1031dd5eaf3b5dad3831d15
tanay2001 <60615590+tanay2001@users.noreply.github.com>
2021-06-30 18:01:03 +0530

    Update filter.py

--- filters/quantitative_ques/filter.py
+++ filters/quantitative_ques/filter.py
@@ -10,10 +10,11 @@ class QuantitativeQuestion(SentenceOperation):
     def __init__(self):
         super().__init__()
         self.nlp = spacy.load("en_core_web_sm")
+        self.quant_ques = ['many','far','close','long','much','old']
 
     def filter(self, sentence: str = None) -> bool:
         tokenized = self.nlp(sentence, disable=["parser", "tagger", "ner"])
-        if 'how' == tokenized[0].text.lower() and tokenized[1].text.lower() in ['many','far','long','much','old']:
+        if 'how' == tokenized[0].text.lower() and tokenized[1].text.lower() in self.quant_ques:
             return True
         else:
             return False
