cdef6de432874440c551c0c3ed7439a40f020650
tanay2001 <60615590+tanay2001@users.noreply.github.com>
2021-06-30 19:31:20 +0530

    Update filter.py

--- filters/quantitative_ques/filter.py
+++ filters/quantitative_ques/filter.py
@@ -1,19 +1,20 @@
 import spacy
-from interfaces.SentenceOperation import SentenceOperation
+from interfaces.QuestionAnswerOperation import QuestionAnswerOperation
 from tasks.TaskTypes import TaskType
 
 
-class QuantitativeQuestion(SentenceOperation):
+class QuantitativeQuestion(QuestionAnswerOperation):
     tasks = [TaskType.QUESTION_ANSWERING, TaskType.QUESTION_GENERATION]
     languages = ["en"]
 
     def __init__(self):
         super().__init__()
         self.nlp = spacy.load("en_core_web_sm")
+        # Covers the broad types of quant questions: distance , age , measurable , un-measurable
         self.quant_ques = ['many','far','close','long','much','old']
 
-    def filter(self, sentence: str = None) -> bool:
-        tokenized = self.nlp(sentence, disable=["parser", "tagger", "ner"])
+    def filter(self,context:str = None,question: str = None,answers:str = None) -> bool:
+        tokenized = self.nlp(question, disable=["parser", "tagger", "ner"])
         if 'how' == tokenized[0].text.lower() and tokenized[1].text.lower() in self.quant_ques:
             return True
         else:
