a6d28f5b081470f894edfcd58ca89f1c70448e42
Przemyslaw Joniak <joniak@>
2021-07-02 22:27:31 +0900

    1. Replaced csv with python file with dict (as test were faiing on reading a file) 2. Wrote simple tests 3. 1st draft of replacing algo

--- transformations/gender_swap/transformation.py
+++ transformations/gender_swap/transformation.py
@@ -1,7 +1,8 @@
-from typing import Dict, List, Sequence
+from typing import List
 
 from interfaces.SentenceOperation import SentenceOperation
 from tasks.TaskTypes import TaskType
+from .gender_pairs import GENDER_PAIRS
 
 """
 TODO desc
@@ -15,23 +16,27 @@ class GenderSwap(SentenceOperation):
     ]
     languages = ["en"]
 
-    _gender_pairs_path = "gender_pairs.csv"
-
     def __init__(self) -> None:
         super().__init__()
-
-    def _get_pairs(self) -> Dict[str, str]:
-        """Reads csv file and retruns dict of gender pairs."""
-        with open(self._gender_pairs_path) as f:
-            pairs = [l.strip('\n').split(',') for l in f.readlines()]
-
-        male2female = {p[0]: p[1] for p in pairs}
-        female2male = {p[1]: p[0] for p in pairs}
-
-        return {**male2female, **female2male}
+        self.pairs = GENDER_PAIRS
 
     def generate(self, sentence: str) -> List[str]:
-        return sentence
+        output = []
+        words = sentence.split(' ')
+
+        for word in words:
+            # Filter all non-alphabetic chars in word and then lowercase
+            raw = ''.join(c for c in word if c.isalpha()).lower()
+
+            if raw in self.pairs:
+                # If the word is gendered, replace it with its counterpart
+                y = word.lower()
+                y = y.replace(raw, self.pairs[raw])
+                output.append(y)
+            else:
+                output.append(word)
+
+        return [' '.join(output)]
 
 
 if __name__ == '__main__':
