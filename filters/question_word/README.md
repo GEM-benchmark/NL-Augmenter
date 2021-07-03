693dce77f32d73535e6dc583990a016850c5ed3d
tanay2001 <dixittanay@gmail.com>
2021-07-03 16:33:17 +0530

    expanded types

--- filters/question_word/README.md
+++ filters/question_word/README.md
@@ -1,13 +1,46 @@
-## Question_word filter
+## Question type filter
 
 ## What type of a filter is this?
-This filter helps identify the question category(who, what, why ..etc)
+This filter helps identify the question category either based on question word or type of answer
 
 ## Why is measuring performance on this split important?
-This filter helps identify the type of question being generated or asked, thus helps to identify possible biases towards some categories of questions.
+This filter provides a fine-grained analysis of question answering systems,helps to identify possible biases towards some categories of questions.
 
 ## Related Work
-The code is built upon the keywords filter
-
+```bibtex
+@article{DBLP:journals/corr/RajpurkarZLL16,
+  author    = {Pranav Rajpurkar and
+               Jian Zhang and
+               Konstantin Lopyrev and
+               Percy Liang},
+  title     = {SQuAD: 100, 000+ Questions for Machine Comprehension of Text},
+  journal   = {CoRR},
+  volume    = {abs/1606.05250},
+  year      = {2016},
+  url       = {http://arxiv.org/abs/1606.05250},
+  archivePrefix = {arXiv},
+  eprint    = {1606.05250},
+  timestamp = {Mon, 24 Aug 2020 14:01:25 +0200},
+  biburl    = {https://dblp.org/rec/journals/corr/RajpurkarZLL16.bib},
+  bibsource = {dblp computer science bibliography, https://dblp.org}
+}
+@article{DBLP:journals/corr/abs-1904-02651,
+  author    = {Soham Parikh and
+               Ananya B. Sai and
+               Preksha Nema and
+               Mitesh M. Khapra},
+  title     = {ElimiNet: {A} Model for Eliminating Options for Reading Comprehension
+               with Multiple Choice Questions},
+  journal   = {CoRR},
+  volume    = {abs/1904.02651},
+  year      = {2019},
+  url       = {http://arxiv.org/abs/1904.02651},
+  archivePrefix = {arXiv},
+  eprint    = {1904.02651},
+  timestamp = {Wed, 24 Apr 2019 12:21:25 +0200},
+  biburl    = {https://dblp.org/rec/journals/corr/abs-1904-02651.bib},
+  bibsource = {dblp computer science bibliography, https://dblp.org}
+}
+```
 ## What are the limitations of this filter?
 It relies on lexical and purely context-free matching for filtering out questions. 
--- filters/question_word/filter.py
+++ filters/question_word/filter.py
@@ -1,10 +1,10 @@
 from tasks.TaskTypes import TaskType
 from typing import List
-from interfaces.SentenceOperation import SentenceOperation
+from interfaces.QuestionAnswerOperation import QuestionAnswerOperation
 import spacy
+from typing import List
 
-
-class KeywordsFilter(SentenceOperation):
+class BaseFilter(QuestionAnswerOperation):
     tasks = [TaskType.TEXT_CLASSIFICATION, TaskType.TEXT_TO_TEXT_GENERATION]
     languages = ["en"]
 
@@ -13,74 +13,205 @@ class KeywordsFilter(SentenceOperation):
         self.keywords = keywords
         self.nlp = spacy.load("en_core_web_sm")
 
-    def filter(self, sentence: str = None) -> bool:
-        tokenized = self.nlp(sentence, disable=["parser", "tagger", "ner"])
+    def filter(self,context:str = None ,question: str = None, answers: List[str]= None) -> bool:
+        tokenized = self.nlp(question, disable=["parser", "tagger", "ner"])
         tokenized = [token.text.lower() for token in tokenized]
         contained_keywords = set(tokenized).intersection(set(self.keywords))
         return bool(contained_keywords)
 
 
-class WhereQuestion(KeywordsFilter):
+class WhereQuestion(BaseFilter):
     tasks = [TaskType.QUESTION_GENERATION, TaskType.QUESTION_ANSWERING]
     languages = ["en"]
 
     def __init__(self, keywords=["where"]):
         super().__init__(keywords=keywords)
 
-    def filter(self, sentence: str) -> bool:
-        return super().filter(sentence=sentence)
+    def filter(self,context:str = None ,question: str = None, answers: List[str]= None) -> bool:
+        return super().filter(context,question,answers)
 
 
-class WhatQuestion(KeywordsFilter):
+class WhatQuestion(BaseFilter):
     tasks = [TaskType.QUESTION_GENERATION, TaskType.QUESTION_ANSWERING]
     languages = ["en"]
 
     def __init__(self, keywords=["what"]):
         super().__init__(keywords=keywords)
 
-    def filter(self, sentence: str) -> bool:
-        return super().filter(sentence=sentence)
+    def filter(self,context:str = None ,question: str = None, answers: List[str]= None) -> bool:
+        return super().filter(context,question,answers)
 
 
-class WhoQuestion(KeywordsFilter):
+class WhoQuestion(BaseFilter):
     tasks = [TaskType.QUESTION_GENERATION, TaskType.QUESTION_ANSWERING]
     languages = ["en"]
 
     def __init__(self, keywords=["who"]):
         super().__init__(keywords=keywords)
 
-    def filter(self, sentence: str) -> bool:
-        return super().filter(sentence=sentence)
+    def filter(self,context:str = None ,question: str = None, answers: List[str]= None) -> bool:
+        return super().filter(context,question,answers)
 
-
-class WhichQuestion(KeywordsFilter):
+class WhichQuestion(BaseFilter):
     tasks = [TaskType.QUESTION_GENERATION, TaskType.QUESTION_ANSWERING]
     languages = ["en"]
 
     def __init__(self, keywords=["which"]):
         super().__init__(keywords=keywords)
 
-    def filter(self, sentence: str) -> bool:
-        return super().filter(sentence=sentence)
+    def filter(self,context:str = None ,question: str = None, answers: List[str]= None) -> bool:
+        return super().filter(context,question,answers)
 
 
-class WhyQuestion(KeywordsFilter):
+class WhyQuestion(BaseFilter):
     tasks = [TaskType.QUESTION_GENERATION, TaskType.QUESTION_ANSWERING]
     languages = ["en"]
 
     def __init__(self, keywords=["why"]):
         super().__init__(keywords=keywords)
 
-    def filter(self, sentence: str) -> bool:
-        return super().filter(sentence=sentence)
+    def filter(self,context:str = None ,question: str = None, answers: List[str]= None) -> bool:
+        return super().filter(context,question,answers)
 
 
-class WhenQuestion(KeywordsFilter):
+class WhenQuestion(BaseFilter):
     tasks = [TaskType.QUESTION_GENERATION, TaskType.QUESTION_ANSWERING]
     languages = ["en"]
 
     def __init__(self, keywords=["when"]):
         super().__init__(keywords=keywords)
 
-    def filter(self, sentence: str) -> bool:
-        return super().filter(sentence=sentence)
+    def filter(self,context:str = None ,question: str = None, answers:List[str] =  None) -> bool:
+        return super().filter(context,question,answers)
+
+
+class NumericQuestion(QuestionAnswerOperation):
+    tasks = [TaskType.QUESTION_GENERATION, TaskType.QUESTION_ANSWERING]
+    languages = ["en"]
+
+    def __init__(self):
+        super().__init__()
+        self.nlp = spacy.load("en_core_web_sm")
+    
+    def filter(self,context:str = None ,question: str = None, answers: List[str]= None) -> bool:
+        for ans in answers:
+            tokenized = self.nlp(ans)
+            tags = [i.pos_ for i in tokenized]
+            ner = [i.label_ for i in tokenized.ents]
+            if 'NUM' in tags and 'DATE' not in ner:
+                return True
+        return False
+
+class DateQuestion(QuestionAnswerOperation):
+    tasks = [TaskType.QUESTION_GENERATION, TaskType.QUESTION_ANSWERING]
+    languages = ["en"]
+
+    def __init__(self):
+        super().__init__()
+        self.nlp = spacy.load("en_core_web_sm")
+    
+    def filter(self,context:str = None ,question: str = None, answers: List[str]= None) -> bool:
+        for ans in answers:
+            tokenized = self.nlp(ans)
+            tags = [i.pos_ for i in tokenized]
+            ner = [i.label_ for i in tokenized.ents]
+            if 'NUM' in tags and 'DATE' in ner:
+                return True
+        return False
+
+class PersonQuestion(QuestionAnswerOperation):
+    tasks = [TaskType.QUESTION_GENERATION, TaskType.QUESTION_ANSWERING]
+    languages = ["en"]
+
+    def __init__(self):
+        super().__init__()
+        self.nlp = spacy.load("en_core_web_sm")
+    
+    def filter(self,context:str = None ,question: str = None, answers: List[str]= None) -> bool:
+        if not NumericQuestion().filter(context = context,
+                                question= question,
+                                answers = answers):
+            for ans in answers:
+                tokenized = self.nlp(ans)
+                ner = [i.label_ for i in tokenized.ents]
+                if 'PERSON' in ner:
+                    return True
+        return False
+                    
+class LocationQuestion(QuestionAnswerOperation):
+    tasks = [TaskType.QUESTION_GENERATION, TaskType.QUESTION_ANSWERING]
+    languages = ["en"]
+
+    def __init__(self):
+        super().__init__()
+        self.nlp = spacy.load("en_core_web_sm")
+    
+    def filter(self,context:str = None ,question: str = None, answers: List[str]= None) -> bool:
+        if not NumericQuestion().filter(context = context,
+                                question= question,
+                                answers = answers):
+            for ans in answers:
+                tokenized = self.nlp(ans)
+                ner = [i.label_ for i in tokenized.ents]
+                if 'LOC' in ner or 'GPE' in ner:
+                    return True
+        return False
+                    
+class CommonNounPhraseQuestion(QuestionAnswerOperation):
+    tasks = [TaskType.QUESTION_GENERATION, TaskType.QUESTION_ANSWERING]
+    languages = ["en"]
+
+    def __init__(self):
+        super().__init__()
+        self.nlp = spacy.load("en_core_web_sm")
+    
+    def filter(self,context:str = None ,question: str = None, answers: List[str]= None) -> bool:
+        if not NumericQuestion().filter(context = context,
+                                question= question,
+                                answers = answers):
+            for ans in answers:
+                tokenized = self.nlp(ans)
+                tags = [i.pos_ for i in tokenized]
+                phrases = [i for i in tokenized.noun_chunks]
+                if 'PROPN' not in tags and 'VERB' not in tags and 'ADJ' not in tags and phrases !=[]:
+                    return True
+        return False
+
+class AdjectivePhraseQuestion(QuestionAnswerOperation):
+    tasks = [TaskType.QUESTION_GENERATION, TaskType.QUESTION_ANSWERING]
+    languages = ["en"]
+
+    def __init__(self):
+        super().__init__()
+        self.nlp = spacy.load("en_core_web_sm")
+    
+    def filter(self,context:str = None ,question: str = None, answers: List[str]= None) -> bool:
+        if not NumericQuestion().filter(context = [],
+                                question= [],
+                                answers = answers):
+            for ans in answers:
+                tokenized = self.nlp(ans)
+                tags = [i.pos_ for i in tokenized]
+                if 'PROPN' not in tags and 'VERB' not in tags and 'ADJ' in tags:
+                    return True
+        return False
+
+class VerbPhraseQuestion(QuestionAnswerOperation):
+    tasks = [TaskType.QUESTION_GENERATION, TaskType.QUESTION_ANSWERING]
+    languages = ["en"]
+
+    def __init__(self):
+        super().__init__()
+        self.nlp = spacy.load("en_core_web_sm")
+    
+    def filter(self,context:str = None ,question: str = None, answers: List[str]= None) -> bool:
+        if not NumericQuestion().filter(context = [],
+                                question= [],
+                                answers = answers):
+            for ans in answers:
+                tokenized = self.nlp(ans)
+                tags = [i.pos_ for i in tokenized]
+                if 'PROPN' not in tags and 'VERB' in tags and 'ADJ' not in tags:
+                    return True
+        return False           
+            
