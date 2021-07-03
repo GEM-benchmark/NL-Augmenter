43746ad88d7beda5dd29e616a78a7c867d771914
Ashish Shrivastava <ashish3586@gmail.com>
2021-07-04 01:08:32 +0530

    complete all suggested changes

--- transformations/add_hashtags/transformation.py
+++ transformations/add_hashtags/transformation.py
@@ -3,12 +3,7 @@ from tasks.TaskTypes import TaskType
 import random
 from spacy import load
 
-be_class_verb = ["is", "am", "are", "was", "were", "will", "shall"]
-subj_obj_list = ["i", "you","we", "they", "he", "she"]
-nlp=load('en_core_web_sm')
-
-
-def extract_dep_nodes(dep_parse):
+def extract_dep_nodes(dep_parse, be_class_verb, subj_obj_list):
     # method for extracting VERB, NSUBJ and DOBJ dependency nodes
     verb, nsubj, dobj = "", "", ""
     for token in dep_parse:
@@ -25,48 +20,49 @@ def extract_dep_nodes(dep_parse):
     return verb.title(), nsubj.title(), dobj.title()
 
 
-def generate_hashtag_from_noun_chunk(chunk_list):
+def generate_hashtag_from_noun_chunk(chunk_list, subj_obj_list):
+    # method for generating hastags from noun chunks
     hash_tag_list = []
     if not chunk_list:
         return None
     else:
         for chunk in chunk_list:
             if chunk.lower() not in subj_obj_list:
-                chunk_words = chunk.split(" ")
-                chunk_words = [word.title() for word in chunk_words]
+                chunk_words = [word.title() for word in chunk.split(" ")]
                 hash_tag_list.append("#"+"".join(chunk_words))
     return hash_tag_list
 
 
-def extract_noun_chunks_hashtag(dep_parse):
+def extract_noun_chunks_hashtag(dep_parse, subj_obj_list):
+    # Method for extracting noun chunks from dependency parse
     chunk_list = []
     for chunk in dep_parse.noun_chunks:
         if len(str(chunk.text.split(" ")))>0:
             chunk_list.append(chunk.text)
-    return generate_hashtag_from_noun_chunk(chunk_list)
+    return generate_hashtag_from_noun_chunk(chunk_list, subj_obj_list)
 
 
-def extract_hashtags(sentence):
+def extract_hashtags(sentence, nlp, be_class_verb, subj_obj_list):
     # method for gathering all hashtags
     dep_parse = nlp(sentence)
-    verb, nsubj, dobj = extract_dep_nodes(dep_parse)
+    verb, nsubj, dobj = extract_dep_nodes(dep_parse, be_class_verb, subj_obj_list)
     hash_tag_list = []
     for dep_n in [verb, nsubj, dobj]:
         if(dep_n != ""):
             hash_tag_list.append("#"+dep_n)
     if verb != "" and dobj != "":
         hash_tag_list.append("#"+verb+dobj)
-    noun_chunks_hashtags = extract_noun_chunks_hashtag(dep_parse)
+    noun_chunks_hashtags = extract_noun_chunks_hashtag(dep_parse, subj_obj_list)
     if noun_chunks_hashtags is not None:
         hash_tag_list.extend(noun_chunks_hashtags) # merging both hashtag list
-        return set(hash_tag_list)
-    return hash_tag_list
+        return verb, set(hash_tag_list)
+    return verb, hash_tag_list
 
 
-def get_hash_tags(sentence, seed=0, max_outputs=1):
-    # method for adding hashtags to sentence
+def get_hash_tags(sentence, be_class_verb, subj_obj_list, seed=0, max_outputs=1, nlp=None):
+    # method for appending hashtags to sentence
     random.seed(seed)
-    hashtag_list = extract_hashtags(sentence)
+    verb, hashtag_list = extract_hashtags(sentence, nlp, be_class_verb, subj_obj_list)
     transformation_list = []
     for _ in range(max_outputs):
         selected_hastag = random.sample(hashtag_list, random.randint(1, len(hashtag_list)))
@@ -80,17 +76,20 @@ def get_hash_tags(sentence, seed=0, max_outputs=1):
 class HashtagGeneration(SentenceOperation):
     tasks = [
         TaskType.TEXT_CLASSIFICATION,
-        TaskType.TEXT_TO_TEXT_GENERATION,
-        TaskType.TEXT_TAGGING
+        TaskType.TEXT_TO_TEXT_GENERATION
     ]
     languages = ["en"]
 
     def __init__(self, seed=0, max_outputs=2):
         super().__init__(seed)
         self.max_outputs=max_outputs
+        self.nlp = load('en_core_web_sm')
+        self.be_class_verb = ["is", "am", "are", "was", "were", "will", "shall"]
+        self.subj_obj_list = ["i", "you", "we", "they", "he", "she"]
 
     def generate(self, sentence: str):
-        transformed_sentences = get_hash_tags(sentence, seed=self.seed, max_outputs=self.max_outputs)
+        transformed_sentences = get_hash_tags(sentence, self.be_class_verb, self.subj_obj_list,
+                                              seed=self.seed, max_outputs=self.max_outputs, nlp = self.nlp)
         return transformed_sentences
 
 
@@ -103,7 +102,7 @@ class HashtagGeneration(SentenceOperation):
 #     input_sent = ["I love domino's pizza .",
 #                   "Many people like T20 cricket these days .",
 #                   "Attention is all you need .",
-#                   "Natural Language Processing research is awesome."]
+#                   "Natural Language Processing research is awesome ."]
 #
 #     for i,sentence in enumerate(input_sent):
 #         transformed_sentence = tf.generate(sentence)
@@ -114,5 +113,5 @@ class HashtagGeneration(SentenceOperation):
 #             )
 #         for trans_sentence in transformed_sentence:
 #             test_cases[i]["outputs"].append({"sentence":trans_sentence})
-#     json_file = {"type": convert_to_snake_case(tf.name()), "test_cases": test_cases}
+#     json_file = {"type": convert_to_snake_case("add_hashtags"), "test_cases": test_cases}
 #     print(json.dumps(json_file))
