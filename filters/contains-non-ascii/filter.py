025694d7e2aa78af8b8c47d58a42283439288b8f
Jan Pfister <janpf@users.noreply.github.com>
2021-07-07 20:14:36 +0200

    Update filter.py

--- filters/contains-non-ascii/filter.py
+++ filters/contains-non-ascii/filter.py
@@ -4,12 +4,8 @@ from tasks.TaskTypes import TaskType
 
 class TextContainsNonAsciiFilter(SentenceOperation):
     tasks = [e for e in TaskType]
-    languages = ["en", "de", "all"]
-
-    def __init__(self):
-        super().__init__()
+    languages = ["en", "de"]
 
     def filter(self, sentence: str = None) -> bool:
-        print(sentence)
         contains_unicode = sentence != sentence.encode("ascii", "ignore").decode("ascii")
         return contains_unicode
