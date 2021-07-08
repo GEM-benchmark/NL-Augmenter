dd2117b81194a5630f2228472ea63d720769a515
KaizhaoLiang <kl2@illinois.edu>
2021-07-07 23:25:34 -0700

    remove upper and lower case agumentation and irrevelant cases

--- transformations/close_homophones_swap/transformation.py
+++ transformations/close_homophones_swap/transformation.py
@@ -9,23 +9,21 @@ from SoundsLike.SoundsLike import Search
 from spacy.attrs import LOWER, POS, ENT_TYPE, IS_ALPHA
 from spacy.tokens import Doc
 
-"""
-Base Class for implementing the different input transformations a generation should be robust against.
-"""
-
-
-def close_homophones_swap(text, corrupt_prob, seed=0, max_output=1, nlp = None):
+def close_homophones_swap(text, corrupt_prob, seed=0, max_outputs=1, nlp = None):
     random.seed(seed)
     doc = nlp(text)
     perturbed_texts = []
     spaces = [True if tok.whitespace_ else False for tok in doc]
-    for _ in range(max_output):
+    for _ in range(max_outputs):
         perturbed_text = []
         for index, token in enumerate(doc):
             if random.uniform(0, 1) < corrupt_prob:
                 try:
                     replacement = random.choice(Search.closeHomophones(token.text))
-                    perturbed_text.append(replacement)
+                    if replacement.lower()!=token.text.lower() and token.text.lower()!='a':
+                        perturbed_text.append(replacement)
+                    else:
+                        perturbed_text.append(token.text)
                 except:
                     perturbed_text.append(token.text)
             else:
@@ -47,13 +45,32 @@ class CloseHomophonesSwap(SentenceOperation):
     ]
     languages = ["en"]
 
-    def __init__(self, seed=0, max_output=1):
+    def __init__(self, seed=0, max_outputs=1):
         super().__init__(seed)
-        self.max_output = max_output
+        self.max_outputs = max_outputs
         self.nlp = spacy.load("en_core_web_sm")
 
     def generate(self, sentence: str):
         perturbed_texts = close_homophones_swap(
-            text=sentence, corrupt_prob=0.5, seed=self.seed, max_output=self.max_output, nlp = self.nlp
+            text=sentence, corrupt_prob=0.5, seed=self.seed, max_outputs=self.max_outputs, nlp = self.nlp
         )
         return perturbed_texts
+# Sample code to demonstrate usage. Can also assist in adding test cases.
+# You don't need to keep this code in your transformation.
+if __name__ == '__main__':
+    import json
+    from TestRunner import convert_to_snake_case
+    tf = CloseHomophonesSwap(max_outputs=1)
+    sentence = "Andrew finally returned the French book to Chris that I bought last week"
+    test_cases = []
+    for sentence in ["Andrew finally returned the French book to Chris that I bought last week",
+                     "Sentences with gapping, such as Paul likes coffee and Mary tea, lack an overt predicate to indicate the relation between two or more arguments.",
+                     "Alice in Wonderland is a 2010 American live-action/animated dark fantasy adventure film",
+                     "Ujjal Dev Dosanjh served as 33rd Premier of British Columbia from 2000 to 2001",
+                     "Neuroplasticity is a continuous processing allowing short-term, medium-term, and long-term remodeling of the neuronosynaptic organization."]:
+        test_cases.append({
+            "class": tf.name(),
+            "inputs": {"sentence": sentence}, "outputs": [{"sentence": o} for o in tf.generate(sentence)]}
+        )
+    json_file = {"type": convert_to_snake_case(tf.name()), "test_cases": test_cases}
+    print(json.dumps(json_file))
