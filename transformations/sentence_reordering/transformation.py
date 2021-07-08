5cefe721ff1b1914f876acf6f6c3b3272261e056
kaustubhdhole <caxton007.john@gmail.com>
2021-07-08 20:51:05 +0530

    Update sentence ordering to heavy.

--- transformations/sentence_reordering/transformation.py
+++ transformations/sentence_reordering/transformation.py
@@ -7,9 +7,7 @@ import spacy
 
 # coref resolution from allennlp
 # ref: https://demo.allennlp.org/coreference-resolution
-import allennlp_models.tagging
 from allennlp.predictors.predictor import Predictor
-
 
 """
 Shuffle sentence order
@@ -22,15 +20,18 @@ class SentenceReordering(SentenceOperation):
         TaskType.TEXT_TO_TEXT_GENERATION,
     ]
     languages = ["en"]
+    heavy = True
 
-    def __init__(self, seed=42, max_output=1):
+    def __init__(self, enable_coref=True, seed=42, max_output=1):
         super().__init__(seed)
         self.seed = seed
         self.nlp = spacy.load("en_core_web_sm")
         self.max_output = max_output
-        self.coref_model = Predictor.from_path(
-            "https://storage.googleapis.com/allennlp-public-models/coref-spanbert-large-2021.03.10.tar.gz"
-        )
+        self.enable_coref = enable_coref
+        if enable_coref:
+            self.coref_model = Predictor.from_path(
+                "https://storage.googleapis.com/allennlp-public-models/coref-spanbert-large-2021.03.10.tar.gz"
+            )
 
     def generate(self, sentence: str):
         pertubed = [self.sentence_reordering(text=sentence)]
@@ -39,7 +40,8 @@ class SentenceReordering(SentenceOperation):
     def sentence_reordering(self, text):
         random.seed(self.seed)
         # resolve coref
-        text = self.coref_model.coref_resolved(document=text)
+        if self.enable_coref:
+            text = self.coref_model.coref_resolved(document=text)
 
         # tokenize and shuffle
         text_split = [i.text for i in self.nlp(text).sents]
