821deffcb56cf1b094004a12f6741fec902adecf
KaizhaoLiang <kl2@illinois.edu>
2021-07-07 23:38:07 -0700

    remove unnecessary code

--- transformations/close_homophones_swap/transformation.py
+++ transformations/close_homophones_swap/transformation.py
@@ -55,22 +55,3 @@ class CloseHomophonesSwap(SentenceOperation):
             text=sentence, corrupt_prob=0.5, seed=self.seed, max_outputs=self.max_outputs, nlp = self.nlp
         )
         return perturbed_texts
-# Sample code to demonstrate usage. Can also assist in adding test cases.
-# You don't need to keep this code in your transformation.
-if __name__ == '__main__':
-    import json
-    from TestRunner import convert_to_snake_case
-    tf = CloseHomophonesSwap(max_outputs=1)
-    sentence = "Andrew finally returned the French book to Chris that I bought last week"
-    test_cases = []
-    for sentence in ["Andrew finally returned the French book to Chris that I bought last week",
-                     "Sentences with gapping, such as Paul likes coffee and Mary tea, lack an overt predicate to indicate the relation between two or more arguments.",
-                     "Alice in Wonderland is a 2010 American live-action/animated dark fantasy adventure film",
-                     "Ujjal Dev Dosanjh served as 33rd Premier of British Columbia from 2000 to 2001",
-                     "Neuroplasticity is a continuous processing allowing short-term, medium-term, and long-term remodeling of the neuronosynaptic organization."]:
-        test_cases.append({
-            "class": tf.name(),
-            "inputs": {"sentence": sentence}, "outputs": [{"sentence": o} for o in tf.generate(sentence)]}
-        )
-    json_file = {"type": convert_to_snake_case(tf.name()), "test_cases": test_cases}
-    print(json.dumps(json_file))
