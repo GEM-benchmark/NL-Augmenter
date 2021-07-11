7719ba7e27243e6a13d2af26866cfd8dc9b5053d
vasile <vasile@localhost>
2021-07-11 15:09:59 +0300

    correction for TASKS

--- transformations/geonames_transformation/transformation.py
+++ transformations/geonames_transformation/transformation.py
@@ -10,7 +10,6 @@ class GeoNamesTransformation(SentenceOperation):
     tasks = [
         TaskType.TEXT_CLASSIFICATION,
         TaskType.TEXT_TO_TEXT_GENERATION,
-        TaskType.TEXT_TAGGING,
     ]
     languages = ["en"]
 
