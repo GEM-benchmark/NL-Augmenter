7ec1e57ac73825c8eab05e373d2eecc62d1b6f26
Przemyslaw Joniak <joniak@>
2021-07-14 12:28:56 +0900

    gender_swap now swaps male<->female names by default

--- transformations/gender_swap/transformation.py
+++ transformations/gender_swap/transformation.py
@@ -1,4 +1,7 @@
+import random
+
 from typing import List
+from checklist.editor import Editor
 
 from interfaces.SentenceOperation import SentenceOperation
 from tasks.TaskTypes import TaskType
@@ -6,7 +9,13 @@ from .gender_pairs import GENDER_PAIRS
 
 
 class GenderSwap(SentenceOperation):
-    """Swaps all gendered words in a given sentence with their counterparts."""
+    """Swaps all gendered words in a given sentence with their counterparts.
+
+    Args:
+        swap_names: swap names randomly? e.g Alice ↔ Bob. Defaults: True.
+        seed: initial seed. Defaults: 0.
+        max_outputs: maximum number of generated outputs. Defaults: 1.
+    """
 
     tasks = [
         TaskType.TEXT_CLASSIFICATION,
@@ -15,9 +24,21 @@ class GenderSwap(SentenceOperation):
     ]
     languages = ["en"]
 
-    def __init__(self, seed: int = 0, max_outputs: int = 1) -> None:
+    def __init__(
+        self, swap_names: bool = True, seed: int = 0, max_outputs: int = 1
+    ) -> None:
         super().__init__(seed, max_outputs)
         self.pairs = GENDER_PAIRS
+        self.swap_names = swap_names
+
+        if self.swap_names:
+            # Random choice from a set is not possible, we'll cache the lists
+            self._male_names_list = Editor().lexicons["male"]
+            self._female_name_list = Editor().lexicons["female"]
+
+            # And keep sets for a quick lookup
+            self.male_names = set(map(str.lower, self._male_names_list))
+            self.female_names = set(map(str.lower, self._female_name_list))
 
     def _copy_casing(self, target: str, input: str) -> str:
         """Sets the first char of input to have the same case as target.
@@ -56,9 +77,21 @@ class GenderSwap(SentenceOperation):
 
             raw = self._normalize(word)
 
+            counterpart = None
+
             if raw in self.pairs:
-                # If the word is gendered, replace it with its counterpart
-                y = word.lower().replace(raw, self.pairs[raw])
+                counterpart = self.pairs[raw]
+
+            if self.swap_names:
+                if raw in self.female_names:
+                    counterpart = random.choice(self._male_names_list)
+
+                if raw in self.male_names:
+                    counterpart = random.choice(self._female_name_list)
+
+            if counterpart is not None:
+                # If we found a counterpart to an original word, replace it
+                y = word.lower().replace(raw, counterpart)
 
                 # Bring back casing of the first letter
                 y = self._copy_casing(word, y)
