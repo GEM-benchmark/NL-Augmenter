1192e9ea8ecf4387fcd3b8aea0e2b4cb043d692d
vasile <vasile@localhost>
2021-07-15 18:42:22 +0300

    Implemented new patterns: "city, the capital of", country neighbors, country codes, spoken languages. There is a check to prevent adding a pattern if the city/country specified already exists. Updated README.md The code is now split into methods for each pattern to make it easier to read.

--- transformations/geonames_transformation/README.md
+++ transformations/geonames_transformation/README.md
@@ -8,7 +8,7 @@ Author Affiliation: Research Institute for Artificial Intelligence "Mihai Drăg�
 ## What type of a transformation is this?
 This transformation identifies cities and countries present in GeoNames and augment the sentence with additional information.
 
-For countries, the following information is used: continent, capital, population.
+For countries, the following information is used: continent, capital, population, neighbours, languages.
 
 For cities, the following information is used: continent, country, population.
 
@@ -34,8 +34,12 @@ Generated sentences:
 ```Mangalia, a city in Europe, with a population of 39,619 inhabitants, is situated on the shores of the Black Sea```
 
 
-## Previous Work
+## Data and code provenance
+This transformation makes use of city and country information obtained from GeoNames ( http://download.geonames.org/export/dump/ ).
+Additionally, locale codes were used for obtaining the language names. This information was obtained from ( https://www.science.co.il/language/Locale-codes.php ).
 
+The code was written from scratch by the author for this project. An initial idea was explored for the purposes of my PhD thesis:
+```Vasile Păiș. Contributions to semantic processing of texts; Identification of entities and relations between textual units; Case study on Romanian language. PhD thesis, Romanian Academy, 2019.```
 
 ## What are the limitations of this transformation?
 If the identified name corresponds to both a city and a country, only the country enhancements will be applied. The transformation does not perform disambiguation to understand if it is a city or a country.
--- transformations/geonames_transformation/transformation.py
+++ transformations/geonames_transformation/transformation.py
@@ -53,9 +53,48 @@ class GeoNamesTransformation(SentenceOperation):
 
         # print("Loaded %d cities"%(len(self.cities)))
 
+    def loadLocales(self):
+        self.locales = {}
+
+        __location__ = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__))
+        )
+
+        with io.open(
+            os.path.join(__location__, "locales.txt"), mode="r", encoding="utf8"
+        ) as file:
+            first = True
+            while True:
+                data = file.readline()
+                if not data:
+                    break
+
+                line = data.split("\t")
+
+                if first:
+                    first = False
+                    continue
+
+                # 0 = locale name
+                # 1 = language code
+                # 2 = LCID code
+
+                language = line[0].split("-")[0].strip()
+                basicCode = line[2].split("-")[0].strip()
+
+                self.locales[line[2]] = {
+                    "language": language,
+                }
+
+                self.locales[basicCode] = {
+                    "language": language,
+                }
+        # print("Loaded %d locales"%(len(self.locales)))
+
     def loadCountries(self):
         self.countries = {}
         self.countriesName = {}
+        self.capitals = {}
 
         __location__ = os.path.realpath(
             os.path.join(os.getcwd(), os.path.dirname(__file__))
@@ -75,11 +114,18 @@ class GeoNamesTransformation(SentenceOperation):
                 line = data.split("\t")
 
                 # 0 = ISO
+                # 1 = ISO3
                 # 4 = name
                 # 5 = capital
                 # 7 = population
                 # 8 = continent
                 # 11 = currency name
+                # 15 = languages
+                # 17 = neighbours
+
+                neighbours = []
+                if len(line[17]) > 0:
+                    neighbours = line[17].split(",")
 
                 self.countries[line[0]] = {
                     "name": line[4],
@@ -90,11 +136,17 @@ class GeoNamesTransformation(SentenceOperation):
                 }
 
                 self.countriesName[line[4]] = {
+                    "iso": line[0],
+                    "iso3": line[1],
                     "capital": line[5],
                     "population": line[7],
                     "continent": line[8],
                     "currency": line[11],
+                    "neighbours": neighbours,
+                    "languages": line[15].split(","),
                 }
+
+                self.capitals[line[5]] = {"countryName": line[4]}
 
         # print("Loaded %d countries"%(len(self.countries)))
 
@@ -115,6 +167,277 @@ class GeoNamesTransformation(SentenceOperation):
         self.loadContinents()
         self.loadCountries()
         self.loadCities()
+        self.loadLocales()
+
+    def pattern_countryInContinent(self, start, final, data, textFeatures):
+        continent = self.continents[data["continent"]]
+        if continent in textFeatures:
+            return []
+        return [(start + ", a country in " + continent + final)]
+
+    def pattern_countryInContinentWithPopulation(
+        self, start, final, data, textFeatures
+    ):
+        continent = self.continents[data["continent"]]
+        population = "{:,}".format(int(data["population"]))
+        if continent in textFeatures:
+            return []
+        return [
+            (
+                start
+                + ", a country in "
+                + continent
+                + ", with a population of "
+                + population
+                + " inhabitants"
+                + final
+            )
+        ]
+
+    def pattern_countryWithCapital(self, start, final, data, textFeatures):
+        capital = data["capital"]
+        if capital in textFeatures:
+            return []
+        return [(start + ", whose capital city is " + capital + final)]
+
+    def pattern_countryInContinentWithPopulationAndCapital(
+        self, start, final, data, textFeatures
+    ):
+        continent = self.continents[data["continent"]]
+        population = "{:,}".format(int(data["population"]))
+        capital = data["capital"]
+        if continent in textFeatures or capital in textFeatures:
+            return []
+        return [
+            (
+                start
+                + ", a country in "
+                + continent
+                + ", with a population of "
+                + population
+                + " inhabitants, with the capital at "
+                + capital
+                + final
+            )
+        ]
+
+    def pattern_countryWithISO(self, start, final, data, textFeatures):
+        iso = data["iso"]
+        if iso in textFeatures:
+            return []
+        return [(start + " (" + iso + ")" + final)]
+
+    def pattern_countryWithISO3(self, start, final, data, textFeatures):
+        iso3 = data["iso3"]
+        if iso3 in textFeatures:
+            return []
+        return [(start + " (" + iso3 + ")" + final)]
+
+    def pattern_countryWithISOAndISO3(self, start, final, data, textFeatures):
+        iso = data["iso"]
+        iso3 = data["iso3"]
+        if iso in textFeatures or iso3 in textFeatures:
+            return []
+        return [(start + " (" + iso + "/" + iso3 + ")" + final)]
+
+    def pattern_countryWithNeighbours(self, start, final, data, textFeatures):
+        neighbours = data["neighbours"]
+        if len(neighbours) == 0:
+            return []
+
+        actual = []
+        for n in neighbours:
+            name = self.countries[n]["name"]
+            if name not in textFeatures:
+                actual.append(name)
+
+        if len(actual) == 0:
+            return []
+
+        n1 = ", ".join(actual[: len(actual) - 1])
+        n2 = actual[len(actual) - 1]
+        if len(n1) > 0:
+            n2 = " and " + n2
+        return [(start + ", a country neighbouring " + n1 + n2 + final)]
+
+    def pattern_countryWithPrimaryLanguage(self, start, final, data, textFeatures):
+        if len(data["languages"]) == 0:
+            return []
+        lang = data["languages"][0]
+        if lang not in self.locales:
+            lang = lang.split("-")[0].strip()
+        if lang not in self.locales:
+            return []
+
+        lang = self.locales[lang]["language"]
+        return [
+            (start + ", a country speaking primarily the " + lang + " language" + final)
+        ]
+
+    def pattern_countryWithNumberOfLanguages(self, start, final, data, textFeatures):
+        num_languages = len(data["languages"])
+        if num_languages == 0:
+            return []
+        s = ""
+        if num_languages > 1:
+            s = "s"
+        return [
+            (
+                start
+                + ", a country with "
+                + str(num_languages)
+                + " spoken language"
+                + s
+                + final
+            )
+        ]
+
+    def pattern_countryWithSpokenLanguages(self, start, final, data, textFeatures):
+        num_languages = len(data["languages"])
+        if num_languages == 0:
+            return []
+
+        actual = []
+        for lang in data["languages"]:
+            if lang not in self.locales:
+                lang = lang.split("-")[0].strip()
+            if lang in self.locales:
+                actual.append(self.locales[lang]["language"])
+
+        if len(actual) == 0:
+            return []
+
+        n1 = ", ".join(actual[: len(actual) - 1])
+        n2 = actual[len(actual) - 1]
+        if len(n1) > 0:
+            n2 = " and " + n2
+
+        num_languages = len(actual)
+        # if there is only 1 language this will be the primary language => no need to write it again
+        if num_languages < 2:
+            return []
+
+        s = ""
+        if num_languages > 1:
+            s = "s"
+
+        return [
+            (
+                start
+                + ", a country with the "
+                + str(num_languages)
+                + " most spoken language"
+                + s
+                + " being "
+                + n1
+                + n2
+                + final
+            )
+        ]
+
+    def generateCountry(self, feature, start, final, data, textFeatures):
+        result = []
+
+        result += self.pattern_countryInContinent(start, final, data, textFeatures)
+        result += self.pattern_countryInContinentWithPopulation(
+            start, final, data, textFeatures
+        )
+        result += self.pattern_countryWithCapital(start, final, data, textFeatures)
+        result += self.pattern_countryInContinentWithPopulationAndCapital(
+            start, final, data, textFeatures
+        )
+        result += self.pattern_countryWithISO(start, final, data, textFeatures)
+        result += self.pattern_countryWithISO3(start, final, data, textFeatures)
+        result += self.pattern_countryWithISOAndISO3(start, final, data, textFeatures)
+        result += self.pattern_countryWithNeighbours(start, final, data, textFeatures)
+        result += self.pattern_countryWithPrimaryLanguage(
+            start, final, data, textFeatures
+        )
+        result += self.pattern_countryWithNumberOfLanguages(
+            start, final, data, textFeatures
+        )
+        result += self.pattern_countryWithSpokenLanguages(
+            start, final, data, textFeatures
+        )
+
+        return result
+
+    def pattern_cityInCountry(self, start, final, data, textFeatures):
+        country = self.countries[data["country"]]["name"]
+        if country in textFeatures:
+            return []
+        return [(start + ", a city in " + country + final)]
+
+    def pattern_cityWithPopulation(self, start, final, data, textFeatures):
+        population = "{:,}".format(int(data["population"]))
+        # if country in textFeatures: return []
+        return [(start + ", a city with a population of " + population + final)]
+
+    def pattern_cityInCountryWithPopulation(self, start, final, data, textFeatures):
+        country = self.countries[data["country"]]["name"]
+        population = "{:,}".format(int(data["population"]))
+        if country in textFeatures:
+            return []
+        return [
+            (
+                start
+                + ", a city in "
+                + country
+                + ", with a population of "
+                + population
+                + " inhabitants"
+                + final
+            )
+        ]
+
+    def pattern_cityInContinent(self, start, final, data, textFeatures):
+        continent = self.continents[self.countries[data["country"]]["continent"]]
+        if continent in textFeatures:
+            return []
+        return [(start + ", a city in " + continent + final)]
+
+    def pattern_cityInContinentWithPopulation(self, start, final, data, textFeatures):
+        continent = self.continents[self.countries[data["country"]]["continent"]]
+        population = "{:,}".format(int(data["population"]))
+        if continent in textFeatures:
+            return []
+        return [
+            (
+                start
+                + ", a city in "
+                + continent
+                + ", with a population of "
+                + population
+                + " inhabitants"
+                + final
+            )
+        ]
+
+    def pattern_cityCapitalOfCountry(self, city, start, final, data, textFeatures):
+        if city not in self.capitals:
+            return []
+        country = self.capitals[city]["countryName"]
+        if country in textFeatures:
+            return []
+        return [(start + ", the capital of " + country + final)]
+
+    def generateCity(self, feature, start, final, data, textFeatures):
+        result = []
+
+        result += self.pattern_cityInCountry(start, final, data, textFeatures)
+        result += self.pattern_cityWithPopulation(start, final, data, textFeatures)
+        result += self.pattern_cityInCountryWithPopulation(
+            start, final, data, textFeatures
+        )
+        result += self.pattern_cityInContinent(start, final, data, textFeatures)
+        result += self.pattern_cityInContinentWithPopulation(
+            start, final, data, textFeatures
+        )
+        result += self.pattern_cityCapitalOfCountry(
+            feature, start, final, data, textFeatures
+        )
+
+        return result
 
     def generate(self, sentence: str):
         result = []
@@ -125,6 +448,10 @@ class GeoNamesTransformation(SentenceOperation):
             nltk.download("punkt")
             tokens = nltk.word_tokenize(sentence)
 
+        textFeatures = {}
+
+        # First extract the GeoNames features existing in text
+        # This will be used both for enhancing the text and for preventing inserting duplicates
         for i in range(len(tokens)):
             for j in reversed(range(1, min(6, len(tokens) - i + 1))):
                 s = " ".join(tokens[i : i + j])
@@ -132,103 +459,46 @@ class GeoNamesTransformation(SentenceOperation):
                     continue
 
                 if s in self.countriesName:
-                    data = self.countriesName[s]
-                    start = " ".join(tokens[0 : i + j])
-                    final = " ".join(tokens[i + j :]).strip()
-                    if len(final) > 0:
-                        final = ", " + final
+                    textFeatures[s] = {"from": i, "to": i + j, "type": "country"}
+                    break
+                elif s in self.cities:
+                    textFeatures[s] = {"from": i, "to": i + j, "type": "city"}
+                    break
+                elif s in self.continents:
+                    textFeatures[s] = {"from": i, "to": i + j, "type": "continent"}
+                    break
+                elif len(s) == 2 and s.isupper():
+                    textFeatures[s] = {"type": "iso"}
+                    break
+                elif len(s) == 3 and s.isupper():
+                    textFeatures[s] = {"type": "iso3"}
+                    break
 
-                    new_sent = (
-                        start
-                        + ", a country in "
-                        + self.continents[data["continent"]]
-                        + final
-                    )
-                    result.append(new_sent)
+        # Iterate through all identified features and perform text augmentation
+        for feature, featureData in textFeatures.items():
+            # Augment countries
+            if featureData["type"] == "country":
+                data = self.countriesName[feature]
+                start = " ".join(tokens[0 : featureData["to"]])
+                final = " ".join(tokens[featureData["to"] :]).strip()
+                if len(final) > 0:
+                    final = ", " + final
 
-                    new_sent = (
-                        start
-                        + ", a country in "
-                        + self.continents[data["continent"]]
-                        + ", with a population of "
-                        + "{:,}".format(int(data["population"]))
-                        + " inhabitants"
-                        + final
-                    )
-                    result.append(new_sent)
+                result += self.generateCountry(
+                    feature, start, final, data, textFeatures
+                )
 
-                    new_sent = (
-                        start + ", whose capital city is " + data["capital"] + final
-                    )
-                    result.append(new_sent)
+                continue
 
-                    new_sent = (
-                        start
-                        + ", a country in "
-                        + self.continents[data["continent"]]
-                        + ", with a population of "
-                        + "{:,}".format(int(data["population"]))
-                        + " inhabitants, with the capital at "
-                        + data["capital"]
-                        + final
-                    )
-                    result.append(new_sent)
+            # Augment cities
+            if featureData["type"] == "city":
+                data = self.cities[feature]
+                start = " ".join(tokens[0 : featureData["to"]])
+                final = " ".join(tokens[featureData["to"] :]).strip()
+                if len(final) > 0:
+                    final = ", " + final
 
-                    continue
-
-                if s in self.cities:
-                    # print("s=[%s]"%(s))
-                    data = self.cities[s]
-                    start = " ".join(tokens[0 : i + j])
-                    final = " ".join(tokens[i + j :]).strip()
-                    if len(final) > 0:
-                        final = ", " + final
-
-                    new_sent = (
-                        start
-                        + ", a city in "
-                        + self.countries[data["country"]]["name"]
-                        + final
-                    )
-                    result.append(new_sent)
-
-                    new_sent = (
-                        start
-                        + ", a city with a population of "
-                        + "{:,}".format(int(data["population"]))
-                        + final
-                    )
-                    result.append(new_sent)
-
-                    new_sent = (
-                        start
-                        + ", a city in "
-                        + self.countries[data["country"]]["name"]
-                        + ", with a population of "
-                        + "{:,}".format(int(data["population"]))
-                        + " inhabitants"
-                        + final
-                    )
-                    result.append(new_sent)
-
-                    new_sent = (
-                        start
-                        + ", a city in "
-                        + self.continents[self.countries[data["country"]]["continent"]]
-                        + final
-                    )
-                    result.append(new_sent)
-
-                    new_sent = (
-                        start
-                        + ", a city in "
-                        + self.continents[self.countries[data["country"]]["continent"]]
-                        + ", with a population of "
-                        + "{:,}".format(int(data["population"]))
-                        + " inhabitants"
-                        + final
-                    )
-                    result.append(new_sent)
+                result += self.generateCity(feature, start, final, data, textFeatures)
 
         return result
 
@@ -242,7 +512,7 @@ if __name__ == "__main__":
     from TestRunner import convert_to_snake_case
 
     tf = GeoNamesTransformation(max_outputs=3)
-    # sentence = "Bucharest is the largest city in Romania"
+    # sentence = "Bucharest is the largest city in Romania" # Antigua and Barbuda"
     # sentence = "Klaus Iohannis is the current president of Romania"
 
     # for x in tf.generate(sentence):
