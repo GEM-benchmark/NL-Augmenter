8c7e586adcf5e8fc6cf70be759dd4bc016de62cd
Abhilash Pal <abhilashpal8@gmail.com>
2021-07-23 20:53:05 +0200

    Adding VariableCharPerturbation

---
+++ transformations/variable_char_perturbation/README.md
@@ -0,0 +1,54 @@
+# Variable Character Perturbation 🦎 + 🎮 → 🐍
+This perturbation performes 5 way perturbations on non numeric words, either deletion insertion substitution swapping and duplication, with configurable probability and subset of perturbations to choose from.
+
+For example: Banana -> Banan | Banabna | Banama | Baanna | Bannana
+
+
+Author name: Abhilash Pal
+<br/>
+Author email: abhilash.pal@tum.de / abhilashpal8@gmail.com
+<br/>
+Author Affiliation: Technische Universität München
+
+## What type of a transformation is this?
+This transformation adds random noise to the input word. Based on a predefined probability, words of atleast length 3 are perturbed with either random insertion deletion substitution swapping or duplication. Users can set which of the transformations they want as well.
+Generated transformations display high similarity to the source sentences i.e. the code outputs highly precise generations. 
+
+
+
+## What tasks does it intend to benefit?
+This perturbation would benefit all tasks which have a sentence/paragraph/document as input like text classification, 
+text generation, etc. 
+
+```python evaluate.py -t VariableCharPerturbation -task TEXT_CLASSIFICATION```
+```model_name = "aychang/roberta-base-imdb"```
+
+The accuracy of a RoBERTa model (fine-tuned on IMDB) on a subset of IMDB sentiment dataset = 96% The accuracy of the same model on the perturbed set = 94%
+
+
+## Previous Work
+
+```bibtex
+@inproceedings{karpukhin-etal-2019-training,
+    title = "Training on Synthetic Noise Improves Robustness to Natural Noise in Machine Translation",
+    author = "Karpukhin, Vladimir  and
+      Levy, Omer  and
+      Eisenstein, Jacob  and
+      Ghazvininejad, Marjan",
+    booktitle = "Proceedings of the 5th Workshop on Noisy User-generated Text (W-NUT 2019)",
+    month = nov,
+    year = "2019",
+    address = "Hong Kong, China",
+    publisher = "Association for Computational Linguistics",
+    url = "https://aclanthology.org/D19-5506",
+    doi = "10.18653/v1/D19-5506",
+    pages = "42--47",
+    abstract = "Contemporary machine translation systems achieve greater coverage by applying subword models such as BPE and character-level CNNs, but these methods are highly sensitive to orthographical variations such as spelling mistakes. We show how training on a mild amount of random synthetic noise can dramatically improve robustness to these variations, without diminishing performance on clean text. We focus on translation performance on natural typos, and show that robustness to such noise can be achieved using a balanced diet of simple synthetic noises at training time, without access to the natural noise data or distribution.",
+}
+
+```
+
+
+## What are the limitations of this transformation?
+- If perturbation probability is kept very high, output sentences might be extremely perturbed, with perturbations on every word with len>3.
+
---
+++ transformations/variable_char_perturbation/transformation.py
@@ -0,0 +1,154 @@
+import itertools
+import random
+
+from typing import List
+
+from interfaces.SentenceOperation import SentenceOperation
+from tasks.TaskTypes import TaskType
+
+"""
+5 way Character Perturbation [ Swap, Delete, Insert, Duplicate, Substitute]
+Pass to Operations a list of booleans corresponding to which operations you want to be a part of final perturbation :
+1. Swap
+2. Delete
+3. Insert
+4. Duplicate
+5. Substitute
+
+Eg : Only Substitution : operation = [0,0,0,0,1]
+     Deletion + Substitution : operation = [0,1,0,0,1]
+
+"""
+
+
+
+class VariableCharPerturbation(SentenceOperation):
+    tasks = [TaskType.TEXT_CLASSIFICATION, TaskType.TEXT_TO_TEXT_GENERATION, TaskType.TEXT_TAGGING]
+
+    languages = ["en"]
+
+    def __init__(self, seed: int = 0, max_outputs: int = 1, probability: float = 0.2,operations: list = [1,1,1,1,1]) -> None:
+        super().__init__(seed=seed, max_outputs=max_outputs)
+        self.probability = probability
+        self.operations = operations
+        assert(sum(self.operations)!=0)
+
+
+    def swap(self,word,i,j):
+        """
+          Swap ith and jth position in word 
+        """
+        word_ = ""
+        for x in range(len(word)):
+            if x == i:
+                word_+=word[j]
+            elif x == j:
+                word_+=word[i]
+            else:  
+                word_+=word[x]
+        return word_
+
+    def insert(self,word,i,x):
+        """
+          Insert x at ith position in word 
+        """
+        return word[:i]+x+word[i:]
+
+    def delete(self,word,i):
+        """
+          Delete ith position in word 
+        """
+        return word[:i]+word[i+1:]
+
+    def substitute(self,word,i,x):
+        """
+          Substitute x at ith position in word
+        """
+        string_list = list(word)
+        string_list[i] = x
+        return "".join(string_list)
+
+    def duplicate(self,word,i):
+        """
+          Duplicate ith position in word
+        """
+        d = word[i]
+        return word[:i]+d+word[i:]
+
+    def generate(self, sentence:str) -> List[str]:
+        random.seed(self.seed)
+        perturbed_texts = []
+        # Perturb the input sentence max_output times
+        for _ in itertools.repeat(None, self.max_outputs):
+            new = []
+            for i in sentence.split():
+              if self.probability>random.uniform(0, 1) and len(i)>3 and i.isalpha(): # enter perturbation loop
+                ch = []
+                for j in range(len(self.operations)):
+                    if self.operations[j]:
+                        ch.append(j+1)
+
+                choice = random.choice(ch)
+                if choice == 1: # swap
+                  swap_point = random.randint(0,len(i)-1)
+                  if swap_point == 0:
+                    swap_target = 1
+                  elif swap_point == len(i)-1:
+                    swap_target = swap_point - 1
+                  else:
+                    seqs = [swap_point - 1,swap_point+1]
+                    swap_target = random.choice(seqs)
+                  swapped_word = self.swap(i,swap_point,swap_target)
+
+                  new.append(swapped_word)
+
+                elif choice == 2: # delete
+                  delpoint = random.randint(0,len(i)-1)
+                  del_word = self.delete(i,delpoint)
+                  new.append(del_word)
+
+                elif choice == 3: #insert
+                  ipoint = random.randint(0,len(i)-1)
+                  alphabets = "abcdefghijklmnopqrstuvwxyz"
+                  alphachoice = random.randint(0,len(alphabets)-1)
+                  i_word = self.insert(i,ipoint,alphabets[alphachoice])
+                  new.append(i_word)
+                
+                elif choice == 4: # duplicate
+                  dpoint = random.randint(0,len(i)-1)
+                  d_word = self.duplicate(i,dpoint)
+                  new.append(d_word)
+
+                else: # substitute
+                  spoint = random.randint(0,len(i)-1)
+                  alphabets = "abcdefghijklmnopqrstuvwxyz"
+                  alphachoice = random.randint(0,len(alphabets)-1)
+                  s_word = self.substitute(i,spoint,alphabets[alphachoice])
+                  new.append(s_word)
+
+                
+
+              else:
+                new.append(i)
+              
+            perturbed_texts.append(" ".join(new))
+
+        return perturbed_texts
+
+if __name__ == '__main__':
+    import json
+    from TestRunner import convert_to_snake_case
+    tf = CharPerturbation(max_outputs=3)
+    sentence = "Andrew finally returned the French book to Chris that I bought last week"
+    test_cases = []
+    for sentence in ["Andrew finally returned the French book to Chris that I bought last week",
+                     "Sentences with gapping, such as Paul likes coffee and Mary tea, lack an overt predicate to indicate the relation between two or more arguments.",
+                     "Alice in Wonderland is a 2010 American live-action/animated dark fantasy adventure film",
+                     "Ujjal Dev Dosanjh served as 33rd Premier of British Columbia from 2000 to 2001",
+                     "Neuroplasticity is a continuous processing allowing short-term, medium-term, and long-term remodeling of the neuronosynaptic organization."]:
+        test_cases.append({
+            "class": tf.name(),
+            "inputs": {"sentence": sentence}, "outputs": [{"sentence": o} for o in tf.generate(sentence)]}
+        )
+    json_file = {"type": convert_to_snake_case(tf.name()), "test_cases": test_cases}
+    print(json.dumps(json_file))
