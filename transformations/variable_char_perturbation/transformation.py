4c9744926cefa3cd9f9d5b41c6f5544b347b65b3
Abhilash Pal <abhilashpal8@gmail.com>
2021-07-23 20:53:50 +0200

    Update transformation.py

--- transformations/variable_char_perturbation/transformation.py
+++ transformations/variable_char_perturbation/transformation.py
@@ -135,20 +135,20 @@ class VariableCharPerturbation(SentenceOperation):
 
         return perturbed_texts
 
-if __name__ == '__main__':
-    import json
-    from TestRunner import convert_to_snake_case
-    tf = CharPerturbation(max_outputs=3)
-    sentence = "Andrew finally returned the French book to Chris that I bought last week"
-    test_cases = []
-    for sentence in ["Andrew finally returned the French book to Chris that I bought last week",
-                     "Sentences with gapping, such as Paul likes coffee and Mary tea, lack an overt predicate to indicate the relation between two or more arguments.",
-                     "Alice in Wonderland is a 2010 American live-action/animated dark fantasy adventure film",
-                     "Ujjal Dev Dosanjh served as 33rd Premier of British Columbia from 2000 to 2001",
-                     "Neuroplasticity is a continuous processing allowing short-term, medium-term, and long-term remodeling of the neuronosynaptic organization."]:
-        test_cases.append({
-            "class": tf.name(),
-            "inputs": {"sentence": sentence}, "outputs": [{"sentence": o} for o in tf.generate(sentence)]}
-        )
-    json_file = {"type": convert_to_snake_case(tf.name()), "test_cases": test_cases}
-    print(json.dumps(json_file))
+# if __name__ == '__main__':
+#     import json
+#     from TestRunner import convert_to_snake_case
+#     tf = CharPerturbation(max_outputs=3)
+#     sentence = "Andrew finally returned the French book to Chris that I bought last week"
+#     test_cases = []
+#     for sentence in ["Andrew finally returned the French book to Chris that I bought last week",
+#                      "Sentences with gapping, such as Paul likes coffee and Mary tea, lack an overt predicate to indicate the relation between two or more arguments.",
+#                      "Alice in Wonderland is a 2010 American live-action/animated dark fantasy adventure film",
+#                      "Ujjal Dev Dosanjh served as 33rd Premier of British Columbia from 2000 to 2001",
+#                      "Neuroplasticity is a continuous processing allowing short-term, medium-term, and long-term remodeling of the neuronosynaptic organization."]:
+#         test_cases.append({
+#             "class": tf.name(),
+#             "inputs": {"sentence": sentence}, "outputs": [{"sentence": o} for o in tf.generate(sentence)]}
+#         )
+#     json_file = {"type": convert_to_snake_case(tf.name()), "test_cases": test_cases}
+#     print(json.dumps(json_file))
