691bc66196f0778d33b9ea09de56ec534224e7f8
Thomas Dopierre <thomas.dopierre@protonmail.com>
2021-07-23 18:45:13 +0200

    BLACK formatting

--- transformations/protaugment_diverse_paraphrase/transformation.py
+++ transformations/protaugment_diverse_paraphrase/transformation.py
@@ -1,18 +1,18 @@
-import itertools
+import logging
 import random
-from typing import Union, List, Dict, Callable
-
-from interfaces.SentenceOperation import SentenceOperation
-from tasks.TaskTypes import TaskType
+from typing import Callable, Dict, List, Union
 
 import numpy as np
 import torch
 from sacrebleu import sentence_bleu
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
-import logging
+from interfaces.SentenceOperation import SentenceOperation
+from tasks.TaskTypes import TaskType
 
-logging.basicConfig(format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+logging.basicConfig(
+    format="%(asctime)s | %(name)s | %(levelname)s | %(message)s"
+)
 logger = logging.getLogger(name="protaugment_diverse_paraphrase")
 
 """
@@ -37,10 +37,10 @@ def bleu_score(src: str, dst: str):
 
 
 def filter_generated_texts_with_distance_metric(
-        src: str,
-        texts: List[List[str]],
-        distance_metric_fn: Callable[[str, str], float],
-        lower_is_better: bool = True
+    src: str,
+    texts: List[List[str]],
+    distance_metric_fn: Callable[[str, str], float],
+    lower_is_better: bool = True,
 ):
     """
     This function aims at selecting one representative in each group of texts (`texts`). Such texts are compared to a source sentence (
@@ -54,8 +54,7 @@ def filter_generated_texts_with_distance_metric(
 
     """
     scores = [
-        [distance_metric_fn(src, text) for text in group]
-        for group in texts
+        [distance_metric_fn(src, text) for text in group] for group in texts
     ]
 
     if lower_is_better:
@@ -63,8 +62,7 @@ def filter_generated_texts_with_distance_metric(
     else:
         ranking_fn = np.argmax
     return [
-        group[ranking_fn(scores_)]
-        for group, scores_ in zip(texts, scores)
+        group[ranking_fn(scores_)] for group, scores_ in zip(texts, scores)
     ]
 
 
@@ -78,7 +76,9 @@ class UnigramForbidStrategy(ForbidStrategy):
         super().__init__()
         self.drop_chance = drop_chance
 
-    def bad_words_ids(self, input_ids: torch.Tensor, special_ids: List[str] = None) -> List[List[str]]:
+    def bad_words_ids(
+        self, input_ids: torch.Tensor, special_ids: List[str] = None
+    ) -> List[List[str]]:
         bad_words_ids = list()
         for row in input_ids.tolist():
             if special_ids:
@@ -97,31 +97,30 @@ class BigramForbidStrategy(ForbidStrategy):
     def __init__(self):
         super().__init__()
 
-    def bad_words_ids(self, input_ids: torch.Tensor, special_ids: List[str] = None) -> List[List[str]]:
+    def bad_words_ids(
+        self, input_ids: torch.Tensor, special_ids: List[str] = None
+    ) -> List[List[str]]:
         bad_words_ids = list()
         for row in input_ids.tolist():
             if special_ids:
                 row = [item for item in row if item not in special_ids]
             for i in range(0, len(row) - 1):
-                bad_words_ids.append(row[i:i + 2])
+                bad_words_ids.append(row[i: i + 2])
         return bad_words_ids
 
 
 class ProtaugmentDiverseParaphrase(SentenceOperation):
-    tasks = [
-        TaskType.TEXT_CLASSIFICATION,
-        TaskType.SENTIMENT_ANALYSIS
-    ]
+    tasks = [TaskType.TEXT_CLASSIFICATION, TaskType.SENTIMENT_ANALYSIS]
     languages = ["en"]
 
     def __init__(
-            self,
-            seed: int = 0,
-            num_beams: int = 20,
-            beam_group_size: int = 4,
-            diversity_penalty: float = 0.5,
-            device: Union[torch.device, str] = "auto",
-            forbid_strategy: Union[ForbidStrategy, str, None] = None
+        self,
+        seed: int = 0,
+        num_beams: int = 20,
+        beam_group_size: int = 4,
+        diversity_penalty: float = 0.5,
+        device: Union[torch.device, str] = "auto",
+        forbid_strategy: Union[ForbidStrategy, str, None] = None,
     ):
         # Random seed
         self.seed = seed
@@ -130,13 +129,19 @@ class ProtaugmentDiverseParaphrase(SentenceOperation):
         # Instancing device
         if type(device) == str:
             if device == "auto":
-                device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+                device = (
+                    torch.device("cuda")
+                    if torch.cuda.is_available()
+                    else torch.device("cpu")
+                )
             else:
                 device = torch.device(device)
         self.device = device
 
         # Instancing parameters
-        assert num_beams % beam_group_size == 0  # `num_beams` should be a multiple of `beam_group_size`
+        assert (
+            num_beams % beam_group_size == 0
+        )  # `num_beams` should be a multiple of `beam_group_size`
         self.num_return_sequences = self.num_beams = num_beams
         self.beam_group_size = beam_group_size
         self.num_beam_groups = self.num_beams // self.beam_group_size
@@ -155,20 +160,26 @@ class ProtaugmentDiverseParaphrase(SentenceOperation):
         self.forbid_strategy = forbid_strategy
 
         self.model_name = "tdopierre/ProtAugment-ParaphraseGenerator"
-        self.model = AutoModelForSeq2SeqLM.from_pretrained(self.model_name).to(self.device)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(self.model_name).to(
+            self.device
+        )
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
 
         super().__init__()
 
-    def prepare_batch(self, sentence: str) -> Dict[str, Union[torch.Tensor, List[List[str]]]]:
-        batch = self.tokenizer.prepare_seq2seq_batch(src_texts=[sentence], return_tensors="pt", max_length=512)
+    def prepare_batch(
+        self, sentence: str
+    ) -> Dict[str, Union[torch.Tensor, List[List[str]]]]:
+        batch = self.tokenizer.prepare_seq2seq_batch(
+            src_texts=[sentence], return_tensors="pt", max_length=512
+        )
         batch = {k: v.to(self.device) for k, v in batch.items()}
 
         # If a forbidding strategy exists, then add those forbidden words to the batch
         if self.forbid_strategy:
             bad_words_ids = self.forbid_strategy.bad_words_ids(
                 special_ids=self.tokenizer.all_special_ids,
-                input_ids=batch["input_ids"]
+                input_ids=batch["input_ids"],
             )
 
             if len(bad_words_ids):
@@ -185,17 +196,24 @@ class ProtaugmentDiverseParaphrase(SentenceOperation):
                 num_beams=self.num_beams,
                 num_beam_groups=self.beam_group_size,
                 diversity_penalty=self.diversity_penalty,
-                num_return_sequences=self.num_return_sequences
+                num_return_sequences=self.num_return_sequences,
             )
 
-        tgt_texts = self.tokenizer.batch_decode(preds.detach().cpu(), skip_special_tokens=True)
-        assert len(tgt_texts) == self.num_beams, f'#tgt texts {len(tgt_texts)} does not match num beams {self.num_beams}'
+        tgt_texts = self.tokenizer.batch_decode(
+            preds.detach().cpu(), skip_special_tokens=True
+        )
+        assert (
+            len(tgt_texts) == self.num_beams
+        ), f"#tgt texts {len(tgt_texts)} does not match num beams {self.num_beams}"
 
         filtered = filter_generated_texts_with_distance_metric(
-            texts=[tgt_texts[i:i + self.beam_group_size] for i in range(0, len(tgt_texts), self.beam_group_size)],
+            texts=[
+                tgt_texts[i: i + self.beam_group_size]
+                for i in range(0, len(tgt_texts), self.beam_group_size)
+            ],
             src=sentence,
             distance_metric_fn=bleu_score,
-            lower_is_better=True
+            lower_is_better=True,
         )
 
         return filtered
@@ -203,29 +221,44 @@ class ProtaugmentDiverseParaphrase(SentenceOperation):
 
 # Sample code to demonstrate usage. Can also assist in adding test cases.
 # You don't need to keep this code in your transformation.
-if __name__ == '__main__':
+if __name__ == "__main__":
     import json
+
     from TestRunner import convert_to_snake_case
 
     test_cases = []
 
     for arg_dict in (
-            {"forbid_strategy": "bigram"},
-            {"forbid_strategy": "unigram"},
+        {"forbid_strategy": "bigram"},
+        {"forbid_strategy": "unigram"},
     ):
         tf = ProtaugmentDiverseParaphrase(**arg_dict)
 
-        for sentence in ["Andrew finally returned the French book to Chris that I bought last week",
-                         "Sentences with gapping, such as Paul likes coffee and Mary tea, lack an overt predicate to indicate the relation between two or more arguments.",
-                         "Alice in Wonderland is a 2010 American live-action/animated dark fantasy adventure film",
-                         "Ujjal Dev Dosanjh served as 33rd Premier of British Columbia from 2000 to 2001",
-                         "Neuroplasticity is a continuous processing allowing short-term, medium-term, and long-term remodeling of the neuronosynaptic organization."]:
-            test_cases.append({
-                "class": tf.name(),
-                "args": arg_dict,
-                "inputs": {"sentence": sentence}, "outputs": [{"sentence": o} for o in tf.generate(sentence)]}
+        for sentence in [
+            "Andrew finally returned the French book to Chris that I bought last week",
+            "Sentences with gapping, such as Paul likes coffee and Mary tea, lack an overt predicate to indicate the relation between two or more arguments.",
+            "Alice in Wonderland is a 2010 American live-action/animated dark fantasy adventure film",
+            "Ujjal Dev Dosanjh served as 33rd Premier of British Columbia from 2000 to 2001",
+            "Neuroplasticity is a continuous processing allowing short-term, medium-term, and long-term remodeling of the neuronosynaptic organization.",
+        ]:
+            test_cases.append(
+                {
+                    "class": tf.name(),
+                    "args": arg_dict,
+                    "inputs": {"sentence": sentence},
+                    "outputs": [
+                        {"sentence": o} for o in tf.generate(sentence)
+                    ],
+                }
             )
-    json_file = {"type": convert_to_snake_case(tf.name()), "test_cases": test_cases}
-    with open("transformations/protaugment_diverse_paraphrase/test.json", "w", encoding="utf-8") as file:
+    json_file = {
+        "type": convert_to_snake_case(tf.name()),
+        "test_cases": test_cases,
+    }
+    with open(
+        "transformations/protaugment_diverse_paraphrase/test.json",
+        "w",
+        encoding="utf-8",
+    ) as file:
         json.dump(json_file, file, indent=2, ensure_ascii=False)
     print(json.dumps(json_file, indent=2, ensure_ascii=False))
