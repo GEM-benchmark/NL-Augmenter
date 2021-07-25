a226ac551bf2a86bcd8465e57d0d9ffe1d13edc3
timothy22000 <timothy22000@gmail.com>
2021-07-25 23:46:28 +0100

    Add initial basic version of chinese butter fingers perturbation

---
+++ transformations/chinese_butter_fingers_perturbation/transformation.py
@@ -0,0 +1,68 @@
+import itertools
+import random
+
+from interfaces.SentenceOperation import SentenceOperation
+from tasks.TaskTypes import TaskType
+
+"""
+Base Class for implementing the different input transformations a generation should be robust against.
+"""
+
+
+def butter_finger(text, prob=0.1, keyboard="pinyin", seed=0, max_outputs=1):
+    random.seed(seed)
+    key_approx = {}
+
+    if keyboard == "pinyin":
+        key_approx["妈"] = "马吗嘛骂"
+        key_approx["他"] = "塔踏塌嗒"
+        key_approx[" "] = " "
+    else:
+        print("Keyboard not supported.")
+
+    prob_of_typo = int(prob * 100)
+    perturbed_texts = []
+    for _ in itertools.repeat(None, max_outputs):
+        butter_text = ""
+        for letter in text:
+            lcletter = letter.lower()
+            if lcletter not in key_approx.keys():
+                new_letter = lcletter
+            else:
+                if random.choice(range(0, 100)) <= prob_of_typo:
+                    new_letter = random.choice(key_approx[lcletter])
+                else:
+                    new_letter = lcletter
+            # go back to original case
+            if not lcletter == letter:
+                new_letter = new_letter.upper()
+            butter_text += new_letter
+        perturbed_texts.append(butter_text)
+    return perturbed_texts
+
+
+"""
+Butter Finger implementation borrowed from https://github.com/alexyorke/butter-fingers.
+"""
+
+
+class ChineseButterFingersPerturbation(SentenceOperation):
+    tasks = [
+        TaskType.TEXT_CLASSIFICATION,
+        TaskType.TEXT_TO_TEXT_GENERATION,
+        TaskType.TEXT_TAGGING,
+    ]
+    languages = ["en"]
+
+    def __init__(self, seed=0, max_outputs=1):
+        super().__init__(seed, max_outputs=max_outputs)
+
+    def generate(self, sentence: str):
+        perturbed_texts = butter_finger(
+            text=sentence,
+            prob=0.05,
+            seed=self.seed,
+            max_outputs=self.max_outputs,
+        )
+        return perturbed_texts
+
