cb19cc9685b7244224b9b40a03c894a483ea5513
timothy22000 <timothy22000@gmail.com>
2021-07-27 02:05:47 +0100

    Incorporate suggestions into transformations

--- transformations/chinese_butter_fingers_perturbation/transformation.py
+++ transformations/chinese_butter_fingers_perturbation/transformation.py
@@ -7,17 +7,16 @@ import os
 from interfaces.SentenceOperation import SentenceOperation
 from tasks.TaskTypes import TaskType
 
-def butter_finger(text, prob=0.2, keyboard="pinyin", seed=0, max_outputs=1):
+def chinese_butter_finger(text, chinese_character_database, prob=0.2, keyboard="pinyin", seed=0, max_outputs=1):
     random.seed(seed)
 
-    prob_of_typo = prob * 100
     perturbed_texts = []
     for _ in itertools.repeat(None, max_outputs):
         butter_text = ""
         for chinese_character in text:
-            similar_pinyins = get_characters_with_similar_pinyin(chinese_character, keyboard)
+            similar_pinyins = get_characters_with_similar_pinyin(chinese_character, chinese_character_database, keyboard)
 
-            if random.choice(range(0, 100)) <= prob_of_typo and similar_pinyins != "":
+            if random.random() <= prob and similar_pinyins != "":
                 new_chinese_character = random.choice(similar_pinyins)
             else:
                 new_chinese_character = chinese_character
@@ -26,8 +25,7 @@ def butter_finger(text, prob=0.2, keyboard="pinyin", seed=0, max_outputs=1):
         perturbed_texts.append(butter_text)
     return perturbed_texts
 
-def get_characters_with_similar_pinyin(chinese_character, keyboard):
-    chinese_character_database = load_chinese_character_data_into_memory()
+def get_characters_with_similar_pinyin(chinese_character, chinese_character_database, keyboard):
 
     pinyin_for_char_to_be_perturbed = ""
     if keyboard == "pinyin":
@@ -60,21 +58,22 @@ Chinese Characters Butter Fingers Perturbation
 class ChineseButterFingersPerturbation(SentenceOperation):
     tasks = [
         TaskType.TEXT_CLASSIFICATION,
-        TaskType.TEXT_TO_TEXT_GENERATION,
-        TaskType.TEXT_TAGGING,
+        TaskType.TEXT_TO_TEXT_GENERATION
     ]
-    languages = ["en"]
+    languages = ["zh"]
 
     def __init__(self, seed=0, max_outputs=1, prob=0.1):
         super().__init__(seed, max_outputs=max_outputs)
         self.prob = prob
+        self.chinese_character_database = load_chinese_character_data_into_memory()
 
     def generate(self, sentence: str):
-        perturbed_texts = butter_finger(
+        perturbed_texts = chinese_butter_finger(
             text=sentence,
             prob=self.prob,
             seed=self.seed,
             max_outputs=self.max_outputs,
+            chinese_character_database = load_chinese_character_data_into_memory()
         )
         return perturbed_texts
 
