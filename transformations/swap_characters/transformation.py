e0e0ea5af1b4ec510e8bba1de2a87f51c661dce9
Taylor Sorensen <taylorsorensen@Taylors-MacBook-Pro.local>
2021-07-28 11:55:28 -0600

    transformations/swap_characters/README.md

--- transformations/swap_characters/transformation.py
+++ transformations/swap_characters/transformation.py
@@ -9,34 +9,34 @@ class SwapCharactersPerturbation(SentenceOperation):
         TaskType.TEXT_TO_TEXT_GENERATION,
         TaskType.TEXT_TAGGING,
     ]
-    languages = ["en"]
+    languages = ["All"]
 
     def __init__(self, seed=0):
         super().__init__(seed)
 
     def generate(self, sentence: str, prob=0.05):
         pertubed = self.swap_characters(text=sentence, prob=prob, seed=self.seed)
-        return pertubed
+        return [pertubed]
 
     def swap_characters(self, text, prob=0.05, seed=0):
         max_seed = 2 ** 32
-        # seed with hash so each text of same length gets different treatment
+        # seed with hash so each text of same length gets different treatment.
         np.random.seed((seed + sum([ord(c) for c in text])) % max_seed)
-        # np.random.seed((seed) % max_seed)
+        # np.random.seed((seed) % max_seed).
         # number of possible characters to swap.
         num_pairs = len(text) - 1
         # if no pairs, do nothing
         if num_pairs < 1:
             return text
-        # get indices to swap
+        # get indices to swap.
         indices_to_swap = np.argwhere(np.random.rand(num_pairs) < prob).reshape(-1)
-        # shuffle swapping order, may matter if there are adjacent swaps
+        # shuffle swapping order, may matter if there are adjacent swaps.
         np.random.shuffle(indices_to_swap)
-        # convert to list
+        # convert to list.
         text = list(text)
-        # swap
+        # swap.
         for index in indices_to_swap:
             text[index], text[index + 1] = text[index + 1], text[index]
-        # convert to string
+        # convert to string.
         text = "".join(text)
         return text
