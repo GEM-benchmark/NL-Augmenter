3733945b21f61ae6af57e48fc9d4ebaf841f8994
Anna Shvets <annashvets11@gmail.com>
2021-07-28 11:28:09 +0200

    Added sort_groups() method and code comments

--- filters/universal_bias/README.md
+++ filters/universal_bias/README.md
@@ -13,6 +13,7 @@ Affiliation: FabLab by Inetum in Paris
 ## Why is measuring performance on this split important?
 This filter can be used to measure the underrepresentation of a specific group regarding gender, racial, religion and other criteria.
 The filter aims to prevent the propagation of the historial biases usually present in large text datasets.
+Finally, it additionally allows to retrieve the corresponding groups, which might be useful for further manipulation of the dataset.
 
 ## Example of use
 ```
@@ -28,8 +29,8 @@ f = UniversalBiasFilter(target, test)
 
 f.filter(sentences)
 ```
-Which returns `True`, as the number of sentences tagged as "target" is less than the number of sentences tagged as "test".
-You can also check the exact number by calling the `count_groups()` static method on the instance of the class and providing it with the extracted tags with the `flag_sentences()` static method:
+Which returns `True`, as the number of sentences flagged as "target" is less than the number of sentences tagged as "test".
+You can check the exact number by calling the `count_groups()` static method on the instance of the class and providing it with the extracted tags with the `flag_sentences()` static method:
 ```
 flagged_sentences = f.flag_sentences(sentences, target, test)
 target, test, neutral = f.count_groups(flagged_sentences)
@@ -43,6 +44,20 @@ Which outputs:
 Target tagged sentences: 1
 Test tagged sentences: 2
 Neutral tagged sentences: 1
+```
+You can also retrieve the arrays of sentences from each group, by calling the `sort_groups()` method on the instance of th class, providing it with flags, extracted by `flag_sentences()` method:
+```
+flagged_sentences = f.flag_sentences(sentences, target, test)
+target_group, test_group, neutral_group = f.sort_groups(flagged_sentences)
+print("This is a target group:", target_group)
+print("This is a test group:", test_group)
+print("This is a neutral group:", neutral_group)
+```
+Which outputs:
+```
+This is a target group: ['She is going to program']
+This is a test group: ['He is going to make a cake.', 'He agreed to help me']
+This is a neutral group: ['Nobody likes washing dishes']
 ```
 
 ## What are the limitations of this filter?
--- filters/universal_bias/filter.py
+++ filters/universal_bias/filter.py
@@ -12,9 +12,18 @@ class UniversalBiasFilter(SentenceOperation):
 
     @staticmethod
     def flag_sentences(sentences, target, test):
+        """
+        flag sentences as belonging to the target, test or neutral groups
+        :param sentences: sentences array
+        :param target: array of keywords, describing potentially underrepresented population group
+        :param test: array of keywords, describing dominating group 
+        :return: array of objects, containing the analysed sentence along with three flags, one of which is set to True
+        """
         flagged_sentences = []
 
-        for i, sentence in enumerate(sentences):
+        for sentence in sentences:
+            
+            # Initialize the variables
             target_flag = False
             test_flag = False
             union_flag = False
@@ -22,27 +31,38 @@ class UniversalBiasFilter(SentenceOperation):
 
             intersection_target = set()
             intersection_test = set()
+
+            # Lowercase and split the words in the sentence to find the intersection with the target array of keywords
             intersection_target = set(sentence.lower().split()).intersection(
                 set(target)
             )
+            # Lowercase and split the words in the sentence to find the intersection with the test array of keywords
             intersection_test = set(sentence.lower().split()).intersection(
                 set(test)
             )
 
+            # If the intersection occured, the intersection_target and intersection_test will contain at least one common keyword
+            # use this intersection information to get the value for the corresponding flags
             target_flag = len(intersection_target) > 0
             test_flag = len(intersection_test) > 0
+
+            # In case the sentence contains the keywords from target and test groups, set a union_flag value
             union_flag = (
                 len(intersection_target) > 0 and len(intersection_test) > 0
             )
+
+            # If the sentence didn't contain the keywords neither from target, nor from test groups, set a neutral_flag value 
             neutral_flag = (
                 len(intersection_target) == 0 and len(intersection_test) == 0
             )
 
+            # Use the union_flag value to set the neutral_flag value, setting to False the target and test flags
             if union_flag is True:
                 target_flag = False
                 test_flag = False
                 neutral_flag = True
 
+            # Create the sentence object with the retrieved flag values
             sentence_object = {
                 "sentence": sentence,
                 "target_flag": target_flag,
@@ -50,12 +70,18 @@ class UniversalBiasFilter(SentenceOperation):
                 "neutral_flag": neutral_flag,
             }
 
+            # Append the object to the array we return
             flagged_sentences.append(sentence_object)
 
         return flagged_sentences
 
     @staticmethod
     def count_groups(flagged_corpus):
+        """
+        count the number of sentences in each of groups
+        :param flagged_corpus: array of flagged sentences
+        :return: 3 integer values, representing target, test and neutral groups respectively
+        """
         target_count = len(
             [
                 flag
@@ -76,13 +102,49 @@ class UniversalBiasFilter(SentenceOperation):
 
         return target_count, test_count, neutral_count
 
+    @staticmethod
+    def sort_groups(flagged_corpus):
+        """
+        sort the sentences in each of 3 groups
+        :param flagged_corpus: array of flagged sentences
+        :return: 3 arrays of strings, containing target, test and neutral groups respectively
+        """
+        target_group = [
+                flag.get("sentence")
+                for flag in flagged_corpus
+                if flag.get("target_flag") is True
+            ]
+        test_group = [
+                flag.get("sentence")
+                for flag in flagged_corpus
+                if flag.get("test_flag") is True
+            ]
+        neutral_group = [
+                flag.get("sentence")
+                for flag in flagged_corpus
+                if flag.get("neutral_flag") is True
+            ]
+    
+        return target_group, test_group, neutral_group
+
+
     def filter(self, sentences: []) -> bool:
+        """
+        filter the sentences to define whether the target group is underepresented
+        :param sentences: array of sentences
+        :return: boolean, which is set to True if the the target group is underepresented  
+        """
         biased = False
+        # Retrieve the flags for each of the sentences
         flagged_corpus = self.flag_sentences(sentences, self.target, self.test)
+
+        # Use the retrieved flags to count the number of sentences in each group
         target_count, test_count, neutral_count = self.count_groups(
             flagged_corpus
         )
 
+        # If the mumber of sentences in the target group is lower than in the test group, set bias to True
+        # Note, that the neutral group is not taken into account in this calculation
         if target_count < test_count:
             biased = True
         else:
