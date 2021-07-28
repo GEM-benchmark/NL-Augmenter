4855908f30e436cf4eae4823e92489aa2753e96e
Anna Shvets <annashvets11@gmail.com>
2021-07-28 20:22:54 +0200

    Added French language parameter, keywords, comments

--- filters/gender_bias/README.md
+++ filters/gender_bias/README.md
@@ -2,19 +2,78 @@
 
 ## What type of a filter is this?
 
-This filter filters a subset of text corpus to measure gender fairness in regards of a female gender representation.
-It is based on a pre-defined set personal pronouns, corresponding to the female and male genders accordingly.
-Two utility methods - flag_sentences() and count_genders() give supplementary information to the boolean value returned by the filter() method.
+This is a bilingual filter (for English and French languages), which filters a text corpus to measure gender fairness in regards of a female gender representation.
+It is based on a pre-defined set words, such as personal pronouns, words defining the relation and titles, corresponding to the female and male genders accordingly.
+Two utility methods - flag_sentences(), count_genders() and sort_groups() give supplementary information to the boolean value returned by the main filter() method.
 
 Author: Anna Shvets
-Company: Fablab by Inetum in Paris
+Company:
+Fablab by Inetum in Paris
+157 Boulevard MacDonald
+75019 Paris
+France
+
 
 ## Why is measuring performance on this split important?
 This filter can be used to define whether the female gender is sufficiently represented in a tested subset of sentences.
-Being currently implemented for English language, this filter is potentially language-agnostic, since does not rely on any external dependencies.
+Being currently implemented for English and French language, this filter is potentially language-agnostic, since does not rely on any external dependencies.
 
 ## Related Work
 While the problematics of the gender fairness is an active domain of research in NLP, the current code represents an original implementation.
 
+## Examples of use
+
+```
+f = GenderBiasFilter("en")
+sentences = [ "He is going to make a cake.",
+              "She is going to program",
+              "Nobody likes washing dishes",
+              "He agreed to help him" ]
+
+f.filter(sentences)
+```
+
+The above statement returns True, as the number of sentences flagged as "masculine" is less than the number of sentences tagged as "feminine". 
+You can check the exact number by calling the `count_genders()` static method on the instance of the class and providing it with the extracted flags with the `flag_sentences()` static method:
+```
+flagged_sentences = f.flag_sentences(sentences, "en")
+feminine, masculine, neutral = f.count_genders(flagged_sentences)
+print("Feminine flagged sentences:", feminine)
+print("Masculine flagged sentences:", masculine)
+print("Neutral flagged sentences:", neutral)
+```
+
+which outputs:
+```
+Feminine tagged sentences: 1
+Masculine tagged sentences: 2
+Neutral tagged sentences: 1
+```
+You can also retrieve the arrays of sentences from each group, by calling the `sort_groups()` method on the instance of th class, providing it with flags, extracted by `flag_sentences()` method:
+
+```
+flagged_sentences = f.flag_sentences(sentences, "en")
+feminine_group, masculine_group, neutral_group = f.sort_groups(flagged_sentences)
+print("This is a feminine group:", feminine_group)
+print("This is a masculine group:", masculine_group)
+print("This is a neutral group:", neutral_group)
+```
+
+which outputs:
+```
+This is a feminine group: ['She is going to program']
+This is a masculine group: ['He is going to make a cake.', 'He agreed to help him']
+This is a neutral group: ['Nobody likes washing dishes']
+```
+You are free to change the language at the class initialisation step:
+```
+sentences = [ "Il va preparer un gateau",
+              "Elle va créer un logiciel",
+              "Personne n'aime pas faire la vaiselle",
+              "Maman va à la conférence" ]
+
+f = GenderBiasFilter("fr")
+f.filter(sentences)
+```
 ## What are the limitations of this filter?
-The filter result is based exclusively on the personal pronouns, therefore the gender representation is calculated using only this parameter.
+The filter result is based on n-gram intersection counting approach, which assumes that the word should have the exact form as the internally defined keywords.
--- filters/gender_bias/filter.py
+++ filters/gender_bias/filter.py
@@ -4,54 +4,133 @@ from tasks.TaskTypes import TaskType
 
 class GenderBiasFilter(SentenceOperation):
     tasks = [TaskType.TEXT_TO_TEXT_GENERATION]
-    languages = ["en"]
+    languages = ["en", "fr"]
 
-    def __init__(self):
+    def __init__(self, language):
         super().__init__()
+        self.language = language
 
     @staticmethod
-    def flag_sentences(sentences):
+    def flag_sentences(sentences, language):
+        """
+        flag sentences as belonging to the feminine, masculine or neutral groups
+        :param sentences: sentences array
+        :param language: the string key representing the supported langage
+        :return: array of objects, each containing the analyzed sentence along with three flags
+        """
         flagged_sentences = []
-        feminine = ["she", "her", "hers"]
-        masculine = ["he", "him", "his"]
-        for i, sentence in enumerate(sentences):
+        
+        # Define the words, that represent feminine and masculine groups in both languages
+        if language == "en":
+            feminine_relation = ["woman", "auntie", "niece", "girl", "daughter", "girlfriend", 
+                                 "mistress", "sister", "bride", "wife", "mother", "mum", "mom", "mommy", 
+                                 "grandmother", "granny"]
+            feminine_relation_plural = ["women", "aunties", "nieces", "girls", "daughters", "girlfriends",
+                                 "mistresses", "sisters", "brides", "wives", "mothers", "mums", "moms", "mommies", 
+                                 "grandmothers", "grannies"]
+            masculine_relation = ["man", "uncle", "nephew","boy", "boyfriend", "master", "brother",
+                                  "groom", "bridegroom", "husband", "father", "dad", "daddy", "grandfather"]
+            masculine_relation_plural = ["men", "uncles", "nephews", "boys", "boyfriends", "masters", "brothers",
+                                  "grooms", "bridegrooms", "husbands", "fathers", "dads", "daddies", "grandfathers"]
+
+            feminine_titles = ["mrs", "ms", "miss", "mademoiselle", "fräulein", "madam", "lady", "gentlewoman", 
+                               "baronesse", "countess", "viscountess", "marquise", "duchess", "princess", "emperess", "queen", 
+                               "dame", "primadonna", "diva", "goodwife", "guidwife"]
+            masculine_titles = ["mr", "mister", "sir", "lord", "gentleman", 
+                                "baron", "count", "viscount", "marquess", "duke", "prince", "emperor", "king", "goodman"]
+
+            feminine = ["she", "her", "hers"] + feminine_relation + feminine_relation_plural + feminine_titles
+            masculine = ["he", "him", "his"] + masculine_relation + masculine_relation_plural + masculine_titles
+
+        elif language == "fr":
+            feminine_relation = ["femme", "meuf", "nana", "tante", "fille", "fillette", "gamine", "gonzesse", 
+                                 "amie", "pote", "compagne", "maîtresse", "amante", "soeur ", "épouse", "mariée", 
+                                 "mère", "maman", "daronne", "nièce", "tante", "grand-mère", "mamie"]
+            feminine_relation_plural = ["femmes", "meufs", "nanas", "tantes", "filles", "fillettes", "gamines", "gonzesses", 
+                                 "amies", "potes", "compagnes", "maîtresses", "amantes", "soeurs", "épouses", "mariées", 
+                                 "mères", "mamans", "daronnes", "nièces", "tantes", "grand-mères", "mamies"]
+            masculine_relation = ["homme", "mec", "oncle", "neveu", "garçon", "fils", "gars", "gamin", 
+                                  "ami", "pot", "maître", "frère", "amant", "époux", "mari", "marié" 
+                                  "père", "papa", "daron", "grand-père", "papie"]
+            masculine_relation_plural = ["hommes", "mecs", "oncles", "neveux", "garçons", "fils", "gars", "gamins", 
+                                  "amis", "pots", "maîtres", "frères", "amants", "époux", "maris", "mariés" 
+                                  "pères", "papas", "darons", "grand-pères", "papies"]
+            feminine_titles = ["m", "mlle", "madame", "mademoiselle", "baronesse", "comtesse", "marquise", "duchesse", "princesse", "emperesse", "reine", "dame"]
+            masculine_titles = ["mr", "monsieur", "monseigneur", "baron", "compte", "marquis", "duc", "prince", "dauphin" "empereur", "roi"]
+
+            feminine = ["elle", "madame", "sienne"] + feminine_relation + feminine_relation_plural + feminine_titles
+            masculine = ["il", "monsieur", "sien"] + masculine_relation + masculine_relation_plural + masculine_titles
+
+        else:
+            raise NameError('The specified language is not supported or misformatted. Try "en" or "fr" as language arguments to the filter() method.')
+
+        assert len(sentences) > 0, "You must provide at least one sentence for the analysis. Check the content of your sentences array you pass to the filter() method."
+
+        for sentence in sentences:
+
+            # Initialize the variables
             feminine_flag = False
             masculin_flag = False
             union_flag = False
             neutral_flag = False
             intersection_feminine = set()
             intersection_masculine = set()
+
+            # Lowercase and split the words in the sentence to find the intersection with the feminine array of keywords
             intersection_feminine = set(sentence.lower().split()).intersection(
-                set(feminine)
-            )
-            intersection_masculine = set(
-                sentence.lower().split()
-            ).intersection(set(masculine))
+                    set(feminine)
+                )
+            
+            # Lowercase and split the words in the sentence to find the intersection with the masculine array of keywords
+            intersection_masculine = set(sentence.lower().split()).intersection(
+                    set(masculine))
+           
+            intersection_masculine = set(sentence.lower().split()).intersection(
+                set(masculine))
+            
+            # If the intersection occured, the intersection_feminine and intersection_masculine variables will contain at least one common keyword
+            # use this intersection information to get the value for the corresponding flags
             feminine_flag = len(intersection_feminine) > 0
             masculin_flag = len(intersection_masculine) > 0
+
+            # In case the sentence contains the keywords from feminine and masculine arrays, set a union_flag value
             union_flag = (
                 len(intersection_feminine) > 0
                 and len(intersection_masculine) > 0
             )
+
+            # If the sentence didn't contain the keywords neither from feminine, nor from masculine arrays, set a neutral_flag value
             neutral_flag = (
                 len(intersection_feminine) == 0
                 and len(intersection_masculine) == 0
             )
+
+            # Use the union_flag value to set the neutral_flag value, setting to False the feminine and masculine flags
             if union_flag is True:
                 feminine_flag = False
                 masculin_flag = False
                 neutral_flag = True
+
+            # Create the sentence object with the retrieved flag values
             sentence_object = {
                 "sentence": sentence,
                 "feminine_flag": feminine_flag,
                 "masculin_flag": masculin_flag,
                 "neutral_flag": neutral_flag,
             }
+
+            # Append the object to the array we return
             flagged_sentences.append(sentence_object)
+
         return flagged_sentences
 
     @staticmethod
     def count_genders(flagged_corpus):
+        """
+        count the number of sentences in each of groups
+        :param flagged_corpus: array of flagged sentences
+        :return: 3 integer values, representing feminine, masculine and neutral groups respectively
+        """
         feminine_count = len(
             [
                 flag
@@ -75,14 +154,47 @@ class GenderBiasFilter(SentenceOperation):
         )
         return feminine_count, masculine_count, neutral_count
 
+    @staticmethod
+    def sort_groups(flagged_corpus):
+        """
+        sort the sentences in each of 3 groups
+        :param flagged_corpus: array of flagged sentences
+        :return: 3 arrays of strings, containing feminine, masculine and neutral groups respectively
+        """
+        feminine_group = [
+                flag.get("sentence")
+                for flag in flagged_corpus
+                if flag.get("feminine_flag") is True
+            ]
+        masculine_group = [
+                flag.get("sentence")
+                for flag in flagged_corpus
+                if flag.get("masculine_flag") is True
+            ]
+        neutral_group = [
+                flag.get("sentence")
+                for flag in flagged_corpus
+                if flag.get("neutral_flag") is True
+            ]
+    
+        return feminine_group, masculine_group, neutral_group
+
     def filter(self, sentences: []) -> bool:
         biased = False
-        flagged_corpus = self.flag_sentences(sentences)
+
+        # Retrieve the flags for each of the sentences
+        flagged_corpus = self.flag_sentences(sentences, self.language)
+
+        # Use the retrieved flags to count the number of sentences in each group
         feminine_count, masculine_count, neutral_count = self.count_genders(
             flagged_corpus
         )
+
+        # If the mumber of sentences in the target group is lower than in the test group, set bias to True
+        # Note, that the neutral group is not taken into account in this calculation
         if feminine_count < masculine_count:
             biased = True
         else:
             biased = False
+
         return biased
