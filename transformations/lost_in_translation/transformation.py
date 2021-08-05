3d41b8ac8a4291d533cac5cf0ad79eb6124dd4bc
M. Yee <yeem@csail.mit.edu>
2021-08-05 00:04:53 -0400

    Transformation.py added

--- transformations/lost_in_translation/transformation.py
+++ transformations/lost_in_translation/transformation.py
@@ -7,9 +7,9 @@ class LostInTranslation(SentenceOperation):
   '''
   Repeated translation of input text
   '''
-  tasks = [TaskType.TEXT_CLASSIFICATION, TaskType.TEXT_TO_TEXT_GENERATION]
-  languages = ["en"]
-  heavy = True
+  #tasks = [TaskType.TEXT_CLASSIFICATION, TaskType.TEXT_TO_TEXT_GENERATION]
+  #languages = ["en"]
+  #heavy = True
 
   def __init__(self, seed=0,
                langs=['es', 'de', 'zh', 'fr', 'ru'],
@@ -88,23 +88,23 @@ class LostInTranslation(SentenceOperation):
 
 # Sample code to demonstrate usage. Can also assist in adding test cases.
 # You don't need to keep this code in your transformation.
-if __name__ == '__main__':
-    import json
-    from TestRunner import convert_to_snake_case
-
-    tf = LostInTranslation(max_outputs=3)
-    sentence = "Andrew finally returned the French book to Chris that I bought last week"
-    test_cases = []
-    for sentence in ["Andrew finally returned the French book to Chris that I bought last week",
-                     "Sentences with gapping, such as Paul likes coffee and Mary tea, lack an overt predicate to indicate the relation between two or more arguments.",
-                     "Alice in Wonderland is a 2010 American live-action/animated dark fantasy adventure film",
-                     "Ujjal Dev Dosanjh served as 33rd Premier of British Columbia from 2000 to 2001",
-                     "Neuroplasticity is a continuous processing allowing short-term, medium-term, and long-term remodeling of the neuronosynaptic organization."]:
-        test_cases.append({
-            "class": tf.name(),
-            "inputs": {"sentence": sentence}, "outputs": [{"sentence": o} for o in tf.generate(sentence)]}
-        )
-    json_file = {"type": convert_to_snake_case(tf.name()), "test_cases": test_cases}
-    #print(json.dumps(json_file))
-    with open('test.json','w') as outfile:
-        json.dump(json_file,outfile,indent=4)
+#if __name__ == '__main__':
+#    import json
+#    from TestRunner import convert_to_snake_case
+#
+#    tf = LostInTranslation(max_outputs=3)
+#    sentence = "Andrew finally returned the French book to Chris that I bought last week"
+#    test_cases = []
+#    for sentence in ["Andrew finally returned the French book to Chris that I bought last week",
+#                     "Sentences with gapping, such as Paul likes coffee and Mary tea, lack an overt predicate to indicate the relation between two or more arguments.",
+#                     "Alice in Wonderland is a 2010 American live-action/animated dark fantasy adventure film",
+#                     "Ujjal Dev Dosanjh served as 33rd Premier of British Columbia from 2000 to 2001",
+#                     "Neuroplasticity is a continuous processing allowing short-term, medium-term, and long-term remodeling of the neuronosynaptic organization."]:
+#        test_cases.append({
+#            "class": tf.name(),
+#            "inputs": {"sentence": sentence}, "outputs": [{"sentence": o} for o in tf.generate(sentence)]}
+#        )
+#    json_file = {"type": convert_to_snake_case(tf.name()), "test_cases": test_cases}
+#    #print(json.dumps(json_file))
+#    with open('test.json','w') as outfile:
+#        json.dump(json_file,outfile,indent=4)
