4925467b8790d8bde40fb1ba67b11c8338d4c4d6
Xinyi Wu <wuxinyi@umich.edu>
2021-08-12 20:30:05 -0700

    move loops from whitespace funtion to generate function

--- transformations/whitespace_perturbation/transformation.py
+++ transformations/whitespace_perturbation/transformation.py
@@ -5,21 +5,14 @@ from interfaces.SentenceOperation import SentenceOperation
 from tasks.TaskTypes import TaskType
 
 
-def whitespace(text, remove_prob=0.1, add_prob=0.05, seed=0, max_outputs=1):
-    random.seed(seed)
-    perturbed_texts = []
-    for _ in range(max_outputs):
-        perturbed_text = []
-        for char in text:
-            random_num = random.random()
-            if char.isspace() and random_num < remove_prob:
-                continue
-            perturbed_text.append(char)
-            if (not char.isspace()) and random_num < add_prob:
-                perturbed_text.append(' ')
+def whitespace(char, random_num, remove_prob=0.1, add_prob=0.05):
+    if char.isspace() and random_num < remove_prob:
+        return []
+    purturbed_char = [char]
+    if (not char.isspace()) and random_num < add_prob:
+        purturbed_char.append(' ')
 
-        perturbed_texts.append(''.join(perturbed_text))
-    return perturbed_texts
+    return purturbed_char
 
 
 class WhitespacePerturbation(SentenceOperation):
@@ -30,17 +23,24 @@ class WhitespacePerturbation(SentenceOperation):
     languages = [
         "ar", "ca", "cs", "da", "de", 
         "en", "eo", "es", "fi", "fr", 
-        "ga", "gl", "he", "hi", "id", 
-        "is", "it", "kn", "la", "lt", 
-        "mr", "ms", "no", "pa", "pl", 
-        "pt", "ro", "ru", "sd", "sk", 
-        "sl", "sv", "sw", "ta", "te", 
-        "uk", "ur", "vi" 
+        "ga", "gl", "gu", "he", "hi", 
+        "id", "is", "it", "kn", "la", 
+        "lt", "mr", "ms", "no", "pa", 
+        "pl", "pt", "ro", "ru", "sd", 
+        "sk", "sl", "sv", "sw", "ta", 
+        "te", "uk", "ur", "vi" 
     ]
 
     def __init__(self, seed=0, max_outputs=1):
         super().__init__(seed, max_outputs=max_outputs)
 
     def generate(self, sentence: str):
-        pertubed = whitespace(text=sentence, remove_prob=0.1, add_prob=0.05, seed=self.seed, max_outputs=self.max_outputs)
-        return pertubed
+        random.seed(self.seed)
+        perturbed_texts = []
+        for _ in range(self.max_outputs):
+            perturbed_text = []
+            for char in sentence:
+                random_num = random.random()
+                perturbed_text += whitespace(char, random_num, remove_prob=0.1, add_prob=0.05)
+            perturbed_texts.append(''.join(perturbed_text))
+        return perturbed_texts
