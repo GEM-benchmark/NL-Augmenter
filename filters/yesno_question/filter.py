6e4b31b34b1f31856edfdb5d641a8021058a0178
wwydmanski <wwydmanski@gmail.com>
2021-08-13 10:53:25 +0200

    fix: better handling of sentences with 'or'

--- filters/yesno_question/filter.py
+++ filters/yesno_question/filter.py
@@ -1,10 +1,10 @@
-import spacy
-from typing import Tuple, List
+from typing import List, Tuple
 
+import spacy
+
+from initialize import spacy_nlp
 from interfaces.QuestionAnswerOperation import QuestionAnswerOperation
 from tasks.TaskTypes import TaskType
-from typing import List
-from initialize import spacy_nlp
 
 """
 A filter on text length (number of tokens).
@@ -30,53 +30,53 @@ class YesNoQuestionFilter(QuestionAnswerOperation):
     ) -> List[Tuple[str, str, List[str]]]:
 
         if question.strip()[-1] != "?":
+            # It isn't a question
             return False
-
-        # if " or " in question:
-        #     return False
 
         doc = self.nlp(question)
         token = doc[0]
         if token.pos_ != "AUX":
+            # There is no auxiliary verb
             return False
 
-        token = doc[0]
+        # Everything between auxiliary and verb is the subject of the sentence
+        verb_position = [
+            i for i in range(len(doc)) if str(doc[i]) == token.head.text
+        ][0]
 
-        verb_position = [i for i in range(len(doc)) if str(doc[i]) == token.head.text][
-            0
-        ]
-        rest_of_sentence = [i for i in doc[verb_position:]]
-        print(rest_of_sentence)
+        rest_of_sentence = " ".join(map(str, doc[verb_position:]))
+        if "or" in rest_of_sentence:
+            # This is a question about an alternative
+            return False
 
         return True
 
 
-if __name__ == "__main__":
-    import json
-    from TestRunner import convert_to_snake_case
+# if __name__ == "__main__":
+#     import json
+#     from TestRunner import convert_to_snake_case
 
-    tf = YesNoQuestionFilter()
+#     tf = YesNoQuestionFilter()
 
-    test_cases = []
-    for i, sentence in enumerate(
-        [
-            "Wasn't she angry when you told her about the accident?",
-            "Have you got an identity card?",
-            "Would you rather drink tea or a coffee?",
-            "Would you like some soup?",
-            "Should you need something, I will be in my room",
-            "Can Mark or John do the dishes?",
-        ]
-    ):
-        res = tf.filter("", sentence, [])
-        print(res)
-        test_cases.append(
-            {
-                "class": tf.name(),
-                "inputs": {"context": "", "question": sentence, "answers": []},
-                "outputs": res,
-            }
-        )
+#     test_cases = []
+#     for i, sentence in enumerate(
+#         [
+#             "Wasn't she angry when you told her about the accident?",
+#             "Have you got an identity card?",
+#             "Would you rather drink tea or a coffee?",
+#             "Would you like some soup?",
+#             "Should you need something, I will be in my room",
+#             "Can Mark or John do the dishes?",
+#         ]
+#     ):
+#         res = tf.filter("", sentence, [])
+#         test_cases.append(
+#             {
+#                 "class": tf.name(),
+#                 "inputs": {"context": "", "question": sentence, "answers": []},
+#                 "outputs": res,
+#             }
+#         )
 
-    json_file = {"type": convert_to_snake_case(tf.name()), "test_cases": test_cases}
-    print(json.dumps(json_file))
+#     json_file = {"type": convert_to_snake_case(tf.name()), "test_cases": test_cases}
+#     print(json.dumps(json_file))
