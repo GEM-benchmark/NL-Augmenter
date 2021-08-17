5f720ee662eac190fdddf7abddfad46c4ed0af11
RyanGoslingsBugle <RyanGoslingsBugle@users.noreply.github.com>
2021-08-17 20:55:33 +0100

    Add full lang list/new test case

--- filters/lang/filter.py
+++ filters/lang/filter.py
@@ -6,7 +6,13 @@ from langid.langid import LanguageIdentifier, model
 
 class LanguageFilter(SentenceOperation):
     tasks = [TaskType.TEXT_CLASSIFICATION, TaskType.TEXT_TO_TEXT_GENERATION]
-    languages = ["en"]
+    languages = ['af', 'am', 'an', 'ar', 'as', 'az', 'be', 'bg', 'bn', 'br', 'bs', 'ca', 'cs', 'cy', 'da', 'de',
+                 'dz', 'el', 'en', 'eo', 'es', 'et', 'eu', 'fa', 'fi', 'fo', 'fr', 'ga', 'gl', 'gu', 'he', 'hi',
+                 'hr', 'ht', 'hu', 'hy', 'id', 'is', 'it', 'ja', 'jv', 'ka', 'kk', 'km', 'kn', 'ko', 'ku', 'ky',
+                 'la', 'lb', 'lo', 'lt', 'lv', 'mg', 'mk', 'ml', 'mn', 'mr', 'ms', 'mt', 'nb', 'ne', 'nl', 'nn',
+                 'no', 'oc', 'or', 'pa', 'pl', 'ps', 'pt', 'qu', 'ro', 'ru', 'rw', 'se', 'si', 'sk', 'sl', 'sq',
+                 'sr', 'sv', 'sw', 'ta', 'te', 'th', 'tl', 'tr', 'ug', 'uk', 'ur', 'vi', 'vo', 'wa', 'xh', 'zh',
+                 'zu']
 
     def __init__(self, lang: List[str] = None, min_prob: float = 0.5):
         super().__init__()
