d009b9e8ee130bf5bc0214748eaab4555c192e02
psoni <priyank.soni@ipsoft.com>
2021-08-17 18:18:53 +0530

    Added some other modification and corrections.

--- transformations/propbank_srl_roles/README.md
+++ transformations/propbank_srl_roles/README.md
@@ -1,15 +1,12 @@
 # SRL arguments exchange perturbation 🦎  + ⌨️ → 🐍
-This perturbation adds noise to all types of text sources (sentence, paragraph, etc.) proportional to noise erupting
-from keyboard typos making common spelling errors.
+This perturbation adds noise to all types of text sources (sentence, paragraph, etc.) proportional to number of arguments identified by model (SRL BERT).
 
 Author name: Priyank Soni
 Author email: priyank.soni@ipsoft.com
 Author Affiliation: Amelia, Ipsoft
 
 ## What type of a transformation is this?
-This transformation acts like a perturbation to test robustness. First we are getting semantic role labels with SRL BERT which is state of the art single model for English PropBank SRL.  Few letters picked at random are replaced with letters
-which are at keyboard positions near the source letter. Generated transformations display high similarity to the
-source sentences i.e. the code outputs highly precise generations.
+This transformation acts like a perturbation to test robustness. First we are getting semantic role labels with SRL BERT which is state of the art single model for English PropBank SRL.
 
 Then we apply following rules to modify sentence, so that semantically they remain correct:
 
@@ -43,6 +40,36 @@ Then we apply following rules to modify sentence, so that semantically they rema
 
  you will absolutely love [ARG1: the music] [ARG0: we] [ARGM-MOD: will] be [V: playing] [ARGM-TMP: tomorrow] ! --> you will absolutely love tomorrow the music we will be playing !
 
+6. if ARG0 and ARGM-PRP are both present and ARGM-PRP comes after ARG0, put ARGM-PRP just before ARG0.
+
+Example: We will be going to the office for future meetings
+
+[ARG0: We] [ARGM-MOD: will] be [V: going] [ARG4: to the office] [ARGM-PRP: for future meetings] . --> For future meetings, we will be going to the office.
+
+7. if ARG0 and ARGM-CAU are both present and ARGM-CAU comes after ARG0, put ARGM-CAU just before ARG0.
+
+Example: I am going home because it's raining.
+
+[ARG0: I] am [V: going] [ARG4: home] [ARGM-CAU: because it 's raining] . --> Because it's raining I am going home.
+
+8. if ARG0 and ARGM-GOL are both present and ARGM-GOL comes after ARG0, put ARGM-GOL just before ARG0.
+
+Example: He played the match for his brother.
+
+[ARG0: He] [V: played] [ARG1: the match] [ARGM-GOL: for his brother] . --> For his brother He played the match.
+
+9. if ARGM-TMP and ARGM-PRP both present, exchange them.
+
+Example: From 2023 we will be playing football for fitness.
+
+[ARGM-TMP: From 2023] [ARG0: we] [ARGM-MOD: will] be [V: playing] [ARG1: football] [ARGM-PRP: for fitness] . --> F or fitness we will be playing football from 2023.
+
+10 .if ARGM-TMP and ARGM-CAU both present, exchange them.
+
+Example: In June the team will not practice because of the rain.
+
+[ARGM-TMP: In June] [ARG0: the team] [ARGM-MOD: will] [ARGM-NEG: not] [V: practice] [ARGM-CAU: because of the rain] . --> Because of the rain the team will not practice In June.
+
 ## What tasks does it intend to benefit?
 This perturbation would benefit all tasks which have a sentence/paragraph/document as input like text classification,
 text generation, etc.
@@ -59,6 +86,49 @@ text generation, etc.
   journal={ArXiv},
   year={2019},
   volume={abs/1904.05255}
+}
+```
+3) We are referring this PropBank Annotation Guidelines document for details:
+```
+@article{bonial2012english,
+  title={English propbank annotation guidelines},
+  author={Bonial, Claire and Hwang, Jena and Bonn, Julia and Conger, Kathryn and Babko-Malaya, Olga and Palmer, Martha},
+  journal={Center for Computational Language and Education Research Institute of Cognitive Science University of Colorado at Boulder},
+  volume={48},
+  year={2012}
+}
+```
+
+4) These are 3 core propbank papers:
+
+```@inproceedings{kingsbury2002treebank,
+  title={From TreeBank to PropBank.},
+  author={Kingsbury, Paul R and Palmer, Martha},
+  booktitle={LREC},
+  pages={1989--1993},
+  year={2002},
+  organization={Citeseer}
+}
+```
+```
+@article{palmer2005proposition,
+  title={The proposition bank: An annotated corpus of semantic roles},
+  author={Palmer, Martha and Gildea, Daniel and Kingsbury, Paul},
+  journal={Computational linguistics},
+  volume={31},
+  number={1},
+  pages={71--106},
+  year={2005},
+  publisher={MIT press One Rogers Street, Cambridge, MA 02142-1209, USA journals-info~…}
+}
+```
+```
+@inproceedings{gildea2002necessity,
+  title={The necessity of parsing for predicate argument recognition},
+  author={Gildea, Daniel and Palmer, Martha},
+  booktitle={Proceedings of the 40th Annual Meeting of the Association for Computational Linguistics},
+  pages={239--246},
+  year={2002}
 }
 ```
 
--- transformations/propbank_srl_roles/findargs.py
+++ transformations/propbank_srl_roles/findargs.py
@@ -3,102 +3,113 @@ def perturb_text(s: str, sentence: str):
     desc = sentence
 
     # case1:
-    if desc.__contains__("[ARGM-COM:") and desc.__contains__("[ARGM-TMP:"):
-        start = desc.find("ARGM-TMP:") + 10
-        end = desc.find("]", start)
-        s1 = desc[start:end]
-
-        start = desc.find("ARGM-COM:") + 10
-        end = desc.find("]", start)
-        s2 = desc[start:end]
-
-        t = s.replace(s2, "xxxx")
-        u = t.replace(s1, s2)
-        s = u.replace("xxxx", s1)
-        perturbed_texts.append(s)
+    if desc.__contains__("ARGM-COM") and desc.__contains__("ARGM-TMP"):
+        perturbed_texts.append(exchange_args(s, desc, "ARGM-COM", "ARGM-TMP"))
 
     # case2:
     if (
-        desc.__contains__("[ARG0:")
-        and desc.__contains__("[ARGM-TMP:")
-        and desc.find("ARGM-TMP:") > desc.find("ARG0:")
+        desc.__contains__("ARG0")
+        and desc.__contains__("ARGM-TMP")
+        and desc.find("ARGM-TMP") > desc.find("ARG0")
     ):
-        start = desc.find("ARG0:") + 6
-        end = desc.find("]", start)
-        s1 = desc[start:end]
-
-        start = desc.find("ARGM-TMP:") + 10
-        end = desc.find("]", start)
-        s2 = desc[start:end]
-
-        # s1 -> s2 + s1
-        # s2 -> ""
-        t = s.replace(s2, "xxxx")
-        u = t.replace(s1, s2 + " " + s1)
-        s = u.replace("xxxx", "")
-        perturbed_texts.append(s)
+        perturbed_texts.append(agr2_before_arg1(s, desc, "ARG0", "ARGM-TMP"))
 
     # case3:
     if (
-        desc.__contains__("[ARG0:")
-        and desc.__contains__("[ARGM-LOC:")
-        and desc.find("ARGM-LOC:") > desc.find("ARG0:")
+        desc.__contains__("ARG0")
+        and desc.__contains__("ARGM-LOC")
+        and desc.find("ARGM-LOC") > desc.find("ARG0")
     ):
-        start = desc.find("ARG0:") + 6
-        end = desc.find("]", start)
-        s1 = desc[start:end]
-
-        start = desc.find("ARGM-LOC:") + 10
-        end = desc.find("]", start)
-        s2 = desc[start:end]
-
-        # s1 -> s2 + s1
-        # s2 -> ""
-        t = s.replace(s2, "xxxx")
-        u = t.replace(s1, s2 + " " + s1)
-        s = u.replace("xxxx", "")
-        perturbed_texts.append(s)
+        perturbed_texts.append(agr2_before_arg1(s, desc, "ARG0", "ARGM-LOC"))
 
     # case4:
     if (
-        desc.__contains__("[ARG1:")
-        and desc.__contains__("[ARGM-LOC:")
-        and desc.find("ARGM-LOC:") > desc.find("ARG1:")
+        desc.__contains__("ARG1")
+        and desc.__contains__("ARGM-LOC")
+        and desc.find("ARGM-LOC") > desc.find("ARG1")
     ):
-        start = desc.find("ARG1:") + 6
-        end = desc.find("]", start)
-        s1 = desc[start:end]
-
-        start = desc.find("ARGM-LOC:") + 10
-        end = desc.find("]", start)
-        s2 = desc[start:end]
-
-        # s1 -> s2 + s1
-        # s2 -> ""
-        t = s.replace(s2, "xxxx")
-        u = t.replace(s1, s2 + " " + s1)
-        s = u.replace("xxxx", "")
-        perturbed_texts.append(s)
+        perturbed_texts.append(agr2_before_arg1(s, desc, "ARG1", "ARGM-LOC"))
 
     # case5:
     if (
-        desc.__contains__("[ARG1:")
-        and desc.__contains__("[ARGM-TMP:")
-        and desc.find("ARGM-TMP:") > desc.find("ARG1:")
+        desc.__contains__("ARG1")
+        and desc.__contains__("ARGM-TMP")
+        and desc.find("ARGM-TMP") > desc.find("ARG1")
     ):
-        start = desc.find("ARG1:") + 6
-        end = desc.find("]", start)
-        s1 = desc[start:end]
+        perturbed_texts.append(agr2_before_arg1(s, desc, "ARG1", "ARGM-TMP"))
 
-        start = desc.find("ARGM-TMP:") + 10
-        end = desc.find("]", start)
-        s2 = desc[start:end]
+    # case6:
+    if (
+        desc.__contains__("ARG0")
+        and desc.__contains__("ARGM-PRP")
+        and desc.find("ARGM-PRP") > desc.find("ARG0")
+    ):
+        perturbed_texts.append(agr2_before_arg1(s, desc, "ARG1", "ARGM-TMP"))
 
-        # s1 -> s2 + s1
-        # s2 -> ""
-        t = s.replace(s2, "xxxx")
-        u = t.replace(s1, s2 + " " + s1)
-        s = u.replace("xxxx", "")
-        perturbed_texts.append(s)
+    # case7:
+    if (
+        desc.__contains__("ARG0")
+        and desc.__contains__("ARGM-CAU")
+        and desc.find("ARGM-CAU") > desc.find("ARG0")
+    ):
+        perturbed_texts.append(agr2_before_arg1(s, desc, "ARG1", "ARGM-CAU"))
+
+    # case8:
+    if (
+        desc.__contains__("ARG0")
+        and desc.__contains__("ARGM-GOL")
+        and desc.find("ARGM-GOL") > desc.find("ARG0")
+    ):
+        perturbed_texts.append(agr2_before_arg1(s, desc, "ARG1", "ARGM-GOL"))
+
+    # case9:
+    if desc.__contains__("ARGM-TMP") and desc.__contains__("ARGM-PRP"):
+        perturbed_texts.append(exchange_args(s, desc, "ARGM-TMP", "ARGM-PRP"))
+
+    # case10:
+    if desc.__contains__("ARGM-TMP") and desc.__contains__("ARGM-CAU"):
+        perturbed_texts.append(exchange_args(s, desc, "ARGM-TMP", "ARGM-CAU"))
 
     return perturbed_texts
+
+
+def exchange_args(s, desc, arg1, arg2):
+    start = desc.find(arg1) + len(arg1) + 2
+    end = desc.find("]", start)
+    s1 = desc[start:end]
+
+    start = desc.find(arg2) + len(arg2) + 2
+    end = desc.find("]", start)
+    s2 = desc[start:end]
+
+    # s1 <--> s2
+    t = s.replace(s2, "xxxx")
+    u = t.replace(s1, s2)
+    s = u.replace("xxxx", s1)
+
+    s = s.capitalize()
+
+    return s
+
+
+def agr2_before_arg1(s, desc, arg1, arg2):
+    start = desc.find(arg1) + len(arg1) + 2
+    end = desc.find("]", start)
+    s1 = desc[start:end]
+
+    start = desc.find(arg2) + len(arg2) + 2
+    end = desc.find("]", start)
+    s2 = desc[start:end]
+
+    # s1 -> s2 + s1
+    # s2 -> ""
+    t = s.replace(s2, "xxxx")
+    u = t.replace(s1, s2 + " " + s1)
+
+    wordlist = u.split()
+    newtext = [x for x in wordlist if x not in ["xxxx", "xxxx.", "xxxx,"]]
+    s = " ".join(newtext)
+
+    s = s.capitalize()
+
+    return s
