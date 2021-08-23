f40a8f002e866f4fb388e72355ef34efa720f8ec
Ashish Shrivastava <ashish3586@gmail.com>
2021-08-23 23:37:01 +0530

    changed transformation code, test.json file

--- transformations/add_hashtags/transformation.py
+++ transformations/add_hashtags/transformation.py
@@ -4,7 +4,7 @@ import random
 from spacy import load
 
 
-def extract_dep_nodes(dep_parse, be_class_verb, subj_obj_list):
+def extract_dep_nodes(dep_parse, be_class_verb):
     """method for extracting VERB, NSUBJ phrase and DOBJ phrase dependency nodes"""
     verb = ""
     nsubj_phrase = []
@@ -53,7 +53,7 @@ def extract_noun_chunks_hashtag(dep_parse, subj_obj_list):
 def extract_hashtags(sentence, nlp, be_class_verb, subj_obj_list):
     # method for gathering all hashtags
     dep_parse = nlp(sentence)
-    verb, nsubj, dobj = extract_dep_nodes(dep_parse, be_class_verb, subj_obj_list)
+    verb, nsubj, dobj = extract_dep_nodes(dep_parse, be_class_verb)
     hash_tag_list = []
     for dep_n in [verb, nsubj, dobj]:
         if(dep_n != ""):
@@ -62,40 +62,22 @@ def extract_hashtags(sentence, nlp, be_class_verb, subj_obj_list):
         hash_tag_list.append("#"+verb+dobj)
     noun_chunks_hashtags = extract_noun_chunks_hashtag(dep_parse, subj_obj_list)
     if noun_chunks_hashtags is not None:
-        hash_tag_list.extend(noun_chunks_hashtags) # merging both hashtag list
-        return verb, list(set(hash_tag_list))
+        for ht in noun_chunks_hashtags:
+            if ht not in hash_tag_list:
+                hash_tag_list.append(ht)
     return verb, hash_tag_list
 
 
 def get_hash_tags(sentence, be_class_verb, subj_obj_list, seed=0, max_outputs=1, nlp=None):
     """method for appending hashtags to sentence"""
     verb, hashtag_list = extract_hashtags(sentence, nlp, be_class_verb, subj_obj_list)
-    #random.seed(seed)
-    # num = random.randint(1, len(hashtag_list))
-    # print(num)
-    # selected_hastags = random.sample(hashtag_list, num)
-    # print(selected_hastags)
-    # trans_sentence = sentence + " ".join(selected_hastags)
-    # # if bool(random.getrandbits(1)):
-    # #     trans_sentence = sentence + "".join(selected_hastags)
-    # # else:
-    # #     trans_sentence = sentence + " ".join(selected_hastags)
-    # return trans_sentence
-
     transformation_list = []
-    #random.seed(0)
     for _ in range(max_outputs):
+        random.seed(0)
         num = random.randint(1, len(hashtag_list))
         selected_hastag = random.sample(hashtag_list, num)
-        print(hashtag_list[:5], "*****")
-        print(num)
-        print(selected_hastag)
         trans_sent = sentence + " ".join(selected_hastag)
         transformation_list.append(trans_sent)
-        # if bool(random.getrandbits(1)):
-        #     transformation_list.append(sentence + " " + " ".join(selected_hastag))
-        # else:
-        #     transformation_list.append(sentence + "".join(selected_hastag))
     return transformation_list
 
 
@@ -120,32 +102,32 @@ class HashtagGeneration(SentenceOperation):
         return transformed_sentences
 
 
-if __name__ == "__main__":
-    import json
-    from TestRunner import convert_to_snake_case
-
-    tf = HashtagGeneration()
-    test_cases = []
-    input_sent = ["I love domino's pizza .",
-                  #"Virat Kohli made a big hundred against Australian team .",
-                  #"Many people like T20 cricket these days .",
-                  #"Attention is all you need .",
-                  #"Natural Language Processing research is awesome ."
-                ]
-
-    # for i,sentence in enumerate(input_sent):
-    #     transformed_sentence = tf.generate(sentence)
-    #     test_cases.append({
-    #                 "class": tf.name(),
-    #                 "inputs": {"sentence": sentence},
-    #                 "outputs": [],}
-    #         )
-    #     for trans_sentence in transformed_sentence:
-    #         test_cases[i]["outputs"].append({"sentence":trans_sentence})
-    # json_file = {"type": convert_to_snake_case("add_hashtags"), "test_cases": test_cases}
-    #print(json.dumps(json_file))
-    for ip in input_sent:
-        random.seed(0)
-        print(ip)
-        res = tf.generate(ip)
-        print(res)
+# if __name__ == "__main__":
+#     import json
+#     from TestRunner import convert_to_snake_case
+#
+#     tf = HashtagGeneration()
+#     test_cases = []
+#     input_sent = ["I love domino's pizza .",
+#                   "Virat Kohli made a big hundred against Australian team .",
+#                   "Many people like T20 cricket these days .",
+#                   "Attention is all you need .",
+#                   "Natural Language Processing research is awesome ."
+#                 ]
+#
+#     for i,sentence in enumerate(input_sent):
+#         transformed_sentence = tf.generate(sentence)
+#         test_cases.append({
+#                     "class": tf.name(),
+#                     "inputs": {"sentence": sentence},
+#                     "outputs": [],}
+#             )
+#         for trans_sentence in transformed_sentence:
+#             test_cases[i]["outputs"].append({"sentence":trans_sentence})
+#     json_file = {"type": convert_to_snake_case("add_hashtags"), "test_cases": test_cases}
+#     print(json.dumps(json_file))
+#     # for ip in input_sent:
+#     #     #random.seed(0)
+#     #     print(ip)
+#     #     res = tf.generate(ip)
+#     #     print(res)
