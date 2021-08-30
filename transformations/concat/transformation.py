47388a36244893bd60245d976d02e1a9347f5db2
Kenton Murray <kenton.murray@gmail.com>
2021-08-30 17:27:03 -0400

    black formatted the language ids differently

--- transformations/concat/transformation.py
+++ transformations/concat/transformation.py
@@ -9,8 +9,8 @@ from tasks.TaskTypes import TaskType
 
 class Concat(SentenceAndTargetOperation):
     """
-    This method concatenates two random sentences to create data 
-    that has context diversity, length diversity, and 
+    This method concatenates two random sentences to create data
+    that has context diversity, length diversity, and
     (to a lesser extent) position shifting.
     Note that in Nguyen et al., 2021, concatenating consecutive
     and random sentences yielded the same performance gains. Here,
@@ -23,32 +23,92 @@ class Concat(SentenceAndTargetOperation):
 
     tasks = [TaskType.TEXT_CLASSIFICATION, TaskType.TEXT_TO_TEXT_GENERATION]
     languages = [
-        "ar", "ca", "cs", "da", "de", 
-        "en", "eo", "es", "fi", "fr", 
-        "ga", "gl", "gu", "he", "hi", 
-        "id", "is", "it", "kn", "la", 
-        "lt", "mr", "ms", "no", "pa", 
-        "pl", "pt", "ro", "ru", "sd", 
-        "sk", "sl", "sv", "sw", "ta", 
-        "te", "uk", "ur", "vi" 
+        "ar",
+        "ca",
+        "cs",
+        "da",
+        "de",
+        "en",
+        "eo",
+        "es",
+        "fi",
+        "fr",
+        "ga",
+        "gl",
+        "gu",
+        "he",
+        "hi",
+        "id",
+        "is",
+        "it",
+        "kn",
+        "la",
+        "lt",
+        "mr",
+        "ms",
+        "no",
+        "pa",
+        "pl",
+        "pt",
+        "ro",
+        "ru",
+        "sd",
+        "sk",
+        "sl",
+        "sv",
+        "sw",
+        "ta",
+        "te",
+        "uk",
+        "ur",
+        "vi",
     ]
     tgt_languages = [
-        "ar", "ca", "cs", "da", "de", 
-        "en", "eo", "es", "fi", "fr", 
-        "ga", "gl", "gu", "he", "hi", 
-        "id", "is", "it", "kn", "la", 
-        "lt", "mr", "ms", "no", "pa", 
-        "pl", "pt", "ro", "ru", "sd", 
-        "sk", "sl", "sv", "sw", "ta", 
-        "te", "uk", "ur", "vi" 
+        "ar",
+        "ca",
+        "cs",
+        "da",
+        "de",
+        "en",
+        "eo",
+        "es",
+        "fi",
+        "fr",
+        "ga",
+        "gl",
+        "gu",
+        "he",
+        "hi",
+        "id",
+        "is",
+        "it",
+        "kn",
+        "la",
+        "lt",
+        "mr",
+        "ms",
+        "no",
+        "pa",
+        "pl",
+        "pt",
+        "ro",
+        "ru",
+        "sd",
+        "sk",
+        "sl",
+        "sv",
+        "sw",
+        "ta",
+        "te",
+        "uk",
+        "ur",
+        "vi",
     ]
 
-    def __init__(
-        self, seed=0, max_outputs=1, last_source="", last_target=""
-    ):
+    def __init__(self, seed=0, max_outputs=1, last_source="", last_target=""):
         super().__init__(seed, max_outputs=max_outputs)
-        self.last_source=last_source
-        self.last_target=last_target
+        self.last_source = last_source
+        self.last_target = last_target
 
     def generate(self, sentence: str, target: str):
         perturbed_source = sentence + " " + self.last_source
@@ -56,10 +116,8 @@ class Concat(SentenceAndTargetOperation):
         self.last_source = sentence
         self.last_target = target
 
-
         if self.verbose:
             print(
                 f"Perturbed Input from {self.name()} : \nSource: {perturbed_source}\nLabel: {perturbed_target}"
             )
         return [(perturbed_source, perturbed_target)]
-
