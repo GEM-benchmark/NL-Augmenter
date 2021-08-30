87efe4e3da32a615c6d9aceed9a11db8cec74f38
notricenotsweet <87012101+notricenotsweet@users.noreply.github.com>
2021-08-30 14:35:09 +0200

    feat: Adding dyslexia_words_swap

---
+++ transformations/dyslexia_words_swap/transformation.py
@@ -0,0 +1,39 @@
+import os
+import json
+import spacy
+from interfaces.SentenceOperation import SentenceOperation
+from tasks.TaskTypes import TaskType
+
+
+class DyslexiaWordsSwap(SentenceOperation):
+
+    tasks = [TaskType.TEXT_CLASSIFICATION, TaskType.TEXT_TO_TEXT_GENERATION]
+    languages = ["en"]
+
+    def __init__(self):
+        self.nlp = spacy.load("en_core_web_sm")
+        with open(os.path.join(os.path.dirname(__file__), 'data.json'), "r") as infile:
+            data = json.load(infile)
+        self.swap_words = {k: v for dict in data.values() for k, v in dict.items()}
+        self.swap_words_2 = {v: k for k, v in self.swap_words.items()}
+
+    def generate(self, sentence: str):
+        end_idx = 0
+        new_sentence = ""
+        for word in self.nlp(sentence):
+            new_sentence += sentence[end_idx: word.idx]
+
+            key = word.text.lower()
+            if key in self.swap_words or key in self.swap_words_2:
+                if key in self.swap_words:
+                    new_word = self.swap_words[key]
+                if key in self.swap_words_2:
+                    new_word = self.swap_words_2[key]
+                new_sentence += new_word
+            else:
+                new_sentence += word.text
+
+            end_idx = word.idx + len(word.text)
+        new_sentence += sentence[end_idx:]
+
+        return [new_sentence]
