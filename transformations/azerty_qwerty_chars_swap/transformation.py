ce501ff4f60b02b08ac427528b8cde97cea89d73
notricenotsweet <87012101+notricenotsweet@users.noreply.github.com>
2021-08-30 14:40:55 +0200

    feat: azerty_qwerty_chars_swap

---
+++ transformations/azerty_qwerty_chars_swap/transformation.py
@@ -0,0 +1,27 @@
+import numpy as np
+from interfaces.SentenceOperation import SentenceOperation
+from tasks.TaskTypes import TaskType
+
+
+class AzertyQwertyCharsSwap(SentenceOperation):
+    tasks = [TaskType.TEXT_CLASSIFICATION, TaskType.TEXT_TO_TEXT_GENERATION]
+    languages = ["en"]
+
+    def __init__(self, seed=0, percent_swap=0.2):
+        super().__init__(seed)
+        self.percent_swap = percent_swap
+        self.swap_dict = {"q": "a", "w": "z", "a": "q", "z": "w"}
+
+    def generate(self, sentence: str):
+        np.random.seed(self.seed)
+        new_sentence = ""
+        for char in sentence:
+            if char.lower() in self.swap_dict and np.random.uniform() < self.percent_swap:
+                new_char = self.swap_dict[char.lower()]
+                if char.isupper():
+                    new_char = new_char.upper()
+                new_sentence += new_char
+            else:
+                new_sentence += char
+
+        return [new_sentence]
