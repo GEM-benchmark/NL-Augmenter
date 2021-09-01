beff00f8e4a9b55b77ae8fa960a0dce720a4e478
Sajant Anand <sajantanand@aol.com>
2021-09-01 11:25:59 -0700

    Working barebones version.

--- transformations/random_walk/README.md
+++ transformations/random_walk/README.md
@@ -1,15 +1,27 @@
-# Butter Fingers Perturbation 🦎  + ⌨️ → 🐍
-This perturbation adds noise to all types of text sources (sentence, paragraph, etc.) proportional to noise erupting 
-from keyboard typos making common spelling errors.
+# Random Walk using Masked-Languange Modeling
+This transformation performs a random walk on the original sentence by randomly masking a word and replacing it with a suggestion by the BERT languange model.
 
-Author name: Kaustubh Dhole
-Author email: __
-Author Affiliation: __
+Author names:
+ - Chandan Singh (chandan_singh@berkeley.edu, UC Berkeley)
+ - Jamie Simon (james.simon@berkeley.edu, UC Berkeley)
+ - Sajant Anand (sajant@berkeley.edu, UC Berkeley)
+ - Roy Rinberg (royrinberg@gmail.com, Columbia University)
+
+## Extras
+
+This transformation requires the 'bert-large-cased' pretrained model (~1 GB) from the Hugging Face Transformers library. Provided that the library is installed, this model will be installed the first time this transformation is ran.
 
 ## What type of a transformation is this?
-This transformation acts like a perturbation to test robustness. Few letters picked at random are replaced with letters 
-which are at keyboard positions near the source letter. Generated transformations display high similarity to the 
-source sentences i.e. the code outputs highly precise generations. 
+This transformation acts like a perturbation to test robustness and generate sentences with similar syntactic content. By randomly replacing words with their mostly likely replacements, as determined by a bidirectional model that incorporates context clues from prevous and later words, we hope to generate similar sentences that make grammatical sense. 
+
+## How it works
+At each step in the random walk, we randomly choose a word and replace it by the mask token recognized by BERT. Care is take to preserve punctuation where possible so that the generated sentence has the same punctuation as the original sentence. With a word masked, we run BERT on the sentence and perform a softmax on the output logits. Then we select the high probability replacement words for the masked token and use these to construct new sentences.
+
+The differences between original and generated sentences are controlled by two class initialization parameters, `steps` and `k`.
+ - `steps`: number of random walk steps to do
+ - `k`: number of high probability replacements for the masked word to consider
+
+This process generates $k^steps$ new sentences. We then randomly select a subset of these, as specified by `max_outputs`.
 
 ## What tasks does it intend to benefit?
 This perturbation would benefit all tasks which have a sentence/paragraph/document as input like text classification, 
@@ -25,46 +37,10 @@ The average bleu score of a distillbert model (fine-tuned on xsum) (model: "sshl
 on a subset (10%) of xsum test dataset = 14.9104
 The average bleu score of same model on the pertubed set = 11.9221
 
-## Previous Work
-1) Butter Finger implementation borrowed from this code https://github.com/alexyorke/butter-fingers
-
-2) There has also been some recent work in the contrast sets of the GEM Benchmark (ACL 2021):
-```bibtex
-@article{DBLP:journals/corr/abs-2102-01672,
-  title     = {The {GEM} Benchmark: Natural Language Generation, its Evaluation and
-               Metrics},
-  journal   = {CoRR},
-  volume    = {abs/2102.01672},
-  year      = {2021},
-  url       = {https://arxiv.org/abs/2102.01672},
-  archivePrefix = {arXiv},
-  eprint    = {2102.01672},
-  timestamp = {Tue, 16 Feb 2021 16:58:52 +0100},
-  biburl    = {https://dblp.org/rec/journals/corr/abs-2102-01672.bib},
-  bibsource = {dblp computer science bibliography, https://dblp.org}
-}
-```
-
-3) There has been some recent work in NoiseQA too:
-```bibtex
-@inproceedings{ravichander-etal-2021-noiseqa,
-    title = "{N}oise{QA}: Challenge Set Evaluation for User-Centric Question Answering",
-    author = "Ravichander, Abhilasha  and
-      Dalmia, Siddharth  and
-      Ryskina, Maria  and
-      Metze, Florian  and
-      Hovy, Eduard  and
-      Black, Alan W",
-    booktitle = "Proceedings of the 16th Conference of the European Chapter of the Association for Computational Linguistics: Main Volume",
-    month = apr,
-    year = "2021",
-    address = "Online",
-    publisher = "Association for Computational Linguistics",
-    url = "https://www.aclweb.org/anthology/2021.eacl-main.259",
-    pages = "2976--2992",
-    abstract = "When Question-Answering (QA) systems are deployed in the real world, users query them through a variety of interfaces, such as speaking to voice assistants, typing questions into a search engine, or even translating questions to languages supported by the QA system. While there has been significant community attention devoted to identifying correct answers in passages assuming a perfectly formed question, we show that components in the pipeline that precede an answering engine can introduce varied and considerable sources of error, and performance can degrade substantially based on these upstream noise sources even for powerful pre-trained QA models. We conclude that there is substantial room for progress before QA systems can be effectively deployed, highlight the need for QA evaluation to expand to consider real-world use, and hope that our findings will spur greater community interest in the issues that arise when our systems actually need to be of utility to humans.",
-}
-```
 ## What are the limitations of this transformation?
-The transformation's outputs are too simple to be used for data augmentation. Unlike a paraphraser, it is not capable of
- generating linguistically diverse text.
+
+This transformation can generate nonsensical words when the random walk has many steps (steps >~ number of words in sentence).
+
+## References
+1) Saketh Kotamraju, "How to use BERT from the Hugging face transformer library", https://towardsdatascience.com/how-to-use-bert-from-the-hugging-face-transformer-library-d373a22b0209
+
--- transformations/random_walk/transformation.py
+++ transformations/random_walk/transformation.py
@@ -1,5 +1,12 @@
 import itertools
 import random
+from transformers import BertTokenizer, BertForMaskedLM
+from torch.nn import functional as F
+import torch
+import numpy as np
+import re
+import copy
+import random
 
 from interfaces.SentenceOperation import SentenceOperation
 from tasks.TaskTypes import TaskType
@@ -8,99 +15,76 @@ from tasks.TaskTypes import TaskType
 Base Class for implementing the different input transformations a generation should be robust against.
 """
 
+def random_walk(original_text, steps, k, tokenizer, model):
+    sentences = []
+    old_sentences = [original_text]
+    for i in range(steps): # Do k steps of random walk procedure
+        sentences = []
+        for text in old_sentences:
+            text_split = re.split('[ ?.,!;"]', text)
+            splits = len(text_split)
+            index_to_mask = np.random.randint(splits)
+            while len(text_split[index_to_mask]) == 0:
+                index_to_mask = np.random.randint(splits)
+            word_to_mask = text_split[index_to_mask]
+            # print('Word to mask:', word_to_mask)
+            start_index = text.find(word_to_mask)
+            new_text = text[0:start_index] + tokenizer.mask_token + text[start_index + len(word_to_mask):]
 
-def butter_finger(text, prob=0.1, keyboard="querty", seed=0, max_outputs=1):
-    random.seed(seed)
-    key_approx = {}
+            inputs = tokenizer.encode_plus(new_text, return_tensors='pt')
+            index_to_mask = torch.where(inputs.input_ids[0] == tokenizer.mask_token_id)
+            outputs = model(**inputs)
+            logits = outputs.logits
+            softmax = F.softmax(logits, dim=-1)
+            mask_word = softmax[0, index_to_mask, :]
+            top_k = torch.topk(mask_word, k)[1][0]
 
-    if keyboard == "querty":
-        key_approx["q"] = "qwasedzx"
-        key_approx["w"] = "wqesadrfcx"
-        key_approx["e"] = "ewrsfdqazxcvgt"
-        key_approx["r"] = "retdgfwsxcvgt"
-        key_approx["t"] = "tryfhgedcvbnju"
-        key_approx["y"] = "ytugjhrfvbnji"
-        key_approx["u"] = "uyihkjtgbnmlo"
-        key_approx["i"] = "iuojlkyhnmlp"
-        key_approx["o"] = "oipklujm"
-        key_approx["p"] = "plo['ik"
+            for token in top_k:
+                word = tokenizer.decode([token])
+                new_sentence = new_text.replace(tokenizer.mask_token, word)
+                sentences.append(new_sentence)
 
-        key_approx["a"] = "aqszwxwdce"
-        key_approx["s"] = "swxadrfv"
-        key_approx["d"] = "decsfaqgbv"
-        key_approx["f"] = "fdgrvwsxyhn"
-        key_approx["g"] = "gtbfhedcyjn"
-        key_approx["h"] = "hyngjfrvkim"
-        key_approx["j"] = "jhknugtblom"
-        key_approx["k"] = "kjlinyhn"
-        key_approx["l"] = "lokmpujn"
-
-        key_approx["z"] = "zaxsvde"
-        key_approx["x"] = "xzcsdbvfrewq"
-        key_approx["c"] = "cxvdfzswergb"
-        key_approx["v"] = "vcfbgxdertyn"
-        key_approx["b"] = "bvnghcftyun"
-        key_approx["n"] = "nbmhjvgtuik"
-        key_approx["m"] = "mnkjloik"
-        key_approx[" "] = " "
-    else:
-        print("Keyboard not supported.")
-
-    prob_of_typo = int(prob * 100)
-    perturbed_texts = []
-    for _ in itertools.repeat(None, max_outputs):
-        butter_text = ""
-        for letter in text:
-            lcletter = letter.lower()
-            if lcletter not in key_approx.keys():
-                new_letter = lcletter
-            else:
-                if random.choice(range(0, 100)) <= prob_of_typo:
-                    new_letter = random.choice(key_approx[lcletter])
-                else:
-                    new_letter = lcletter
-            # go back to original case
-            if not lcletter == letter:
-                new_letter = new_letter.upper()
-            butter_text += new_letter
-        perturbed_texts.append(butter_text)
-    return perturbed_texts
+        old_sentences = copy.deepcopy(sentences)
+    assert len(sentences) == k**steps
+    return sentences
 
 
-"""
-Butter Finger implementation borrowed from https://github.com/alexyorke/butter-fingers.
-"""
 
 
-class ButterFingersPerturbation(SentenceOperation):
+class RandomWalk(SentenceOperation):
     tasks = [
-        TaskType.TEXT_CLASSIFICATION,
         TaskType.TEXT_TO_TEXT_GENERATION,
-        TaskType.TEXT_TAGGING,
     ]
     languages = ["en"]
 
-    def __init__(self, seed=0, max_outputs=1):
+    def __init__(self, seed=0, max_outputs=1, steps=5, k=2):
         super().__init__(seed, max_outputs=max_outputs)
+        self.tokenizer = BertTokenizer.from_pretrained('bert-large-cased')
+        self.model = BertForMaskedLM.from_pretrained('bert-large-cased')
+        self.max_outputs = max_outputs
+        self.steps = steps
+        self.k = k
 
     def generate(self, sentence: str):
-        perturbed_texts = butter_finger(
-            text=sentence,
-            prob=0.05,
-            seed=self.seed,
-            max_outputs=self.max_outputs,
+        perturbed_texts = random_walk(
+            original_text=sentence,
+            steps=self.steps,
+            k=self.k,
+            tokenizer=self.tokenizer,
+            model=self.model
         )
+        if len(perturbed_texts) > self.max_outputs:
+            perturbed_texts = random.sample(perturbed_texts, self.max_outputs)
         return perturbed_texts
 
 
-"""
 # Sample code to demonstrate usage. Can also assist in adding test cases.
 # You don't need to keep this code in your transformation.
 if __name__ == '__main__':
     import json
     from TestRunner import convert_to_snake_case
 
-    tf = ButterFingersPerturbation(max_outputs=3)
+    tf = RandomWalk(max_outputs=3, k=2, steps=5)
     sentence = "Andrew finally returned the French book to Chris that I bought last week"
     test_cases = []
     for sentence in ["Andrew finally returned the French book to Chris that I bought last week",
@@ -114,4 +98,6 @@ if __name__ == '__main__':
         )
     json_file = {"type": convert_to_snake_case(tf.name()), "test_cases": test_cases}
     print(json.dumps(json_file, indent=2))
-"""
+
+    with open('test.json', 'w') as f:
+        json.dump(json_file, f, indent=2)
