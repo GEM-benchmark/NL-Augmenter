7b15a9b510bce6a3866c0d3d7cd78c0c477cb69d
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2021-09-09 15:52:36 -0500

    Add per-word replace probability, max outputs.

--- transformations/pig_latin/transformation.py
+++ transformations/pig_latin/transformation.py
@@ -1,4 +1,5 @@
 import piglatin
+import random
 
 from interfaces.SentenceOperation import SentenceOperation
 from tasks.TaskTypes import TaskType
@@ -11,13 +12,20 @@ class PigLatin(SentenceOperation):
     ]
     languages = ["en"]
 
-    def __init__(self, seed=0, max_outputs=1):
+    def __init__(self, seed=0, max_outputs=1, replace_prob=1.0):
         super().__init__(seed, max_outputs=max_outputs)
+        self.replace_prob = replace_prob
 
     def generate(self, sentence: str):
-        output_sentence = piglatin.translate(sentence.lower())
-        piglatin_sentence = []
-        for word in output_sentence.split():
-            piglatin_sentence.append(word.replace('-', ''))
-        piglatin_sentence = ' '.join(piglatin_sentence)
-        return [piglatin_sentence]
+        piglatin_sentences = []
+        for _ in range(self.max_outputs):
+            piglatin_sentence = []
+            for word in sentence.lower().split():
+                if random.random() < self.replace_prob:
+                    new_word = piglatin.translate(word)
+                else:
+                    new_word = word
+                piglatin_sentence.append(new_word.replace('-', ''))
+            piglatin_sentence = ' '.join(piglatin_sentence)
+            piglatin_sentences.append(piglatin_sentence)
+        return piglatin_sentences
