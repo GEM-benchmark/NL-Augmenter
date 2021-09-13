4201b30c9f932c7903bf9a5addd2e4bdbe2d3412
William Soto <williamsotomartinez@gmail.com>
2021-09-13 16:25:15 +0200

    Moved spacy reinitialization to generate() function

--- transformations/auxiliary_negation_removal/transformation.py
+++ transformations/auxiliary_negation_removal/transformation.py
@@ -132,9 +132,12 @@ class SentenceAuxiliaryNegationRemoval(SentenceOperation):
 
     def __init__(self, seed=0, max_outputs=1):
         super().__init__(seed, max_outputs=max_outputs)
-        self.nlp = reinitialize_spacy()
+        self.nlp = spacy_nlp if spacy_nlp else spacy.load("en_core_web_sm")
 
     def generate(self, sentence: str):
+
+        # Reinitialize spacy
+        self.nlp = reinitialize_spacy()
 
         # Initialize Variables
         output_sentence = sentence
@@ -165,11 +168,14 @@ class PairAuxiliaryNegationRemoval(SentencePairOperation):
 
     def __init__(self, seed=0, max_outputs=3, pos_label="1", neg_label="0"):
         super().__init__(seed, max_outputs=max_outputs)
-        self.nlp = reinitialize_spacy()
+        self.nlp = spacy_nlp if spacy_nlp else spacy.load("en_core_web_sm")
         self.pos_label = pos_label
         self.neg_label = neg_label
 
     def generate(self, sentence1: str, sentence2: str, target: str):
+
+        # Reinitialize spacy
+        self.nlp = reinitialize_spacy()
 
         # Initialize Variables
         output_sentences = []
