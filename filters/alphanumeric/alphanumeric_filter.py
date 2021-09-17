ce30eca8328fa519058195cd2097eab2e348ee19
motiwari <mohittiwarinyc+github@gmail.com>
2021-09-17 16:09:51 -0500

    Removing unused imports and dead code in alphanumeric filter, renaming class

--- filters/alphanumeric/alphanumeric_filter.py
+++ filters/alphanumeric/alphanumeric_filter.py
@@ -1,16 +1,11 @@
-from interfaces.SentenceOperation import SentenceOperation
 from tasks.TaskTypes import TaskType
-from initialize import spacy_nlp
-import spacy
 
-
-class TextContainsKeywordsFilter(SentenceOperation):
+class AlphanumericFilter(SentenceOperation):
     tasks = [TaskType.TEXT_CLASSIFICATION, TaskType.TEXT_TO_TEXT_GENERATION]
     languages = ["en"]
 
     def __init__(self, keywords=None):
         super().__init__()
-        self.nlp = spacy_nlp if spacy_nlp else spacy.load("en_core_web_sm")
         self.punctuation = ['!', '.', '?', "'", '"', '(', ')', '-', ':', ';', ' ']
 
     def filter(self, sentence: str = None) -> bool:
