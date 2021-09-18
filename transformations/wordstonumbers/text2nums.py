72203ea80d597d9f30de7ff7a6500aec6bd64d98
motiwari <mohittiwarinyc+github@gmail.com>
2021-09-18 11:55:36 -0700

    Moving dictionaries to separate file

--- transformations/wordstonumbers/text2nums.py
+++ transformations/wordstonumbers/text2nums.py
@@ -4,67 +4,7 @@ Very loosely adapted from https://stackoverflow.com/questions/493174/is-there-a-
 
 import re
 
-units = {
-        "zero": "0'",
-        "a": "1", # e.g. "a million"
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9",
-}
-
-teens = {
-        "ten": "10",
-        "eleven": "11",
-        "twelve": "12",
-        "thirteen": "13",
-        "fourteen": "14",
-        "fifteen": "15",
-        "sixteen": "16",
-        "seventeen": "17",
-        "eighteen": "18",
-        "nineteen": "19",
-    }
-
-tens = {
-    "twenty": '2',
-    "thirty": '3',
-    "forty": '4',
-    "fifty": '5',
-    "sixty": '6',
-    "seventy": '7',
-    "eighty": '8',
-    "ninety": '9',
-}
-
-scales = {
-    "thousand": 3, # 10^3
-    "million": 6, # 10^6
-    "billion": 9, # 10^9
-    "trillion": 12, # 10^12
-    "quadrillion": 15, # 10^15
-    "quintillion": 18, # 10^18
-    "sextillion": 21, # 10^21
-    "septillion": 24, # 10^24
-    "octillion": 27, # 10^27
-    "nonillion": 30, # 10^30
-    "decillion": 33, # 10^33
-    "undecillion": 36, # 10^36
-    "dodecillion": 39, # 10^39
-    "tredecillion": 42, # 10^42
-    "quattuordecillion": 45, # 10^45
-    "quindecillion": 48, # 10^48
-    "sexdecillion": 51, # 10^51
-    "septendecillion": 54, # 10^54
-    "octodecillion": 57, # 10^57
-    "novemdecillion": 60, # 10^60
-    "vigintillion": 63, # 10^63
-}
+from words_to_numbers_constants import units, tens, teens, scales
 
 def period_rep(tokens, period_start_loc, period_end_loc):
     """
