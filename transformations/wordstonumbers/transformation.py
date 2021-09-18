8c27a97c9f8becc63eb18253ccc3d419906e42aa
motiwari <mohittiwarinyc+github@gmail.com>
2021-09-18 14:29:24 -0700

    Adding keywords

--- transformations/wordstonumbers/transformation.py
+++ transformations/wordstonumbers/transformation.py
@@ -9,6 +9,7 @@ from text2nums import *
 class WordsToNumbers(SentenceOperation):
     tasks = [TaskType.TEXT_CLASSIFICATION, TaskType.TEXT_TO_TEXT_GENERATION, TaskType.TEXT_TAGGING]
     languages = ["en"]
+    keywords = ["lexical", "rule-based", "written", "highly-meaning-preserving", "high-precision", "low-generations"]
 
     def __init__(self, seed: int = 0) -> None:
         super().__init__(seed=seed, max_outputs=max_outputs)
