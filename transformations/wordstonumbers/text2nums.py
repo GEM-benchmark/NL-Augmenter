e15bf988f22e6d5582253ca374340f1f158cf3bd
motiwari <mohittiwarinyc+github@gmail.com>
2021-09-18 11:49:25 -0700

    Adding comments throughout

--- transformations/wordstonumbers/text2nums.py
+++ transformations/wordstonumbers/text2nums.py
@@ -1,9 +1,5 @@
 import re
 
-import ipdb
-def f(): pass
-#ipdb.set_trace = f
-
 units = {
         "zero" : "0'",
         "a" : "1", # "a million"
@@ -67,29 +63,32 @@ scales = {
 }
 
 def period_rep(tokens, period_start_loc, period_end_loc):
-    # parse a "period" of the number corresponding to 3 digits
+    """
+    Parse a "period" of the number corresponding to 3 digits, given a sequence of tokens and the location of the period
+    in that sentence
+    """
     str_ = ''
 
     first_digit = '0'
     if 'hundred' in tokens[period_start_loc:period_end_loc]:
-        # The token before "hundred" must be the number of hundreds
+        # The token before "hundred" must be the "number of hundreds"
         hundred_idx = tokens.index('hundred')
-        first_digit = units[tokens[hundred_idx - 1]]  # Should raise KeyError if malformed input
-        period_start_loc += 2
+        first_digit = units[tokens[hundred_idx - 1]]  # Will raise KeyError if malformed input
+        period_start_loc += 2 # Now, only consider tokens after the "X hundred" in the sequence
     str_ += first_digit
 
     second_digit = '0'
     for t in tens:
         if t in tokens[period_start_loc:period_end_loc]:
             second_digit = tens[t]
-            period_start_loc += 1
+            period_start_loc += 1 # Now, only consider tokens after the tens quantifier in the sequence
     str_ += second_digit
 
     third_digit = '0'
     for u in units:
         if u in tokens[period_start_loc:period_end_loc]:
             third_digit = units[u]
-            period_start_loc += 1 # Though this is not used currently, leave it here for extensions like "ninety eight point six degrees"
+            period_start_loc += 1 # Though this is not used currently, leave it here for extensions like "one point six"
     str_ += third_digit
 
     # Handle the case of 11 - 19
@@ -99,12 +98,20 @@ def period_rep(tokens, period_start_loc, period_end_loc):
 
     return str_
 
-def is_token_numeric(token, commas=False):
-    if not commas:
-        return token != ',' and (token in units or token in tens or token in scales or token == 'hundred' or token in teens)
-    return token in units or token in tens or token in scales or token == 'hundred' or token in teens
+def is_token_numeric(token):
+    """
+    Decide if a given token is part of the number
+    """
+    return token != ',' and (token in units or token in tens or token in scales or token == 'hundred' or token in teens)
 
 def find_continugous_number_words(tokens):
+    """
+    Given all the tokens of a sentence, find all the phrases that correspond to words.
+    This is necessary because several "word numbers" may be present in a sentence, e.g.
+    'three hundred people went to twenty two events'
+
+    Returns a set of "word numbers" and their corresponding start and end indices in the original token sequence
+    """
     number_words = []
     start_idcs = []
     end_idcs = []
@@ -112,13 +119,13 @@ def find_continugous_number_words(tokens):
     t_idx = 0
     new_word = True
     while t_idx < len(tokens):
-        if is_token_numeric(tokens[t_idx]): # What about commas and hyphens and "and"?
-            if new_word:
+        if is_token_numeric(tokens[t_idx]): # BUG: What about commas and hyphens and "and"?
+            if new_word: # We've found a new "word number"
                 start_idx = t_idx
                 start_idcs.append(t_idx)
                 new_word = False
         else:
-            if not new_word:
+            if not new_word: # We just completed the "word number"
                 number_words.append(tokens[start_idx:t_idx])
                 end_idcs.append(t_idx)
             new_word = True
@@ -127,40 +134,52 @@ def find_continugous_number_words(tokens):
     return number_words, list(zip(start_idcs, end_idcs))
 
 def parse_number_word(number_tokens):
-    # If there's a hyphen, split
+    """
+    Given a sequence of tokens corresponding to a "word number", converts it to a decimal representation, e.g.
+    'Three thousand five hundred twelve' -> '3512'
+    """
     word_rep = ' '.join(number_tokens)
     word_rep = word_rep.replace('-', ' ')
     word_rep = word_rep.replace(' and ', ' ')
     tokens = re.split('( |,)', word_rep)
-    tokens = list(filter(lambda x: x != ' ' and x != '', tokens))
+    tokens = list(filter(lambda x: x != ' ' and x != '', tokens)) # Remove extraneous empty strings and spaces
 
     num_string = ''
-
     last_found_period = None
-    for period in list(scales)[::-1]:  # Do them backwards to look for biggest scale first. As of Python 3.6, for the CPython implementation of Python, dictionaries maintain insertion order by default.
+
+    # Search the possible period identifiers backwards to look for biggest scale first.
+    # As of Python 3.6, for the CPython implementation of Python, dictionaries maintain insertion order by default.
+    for period in list(scales)[::-1]:
         if period in tokens:
+
+            # We found a new period identifier and had an old one that wasn't the one immediately larger than it,
+            # so we need to pad the middle with zeros, e.g. in the number "one billion, one thousand one"
             if last_found_period is not None:
                 num_string += '0' * 3 * (list(scales).index(last_found_period) - list(scales).index(period) - 1)
 
             last_found_period = period
             period_end_loc = tokens.index(period)
 
-            # walk backwards to find comma or non-number word
+            # We need to find the tokens that correspond to the period under identification.
+            # Walk backwards to find comma or non-number word
             period_start_loc = period_end_loc - 1
             token = tokens[period_start_loc]
-            while is_token_numeric(token) and token not in scales and period_start_loc != 0: # Don't wrap around the string, walk back towards the last seen "scale" number like million, billion, etc.
+            # Walk back towards the last seen period identifier like million, billion, etc. and
+            # don't wrap back around around the string
+            while is_token_numeric(token) and token not in scales and period_start_loc != 0:
                 period_start_loc -= 1
                 token = tokens[period_start_loc]
 
             num_string += period_rep(tokens, period_start_loc, period_end_loc)
 
+    # Handle the corner cases like "one million and twelve"
     if last_found_period is not None and last_found_period != 'thousand':
         num_string += '0' * 3 * (list(scales).index(last_found_period))
 
     # Trim leading 0s
     num_string = num_string.lstrip('0')
 
-    # If the last word is not a scale, then we have a number less than one thousand
+    # If the last token is not a period identifier, then we have a number less than one thousand
     if last_found_period is None:
         num_string += period_rep(tokens, 0, len(tokens))
     elif tokens.index(last_found_period) != len(scales):
@@ -198,5 +217,5 @@ def text2int(sentence):
         output_tokens = original_tokens
     return ' '.join(output_tokens)
 
-
+# BUG: Capitalization
 print(text2int("one thousand three hundred people went to three million twelve stores and two billion one thousand stores"))
