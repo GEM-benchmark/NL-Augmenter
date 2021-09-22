57acbbea350d39910785af3ab6b7eaf2862b3a5e
mnamysl <m.namysl@gmail.com>
2021-09-22 18:53:11 +0200

    added the docstring with parameter details

--- test/helper.py
+++ test/helper.py
@@ -1,11 +1,16 @@
 
-import json, os
+import json
 from TestRunner import convert_to_snake_case
 
 
-def generate_test_cases(transformation, src_sentences=[], print_result=False, output_filename=""):
+def generate_test_cases(transformation, src_sentences=[], print_result=False, output_path=""):
     """
     Sample code to demonstrate usage that can also assist in adding test cases.
+
+    :param transformation: a transformation object.
+    :param src_sentences: a list of input sentences (if empty, the default sentences are used).
+    :param print_results: indicates whether to print the content of the output file to stdout.
+    :param output_path: the path to the output JSON file used to store the generated test cases.
     """
 
     test_cases = []
@@ -34,9 +39,7 @@ def generate_test_cases(transformation, src_sentences=[], print_result=False, ou
     if print_result:
         print(json.dumps(json_file, ensure_ascii=False))
 
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-
-    if len(output_filename) > 0:
-        with open(os.path.join(dir_path, output_filename), "w") as f:
+    if len(output_path) > 0:
+        with open(output_path, "w") as f:
             json.dump(json_file, f, indent=2, ensure_ascii=False)
 
