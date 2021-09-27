2ac61fb92d7eef49907bf944139167221bf37fb0
Vikas Raunak <vyraun@gmail.com>
2021-09-26 22:33:03 -0700

    adding keywords + languages + docstring

--- transformations/diacritic_removal/transformation.py
+++ transformations/diacritic_removal/transformation.py
@@ -3,9 +3,13 @@ from tasks.TaskTypes import TaskType
 import unicodedata
 
 class DiacriticRemoval(SentenceOperation):
+    """
+    Transforms an input sentence by removing any diacritic marks.
+    """
 
     tasks = [TaskType.TEXT_CLASSIFICATION, TaskType.TEXT_TO_TEXT_GENERATION]
-    languages = ["en"]
+    languages = ["en", "fr", "es", "it", "pt", "fi", "nl", "da", "cs", "hr", "bg", "be", "eu", "ru", "uk", "pl", "sv", "sk", "sl"]
+    keywords = ["diacritic", "language-agnostic"]
 
     def __init__(self):
 
