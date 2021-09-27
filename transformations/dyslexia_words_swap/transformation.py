14a5a307649be24b1c4ab5e3ba49510f2a318c25
notricenotsweet <manraglitch@protonmail.com>
2021-09-27 14:19:30 +0200

    fix: adding proposed changes.
    - use global spacy
    - docstrings
    - list in multiple lines
    - 'new_word' might be referenced before assignment

--- transformations/dyslexia_words_swap/transformation.py
+++ transformations/dyslexia_words_swap/transformation.py
@@ -3,18 +3,36 @@ import json
 import spacy
 from interfaces.SentenceOperation import SentenceOperation
 from tasks.TaskTypes import TaskType
+from initialize import spacy_nlp
 
 
 class DyslexiaWordsSwap(SentenceOperation):
+    """Altering some words with mistakes that are likely to happen in the context of dyslexia.
 
-    tasks = [TaskType.TEXT_CLASSIFICATION, TaskType.TEXT_TO_TEXT_GENERATION]
+    Args:
+        seed: initial seed. Defaults: 0.
+        max_outputs: maximum number of generated outputs. Defaults: 1.
+    """
+    tasks = [
+        TaskType.TEXT_CLASSIFICATION,
+        TaskType.TEXT_TO_TEXT_GENERATION
+    ]
     languages = ["en"]
-    keywords = ["lexical", "rule-based", "external-knowledge-based", "aural", "possible-meaning-alteration", "low-precision", "low-coverage", "low-generations"]
+    keywords = [
+        "lexical",
+        "rule-based",
+        "external-knowledge-based",
+        "aural",
+        "possible-meaning-alteration",
+        "low-precision",
+        "low-coverage",
+        "low-generations"
+    ]
 
     def __init__(self, seed=0, max_outputs=1):
         super().__init__(seed=seed, max_outputs=max_outputs)
 
-        self.nlp = spacy.load("en_core_web_sm")
+        self.nlp = spacy_nlp if spacy_nlp else spacy.load("en_core_web_sm")
         with open(os.path.join(os.path.dirname(__file__), 'data.json'), "r") as infile:
             data = json.load(infile)
         self.swap_words = {k: v for dict in data.values() for k, v in dict.items()}
@@ -26,15 +44,14 @@ class DyslexiaWordsSwap(SentenceOperation):
         for word in self.nlp(sentence):
             new_sentence += sentence[end_idx: word.idx]
 
+            new_word = word.text
             key = word.text.lower()
             if key in self.swap_words or key in self.swap_words_2:
                 if key in self.swap_words:
                     new_word = self.swap_words[key]
                 if key in self.swap_words_2:
                     new_word = self.swap_words_2[key]
-                new_sentence += new_word
-            else:
-                new_sentence += word.text
+            new_sentence += new_word
 
             end_idx = word.idx + len(word.text)
         new_sentence += sentence[end_idx:]
