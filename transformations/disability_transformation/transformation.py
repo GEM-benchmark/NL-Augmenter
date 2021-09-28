769dfa1c3f38674cd8c5d3cdb276b692529e831a
raft001 <20336460+raft001@users.noreply.github.com>
2021-09-29 03:49:54 +0500

    Update transformation.py

--- transformations/disability_transformation/transformation.py
+++ transformations/disability_transformation/transformation.py
@@ -1,154 +1,171 @@
-
-
-disability_names= {"blind":"person with a visual impairment ",
-                           "deformed": " person with a physical disability ",
-                           "handicapped":" person with a physical disability ",
-                           "cripple":" person with a physical disability ",
-                           "crippled":" person with a physical disability ",
-                           "paraplegic":" person with paraplegia ",
-                           "psychotic":" person with a psychotic condition ",
-                           "psycho":" person with a psychotic condition ",
-                           "quadriplegic":" person with quadriplegia ",
-                           "schizophrenic":" person with schizophrenia ",
-                           "vegetable":"  person in a vegetative state ",
-                           "mentally retarded":" person with a mental  disablity ",
-                           "senile ":" person with dementia ",
-                           "gimp":" person with a physical disability ",
-                           "spastic":"person with a physical disability ",
-                           "spaz":" person with a physical disability ",
-                           "lame ":" person with a physical disability ",
-                           "lunatic":" person with a mental disability ",
-                           "lunatics":" person with a mental disability ",
-                           "looney":" person with a mental disability ",
-                           "looney bin":" person with a mental disability ",
-                           "manic":" person with a psychological disability ",
-                           "mongoloid":" person with Down syndrome ",
-                           "mutant":" person with an uncommon genetic mutation ",
-                           "mutants": " people with an uncommon genetic mutation ",
-                           "wheelchairbound":" wheelchair user ",
-                           "dwarf":" short-statured person ",
-                           "midget":" short-statured person ",
-                           "deaf":" person with a hearing disability ",
-                           "mute":" person with a listening disability ",
-                           "dumb":" person with a mental and/or speech impairments ",
-                           "demented":" person with dementia ",
-                           "dotard":" old person with impaired intellect or physical disability ",
-                           "dotards":"old people with impaired intellect or physical disability ",
-                           "derp":" person with intellectual disabilities ",
-                           "imbecile":" person with intellectual disabilities ",
-                           "imbeciles":" people with intellectual disabilities ",
-                           "crazy":" person with a mental impairment ",
-                           "insane ":" person with a mental impairment ",
-                           "wacko":" person with a mental impairment ",
-                           "nuts":" person with a mental impairment ",
-                           "retard":" person with an intellectual  disablity ",
-                           "retards":" people with an intellectual  disablity ",
-                           "retarded":" person with an intellectual  disablity "
-                           }
 !python -m spacy download en_core_web_sm
-# POS TAGGER
+
 import spacy
+import string
 from spacy.lang.en.examples import sentences 
 nlp = spacy.load("en_core_web_sm")
+
+
+
+
+# A dict containing offensive words and their alternatives
+disability_names =  {"blind":"person or people with a visual impairment",
+                           "deformed": "person or people with a physical disability",
+                           "handicapped":"person or people with a physical disability",
+                           "cripple":"person with a physical disability",
+                           "crippled":"person or people with a physical disability",
+                           "paraplegic":"person or people with paraplegia",
+                           "psychotic":"person or people with a psychotic condition",
+                           "psycho":"person with a psychotic condition",
+                           "psychopath":"person with a psychotic condition",
+                           "quadriplegic":"person or people with quadriplegia",
+                           "schizophrenic":"person or people with schizophrenia",
+                           "vegetable":"person in a vegetative state",
+                           "bonkers":"person or people with a mental disability",
+                           "senile":"person or people with dementia",
+                           "gimp":"person with a physical disability",
+                           "spastic":"person with a physical disability",
+                           "spaz":"person with a physical disability",
+                           "lame":"person with a physical disability",
+                           "lunatic":"person with a mental disability",
+                           "lunatics":"people with a mental disability",
+                           "looney":"person with a mental disability",
+                           "manic":"person with a psychological disability",
+                           "mutant":"person with an uncommon genetic mutation",
+                           "mutants": "people with an uncommon genetic mutation",
+                           "wheelchairbound":"wheelchair user",
+                           "subnormal":"person or people with learning difficulties or a developmental disorder",
+                           "dwarf":"short-statured person",
+                           "midget":"short-statured person",
+                           "deaf":"person or people with a hearing disability",
+                           "mute":"person or people with a listening disability",
+                           "dumb":"person or people with a mental and/or speech impairment",
+                           "demented":"person or people with dementia",
+                           "dotard":"old person with impaired intellect or physical disability",
+                           "dotards":"old people with impaired intellect or physical disability",
+                           "derp":"person with intellectual disabilities",
+                           "imbecile":"person with intellectual disabilities",
+                           "imbeciles":"people with intellectual disabilities",
+                           "crazy":"person or people with a mental impairment",
+                           "insane ":"person or people with a mental impairment",
+                           "wacko":"person with a mental impairment",
+                           "nuts":"person or people with a mental impairment",
+                           "retard":"person with an intellectual disability",
+                           "retards":"people with an intellectual disability",
+                           "retarded":"person or people with an intellectual disability",
+                           }
+
+
+
+# POS TAGGER
+
 def postag(text):
-  doc = nlp(text)
-  pos_list=[]
-  word_list=[]
-  for token in doc:
-   pos_list.append(token.pos_)
-   word_list.append(token.text)
-  print(pos_list,word_list)
-  return word_list,pos_list
+    doc = nlp(text)
+    pos_list = []
+    word_list = []
+    for token in doc:
+        pos_list.append(token.pos_)
+        word_list.append(token.text)
+    print(pos_list,word_list)
+    return word_list,pos_list
+
+def cleanup(text):
+    st = text
+    l = len(st)
+    indices = [i for i, x in enumerate(st) if x in string.punctuation]
+    new_str = ""
+    if len(indices) != 0:
+        if indices[-1] == l-1:
+            j = 0
+            for i in indices:
+                new_str += st[j:i-1]
+                j = i
+            new_str += st[j:]
+            return new_str
+        
+        elif indices[-1] < l-1:
+            j = 0
+            for i in indices:
+                new_str += st[j:i-1]
+                j = i
+            notlast = indices[-1]
+            new_str += st[notlast:]
+            return new_str
+          
+    elif len(indices) == 0:
+        return st
+    else:
+        return st
+
 #CAPITALIZATION
 def preserve_capitalization(original, transformed):
-  if original[0].isupper():
-    transformed = transformed.capitalize()
-  else:
-    return original
-  return transformed
+    if original[0].isupper():
+        transformed = transformed.capitalize()
+    else:
+        return original
+    return transformed
 
-  #PUNC
-def remove_punc(my_str):
-  punctuations = '''!()-[]{};:\,<>./?@#$%^&*_~'''
-  no_punct = ""
-  for char in my_str:
-   if char not in punctuations:
-       no_punct = no_punct + char
-  return no_punct    
 def get_index(wl,n):
-  indices = [i for i, x in enumerate(wl) if x == n]
-  return indices
+    indices = [i for i, x in enumerate(wl) if x == n]
+    return indices
 
 #ASSUMING FOR NAME IN TEXT
 
 def placement(index_of_dis, wl,pl,input,disability_names,name):
-  inp=input
-  print(inp)
-  text=input.lower()
-  text=remove_punc(text)
-  text = ''.join(' ' if c in '.!' else c for c in text) #for last word case
-  wl,pl=postag(text)
-  index_of_dis=get_index(wl,name)
-  max_len=len(wl)
-  #FOR EACH VALUE OF INDEX
-  for i in index_of_dis: 
-    print("For Occurence", i)
-    print("For Words Less than Maximum Length:")
-    
-    if i<(max_len-1): #1<10 
-      print("Words in between")
-      if pl[i+1]=='NOUN': 
-        #print("NOUN",i)
-        #print(pl[i+1])
-        s=' '.join(wl)
-        text=s
-        #print(1)
-      elif pl[i+1]!='NOUN':
-        s=' '.join(wl) #convert to a string and replace with disability name
-        namew=wl[i]
-        wl[i]=disability_names[namew]
-        text=' '.join(wl)
-        #text=replaces(s,name,input)
-        #print(2)
+
+    text = input.lower()
+    wl,pl = postag(text)
+    index_of_dis = get_index(wl,name)
+    max_len = len(wl)
+    #FOR EACH VALUE OF INDEX
+    for i in index_of_dis: 
+        print("For Occurence", i)
+        print("For Words Less than Maximum Length:")
+      
+        if i < (max_len-1):
+          print("Words in between")
+          if pl[i+1] == 'NOUN': 
+              s = ' '.join(wl)
+              text = s
+   
+          elif pl[i+1]!='NOUN':
+              s=' '.join(wl) #convert to a string and replace with disability name
+              namew = wl[i]
+              wl[i] = disability_names[namew]
+              text = ' '.join(wl)
+
     #last word    
-    elif i>=(max_len-1):
-      print("For Words At Maximum Length:") 
-      namew=wl[i]
-      wl[i]=disability_names[namew]
-      text=' '.join(wl)
-      #print(3)
-    else:
-      s=' '.join(wl)
-    text=preserve_capitalization(input, text)
-  return text    
+        elif i >= (max_len-1):
+            print("For Words At Maximum Length:") 
+            namew = wl[i]
+            wl[i] = disability_names[namew]
+            text = ' '.join(wl)
+   
+        else:
+            s = ' '.join(wl)
+            text = s
+
+        text = preserve_capitalization(input, text)
+    return text    
                           
-def preserve_capitalization(original, transformed):
-  if original[0].isupper():
-    transformed = transformed.capitalize()
-  else:
-    return original
-  return transformed
 
 def different_ability(input, disability_names):
-  #lowercase, remove puncuations
-  text=input.lower()
-  text=remove_punc(text)
-  text = ''.join(' ' if c in '.!' else c for c in text) #for last word case
+  #lowercase
+    text = input.lower()
 
-  for name in disability_names.keys():
-    if name in text:
-      n=name #store name
-      print(n)
-      #GET POS TAGS
-      wl,pl=postag(text)
-      #MAX LENGTH OF LIST
-      max_len=len(wl)
-      #GET ALL OCCURENCES OF NAME
-      indices=get_index(wl,name)
-      #PLACEMENT
-      text=placement(indices, wl,pl,input,disability_names,name) 
-
-  return text
+    for name in disability_names.keys():
+        if name in text:
+            #GET POS TAGS
+            wl,pl = postag(text)
+            #MAX LENGTH OF LIST
+            max_len = len(wl)
+            #GET ALL OCCURENCES OF NAME
+            indices = get_index(wl,name)
+            #PLACEMENT
+            textp = placement(indices,wl,pl,input,disability_names,name) 
+            #restore end formatting
+            text = cleanup(textp)
+    return text
 
 
 class DifferentAbilityTransformation(SentenceOperation):
@@ -176,13 +193,15 @@ if __name__ == '__main__':
     from TestRunner import convert_to_snake_case
 
     tf = DifferentAbilityTransformation()
-    #sentence = "Andrew finally returned the French book to Chris that I bought last week"
     test_cases = []
-    for sentence in ["He is blind.",             
-                     "John is deaf.",
+    for sentence in ["He's blind.",
+                     "They are deaf.",
+                     "Did she become a cripple after the accident???",             
+                     "She's wheelchairbound and he's deaf.",
                      "He's probably a retard.",
-                     "He is a psycho.",
-                     "She became a cripple after the accident."]:
+                     "A psycho was screaming.",
+                     "He is a lunatic!!!"
+                     ]:
         test_cases.append({
             "class": tf.name(),
             "inputs": {"sentence": sentence}, 
